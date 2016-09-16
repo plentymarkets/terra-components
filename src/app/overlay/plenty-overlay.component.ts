686d5dc5af8d53c573c6dff9bd4b473cc74a2ba9
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2016-09-16 13:17:21 +0200

    FIX overlay

--- src/app/overlay/plenty-overlay.component.ts
+++ src/app/overlay/plenty-overlay.component.ts
@@ -1,67 +1,74 @@
-import { Component, OnInit, Input, Output, ViewChild, EventEmitter, AfterViewInit } from '@angular/core';
-
+import {
+    Component,
+    Input,
+    Output,
+    ViewChild,
+    EventEmitter,
+    AfterViewInit
+} from '@angular/core';
+import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
-  selector: 'plenty-overlay',
-  templateUrl: './plenty-overlay.component.html',
-  styleUrls: ['./plenty-overlay.component.css']
-})
+               selector: 'plenty-overlay',
+               templateUrl: './plenty-overlay.component.html',
+               styleUrls: ['./plenty-overlay.component.css']
+           })
 export class PlentyOverlay implements AfterViewInit
 {
-  @ViewChild('overlay') public overlay:ModalDirective;
+    @ViewChild('overlay') public overlay:ModalDirective;
 
-  @Input() addButtonTooltip:string;
-  @Input() cancelButtonTooltip:string;
-  @Input() saveButtonTooltip:string;
-  @Input() overlayTitle:string;
-  @Input() hasPrimaryButton:boolean = false;
-  @Input() hasSecondaryButton:boolean = false;
-  @Input() hasSaveButton:boolean = false;
-  @Input() isStatic:boolean = false;
-  @Input() isCloseable:boolean = true;
-  @Input() isLarge:boolean = false;
-  @Input() isSmall:boolean = false;
+    @Input() addButtonTooltip: string;
+    @Input() cancelButtonTooltip: string;
+    @Input() saveButtonTooltip: string;
+    @Input() overlayTitle: string;
+    @Input() hasPrimaryButton: boolean = false;
+    @Input() hasSecondaryButton: boolean = false;
+    @Input() hasSaveButton: boolean = false;
+    @Input() isStatic: boolean = false;
+    @Input() isCloseable: boolean = true;
+    @Input() isLarge: boolean = false;
+    @Input() isSmall: boolean = false;
 
-  @Output() primaryButtonClicked = new EventEmitter<PlentyOverlay>();
-  @Output() secondaryButtonClicked = new EventEmitter<PlentyOverlay>();
-  @Output() saveButtonClicked = new EventEmitter<PlentyOverlay>();
+    @Output() primaryButtonClicked = new EventEmitter<PlentyOverlay>();
+    @Output() secondaryButtonClicked = new EventEmitter<PlentyOverlay>();
+    @Output() saveButtonClicked = new EventEmitter<PlentyOverlay>();
 
-  constructor()
-  {
-  }
-
-  ngAfterViewInit()
-  {
-    if(!this.isCloseable)
+    constructor()
     {
-      this.isStatic = true;
     }
 
-    if(this.isStatic)
+    ngAfterViewInit()
     {
-      this.overlay.config.backdrop = 'static';
-      this.overlay.config.keyboard = false;
+        if(!this.isCloseable)
+        {
+            this.isStatic = true;
+        }
+
+        if(this.isStatic)
+        {
+            this.overlay.config.backdrop = 'static';
+            this.overlay.config.keyboard = false;
+        }
     }
-  }
 
-  private primaryClicked():void
-  {
-    this.primaryButtonClicked.emit(this);
-  }
+    private primaryClicked(): void
+    {
+        this.primaryButtonClicked.emit(this);
+    }
 
-  private secondaryClicked():void
-  {
-    this.secondaryButtonClicked.emit(this);
-  }
+    private secondaryClicked(): void
+    {
+        this.secondaryButtonClicked.emit(this);
+    }
 
-  public showOverlay():void
-  {
-    this.overlay.show();
-  }
+    public showOverlay(): void
+    {
+        this.overlay.show();
+    }
 
-  public hideOverlay():void
-  {
-    this.overlay.hide();
-  }
+    public hideOverlay(): void
+    {
+        this.overlay.hide();
+    }
 
 }
