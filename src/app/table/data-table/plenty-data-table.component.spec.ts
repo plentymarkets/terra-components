fc0c29880197d81e5c8e90730fd5e16408450c1f
Michael Kunze <michael.kunze@plentymarkets.com>
2016-09-19 11:55:12 +0200

    update version to 0.0.10

--- src/app/table/data-table/plenty-data-table.component.spec.ts
+++ src/app/table/data-table/plenty-data-table.component.spec.ts
@@ -1,11 +1,11 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { PlentyDataTable } from './plenty-data-table.component';
-
-describe('Component: PlentyDataTable', () => {
-  it('should create an instance', () => {
-    let component = new PlentyDataTable();
-    expect(component).toBeTruthy();
-  });
-});
+// /* tslint:disable:no-unused-variable */
+//
+// import { TestBed, async } from '@angular/core/testing';
+// import { PlentyDataTable } from './plenty-data-table.component';
+//
+// describe('Component: PlentyDataTable', () => {
+//   it('should create an instance', () => {
+//     let component = new PlentyDataTable();
+//     expect(component).toBeTruthy();
+//   });
+// });
