c4a6cbf104bc89751845735a493765229ee22eab
Viktor Wiebe <vwiebe@uni-kassel.de>
2016-09-21 15:08:54 +0200

    extracted infobox styles

--- src/app/infobox/plenty-infobox.component.ts
+++ src/app/infobox/plenty-infobox.component.ts
@@ -1,7 +1,8 @@
 import {
     Component,
     OnInit,
-    Input
+    Input,
+    ViewEncapsulation
 } from '@angular/core';
 import { PlentyTaglist } from '../taglist/plenty-taglist.component';
 import { PlentyButton } from '../button/plenty-button.component';
@@ -9,6 +10,7 @@ import { PlentyButton } from '../button/plenty-button.component';
 @Component({
                selector: 'plenty-infobox',
                styles:   [require('./plenty-infobox.component.scss')],
+               encapsulation: ViewEncapsulation.None,
                template: require('./plenty-infobox.component.html')
            })
 export class PlentyInfobox implements OnInit
