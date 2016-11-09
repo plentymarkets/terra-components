c82272b56321f02166fd1f0bfe6a612a7f7c60d8
Daniel Trauf <daniel.trauf@plentymarkets.com>
2016-11-09 13:59:53 +0100

    refactored split-view, table, tag folder

--- src/app/split-view/plenty-split-view.component.ts
+++ src/app/split-view/plenty-split-view.component.ts
@@ -6,20 +6,20 @@ import {
 import { PlentySplitViewData } from './data/plenty-split-view-data';
 
 @Component({
-               selector:  'plenty-split-view',
-               styles:    [require('./plenty-split-view.component.scss')],
-               template:  require('./plenty-split-view.component.html')
+               selector: 'plenty-split-view',
+               styles:   [require('./plenty-split-view.component.scss').toString()],
+               template: require('./plenty-split-view.component.html')
            })
 export class PlentySplitViewComponent implements OnInit
 {
-    @Input() components:Array<PlentySplitViewData>;
-
+    @Input() inputComponents:Array<PlentySplitViewData>;
+    
     constructor()
     {
     }
-
+    
     ngOnInit()
     {
     }
-
+    
 }
--- src/app/tag/plenty-tag.component.ts
+++ src/app/tag/plenty-tag.component.ts
@@ -6,20 +6,20 @@ import {
 
 @Component({
                selector: 'plenty-tag',
-               styles:   [require('./plenty-tag.component.scss')],
+               styles:   [require('./plenty-tag.component.scss').toString()],
                template: require('./plenty-tag.component.html')
            })
 export class PlentyTag implements OnInit
 {
-    @Input() badge:string;
-    @Input() tag:string;
-
+    @Input() inputBadge:string;
+    @Input() inputTag:string;
+    
     constructor()
     {
     }
-
+    
     ngOnInit()
     {
     }
-
+    
 }
