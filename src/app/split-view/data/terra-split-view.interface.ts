390aaea6d53250195b4aa498d1dda6c9c5af2f47
Ziyad Hajj-Hassan <ziyad.hajj-hassan@plentymarkets.com>
2016-12-12 12:34:07 +0100

    removing style from split view interface, using css instead

--- src/app/split-view/data/terra-split-view.interface.ts
+++ src/app/split-view/data/terra-split-view.interface.ts
@@ -5,7 +5,6 @@ export interface TerraSplitViewInterface
 {
     module:any;
     defaultWidth:string;
-    verticalAlignment:string;
     hidden:boolean;
     name:string;
     mainComponentName:string;
