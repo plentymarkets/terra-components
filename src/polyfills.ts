8e4585b756dd67a746698a332f6a0be92dfade68
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2016-12-14 14:22:45 +0100

    - code formatting

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,12 +2,14 @@ import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-if (process.env.ENV === 'production') {
+if(process.env.ENV === 'production')
+{
     // Production
-} else
+}
+else
 {
     // Development
-    Error.stackTraceLimit = Infinity;
-
+    Error['stackTraceLimit'] = Infinity;
+    
     require('zone.js/dist/long-stack-trace-zone');
 }
