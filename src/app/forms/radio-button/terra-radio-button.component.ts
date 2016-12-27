d757f1ba8f88d70917a9df11462b3d2f1028092a
Michael Frank <michael.frank@plentymarkets.com>
2016-12-27 16:12:44 +0100

    feature radio_buttons - add ngModel two way binding to radio buttons

--- src/app/forms/radio-button/terra-radio-button.component.ts
+++ src/app/forms/radio-button/terra-radio-button.component.ts
@@ -1,40 +1,82 @@
 import {
     Component,
     Input,
-    Output,
-    EventEmitter
+    HostListener,
+    forwardRef
 } from '@angular/core';
+import {
+    ControlValueAccessor,
+    NG_VALUE_ACCESSOR
+} from '@angular/forms';
+
+export const RADIO_CONTROL_VALUE_ACCESSOR:any = {
+    provide:     NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TerraRadioButtonComponent),
+    multi:       true
+};
 
 /**
  * @author mfrank
  */
 @Component({
-               selector: 'terra-radio-button',
-               template: require('./terra-radio-button.component.html'),
-               styles:   [require('./terra-radio-button.component.scss').toString()]
+               selector:  'terra-radio-button',
+               template:  require('./terra-radio-button.component.html'),
+               styles:    [require('./terra-radio-button.component.scss').toString()],
+               providers: [RADIO_CONTROL_VALUE_ACCESSOR]
            })
-export class TerraRadioButtonComponent
+export class TerraRadioButtonComponent implements ControlValueAccessor
 {
     @Input() inputCaption:string;
     @Input() inputName:string;
     @Input() inputIsDisabled:boolean;
-    @Output() inputValueChange = new EventEmitter<any>();
+    @Input() inputValue:any;
+    @Input() inputIsUncheckable:boolean;
     
-    private _value:any = false;
-    
-    @Input()
-    public get inputValue():any
+    @HostListener('click')
+    public onClick():void
     {
-        return this._value;
+        if(this.inputIsDisabled)
+        {
+            return;
+        }
+        
+        if(this.inputIsUncheckable && this.inputName === this.inputValue)
+        {
+            this.inputValue = undefined;
+        }
+        else
+        {
+            this.inputValue = this.inputName;
+        }
+        
+        this.onTouchedCallback();
+        this.onChangeCallback(this.inputValue);
     }
     
-    public set inputValue(value:any)
+    private onTouchedCallback:() => void = () =>
     {
-        this._value = value;
-        setTimeout(() => this.inputValueChange.emit(this.inputValue), 0);
-    }
+    };
+    
+    private onChangeCallback:(_:any) => void = (_) =>
+    {
+    };
     
     constructor()
     {
+    }
+    
+    public writeValue(value:any):void
+    {
+        this.inputValue = value;
+    }
+    
+    public registerOnChange(fn:any):void
+    {
+        this.onChangeCallback = fn;
+    }
+    
+    public registerOnTouched(fn:any):void
+    {
+        this.onTouchedCallback = fn;
     }
 }
