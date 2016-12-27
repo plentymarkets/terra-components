42153aaa5c4e0d17dcb654ab240b84e9e1d7e911
Michael Frank <michael.frank@plentymarkets.com>
2016-12-27 13:15:14 +0100

    - format code - remove unused code

--- src/app/forms/radio-button/terra-radio-button.component.ts
+++ src/app/forms/radio-button/terra-radio-button.component.ts
@@ -2,27 +2,22 @@ import {
     Component,
     Input,
     Output,
-    EventEmitter,
-    ViewChild,
-    OnInit
+    EventEmitter
 } from '@angular/core';
 
 /**
  * @author mfrank
  */
 @Component({
-               selector:  'terra-radio-button',
-               template:  require('./terra-radio-button.component.html'),
-               styles:    [require('./terra-radio-button.component.scss').toString()]
+               selector: 'terra-radio-button',
+               template: require('./terra-radio-button.component.html'),
+               styles:   [require('./terra-radio-button.component.scss').toString()]
            })
-export class TerraRadioButtonComponent implements OnInit
+export class TerraRadioButtonComponent
 {
-    @ViewChild('viewChildRadioButton') viewChildRadioButton;
-    
     @Input() inputCaption:string;
     @Input() inputName:string;
     @Input() inputIsDisabled:boolean;
-    @Input() inputIsUncheckable:boolean;
     @Output() inputValueChange = new EventEmitter<any>();
     
     private _value:any = false;
@@ -41,14 +36,5 @@ export class TerraRadioButtonComponent implements OnInit
     
     constructor()
     {
-        this.inputIsUncheckable = false;
-    }
-    
-    public ngOnInit():void
-    {
-        if(this.inputIsUncheckable)
-        {
-            this.viewChildRadioButton.nativeElement.attributes.uncheckable = true;
-        }
     }
 }
