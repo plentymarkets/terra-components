38024757163a8d7058e52b85c14b5234809d457b
Tim Wieder <tim.wieder@plentymarkets.com>
2017-02-22 13:13:35 +0100

    deleted the test-environment for overlay

--- src/app/terra-components.component.ts
+++ src/app/terra-components.component.ts
@@ -22,9 +22,6 @@ export class TerraComponentsComponent extends Locale implements OnInit
 {
     private _viewContainerRef:ViewContainerRef;
     
-    @ViewChild('overlay') overlay:TerraOverlayComponent;
-    private _primaryButtonInterface:TerraOverlayButtonInterface;
-    private _secondaryButtonInterface:TerraOverlayButtonInterface;
     
     public constructor(private viewContainerRef:ViewContainerRef,
                        public local:LocaleService,
@@ -73,50 +70,11 @@ export class TerraComponentsComponent extends Locale implements OnInit
     
     ngOnInit()
     {
-        this.primaryButtonInterface = {
-            icon:          'icon-save',
-            caption:       'Speichern',
-            tooltipText:   'Test',
-            isDisabled:    false,
-            clickFunction: () => this.closeOverlay()
-        };
         
-        this.secondaryButtonInterface = {
-            icon:          'icon-cancel',
-            caption:       'Cancel',
-            tooltipText:   'TEST',
-            isDisabled:    false,
-            clickFunction: () => this.closeOverlay()
-        };
     }
     
     toggle()
     {
-        this.overlay.showOverlay();
-    }
-    
-    closeOverlay()
-    {
-        this.overlay.hideOverlay();
-    }
-    
-    public get primaryButtonInterface():TerraOverlayButtonInterface
-    {
-        return this._primaryButtonInterface;
-    }
-    
-    public set primaryButtonInterface(value:TerraOverlayButtonInterface)
-    {
-        this._primaryButtonInterface = value;
-    }
-    
-    public get secondaryButtonInterface():TerraOverlayButtonInterface
-    {
-        return this._secondaryButtonInterface;
-    }
-    
-    public set secondaryButtonInterface(value:TerraOverlayButtonInterface)
-    {
-        this._secondaryButtonInterface = value;
+        
     }
 }
