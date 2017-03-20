2107b8caab05be38d924173169079d5b36a18d70
Viktor Wiebe <vwiebe@uni-kassel.de>
2017-03-20 13:32:13 +0100

    updated splitview

--- src/app/split-view/data/terra-split-config-base.ts
+++ src/app/split-view/data/terra-split-config-base.ts
@@ -6,17 +6,24 @@ export class TerraSplitConfigBase
     
     public addModule(module:TerraSplitViewInterface):void
     {
-        for(let i = 0; i < this._modules.length; i++)
+        for (let i = 0; i < this._modules.length; i++)
         {
+            let isSameInstanceKey = this._modules[i].instanceKey != null && this._modules[i].instanceKey == module.instanceKey;
+            
             // check if this module is already loaded
-            if(this._modules[i].mainComponentName == module.mainComponentName ||
-               (this._modules[i].instanceKey != null && this._modules[i].instanceKey == module.instanceKey))
+            if (this._modules[i].mainComponentName == module.mainComponentName ||
+               (isSameInstanceKey))
             {
-                if(this._modules[i].parameter == module.parameter)
+                if (this._modules[i].parameter == module.parameter)
                 {
                     // same module, same parameters => do nothing
                     return;
                 }
+                else if (!isSameInstanceKey)
+                {
+                    break;
+                }
+                
                 // same module, different parameters => drop old instance (including child modules) and reload
                 this._modules = this._modules.slice(0, i);
                 break;
