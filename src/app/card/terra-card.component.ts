d52c5b23582f2d76031c2759bb44aea87713a3e6
Michael Kunze <michael.kunze@plentymarkets.com>
2017-03-21 09:14:35 +0100

    added card

---
+++ src/app/card/terra-card.component.ts
@@ -0,0 +1,31 @@
+import {
+    Component,
+    Input,
+    ViewChild,
+    AfterContentChecked
+} from '@angular/core';
+
+@Component({
+               selector: 'terra-card',
+               styles:   [require('./terra-card.component.scss')],
+               template: require('./terra-card.component.html')
+           })
+export class TerraCardComponent implements AfterContentChecked
+{
+    @Input() inputImagePath;
+    
+    @ViewChild('header') viewChildHeader;
+    private showHeader:boolean = false;
+    @ViewChild('footer') viewChildFooter;
+    private showFooter:boolean = false;
+    
+    constructor()
+    {
+    }
+    
+    ngAfterContentChecked()
+    {
+        this.showHeader = this.viewChildHeader.nativeElement.children.length > 0;
+        this.showFooter = this.viewChildFooter.nativeElement.children.length > 0;
+    }
+}
