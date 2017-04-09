3b91520b29fb210fe7516dd9e5289e29d8b53fdb
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2017-04-09 22:16:39 +0200

    NEW show collapse icon if children are given

---
+++ src/app/navigator/button-group/data/terra-button-group.interface.ts
@@ -0,0 +1,9 @@
+import { TerraButtonInterface } from '../../../button/data/terra-button.interface';
+
+/**
+ * @author mscharf
+ */
+export interface TerraButtonGroupInterface extends TerraButtonInterface
+{
+    hasChildren:boolean;
+}
