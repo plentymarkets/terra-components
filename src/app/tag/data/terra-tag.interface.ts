840a4cc243226c93a05c4e074e183c4c03f4c579
Michael Frank <michael.frank@plentymarkets.com>
2017-04-11 15:49:15 +0200

    fix/tag_component - add deprecated warning to prevent major release

--- src/app/tag/data/terra-tag.interface.ts
+++ src/app/tag/data/terra-tag.interface.ts
@@ -4,5 +4,12 @@
  */
 export interface TerraTagInterface
 {
-    badge:string;
+    /**
+     * @deprecated will be removed in next major release. Please use badge instead.
+     */
+    caption?:string;
+    /**
+     *  will be mandatory in next major release.
+     */
+    badge?:string;
 }
