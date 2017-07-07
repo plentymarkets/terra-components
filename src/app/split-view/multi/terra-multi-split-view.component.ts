0d5cc22aea55e8864bc9a7d1a028526d54a7bc16
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-07-07 12:06:27 +0200

    Update terra-multi-split-view.component.ts
    removed empty lines..

--- src/app/split-view/multi/terra-multi-split-view.component.ts
+++ src/app/split-view/multi/terra-multi-split-view.component.ts
@@ -279,10 +279,7 @@ export class TerraMultiSplitViewComponent implements OnDestroy, OnInit
                                         }, 1000);
                                     }
                                 }
-
                             });
-                            
-                            
                         });
                     });
                 });
