0398867f410555e30ffc74ecd728a1b067528707
Viktor Wiebe <viktor.wiebe@plentymarkets.com>
2017-07-31 17:08:46 +0200

    updated input type

--- src/app/dynamic-module-loader/terra-dynamic-module-loader.component.ts
+++ src/app/dynamic-module-loader/terra-dynamic-module-loader.component.ts
@@ -23,7 +23,7 @@ export class TerraDynamicModuleLoaderComponent implements AfterViewInit, OnDestr
     @ViewChild('viewChildTarget', {read: ViewContainerRef}) viewChildTarget;
     @Input() inputModule:any;
     @Input() inputMainComponentName:string;
-    @Input() inputInputs:Array<{key:string, value:any}>;
+    @Input() inputInputs:Array<TerraMultiSplitViewInputInterface>;
     @Input() inputView:TerraMultiSplitViewInterface;
     private _resolvedData:ModuleWithProviders;
 
