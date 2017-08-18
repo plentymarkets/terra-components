4c4443592e10900e52a15fb09e4b71b4f16fac6e
Felix Dausch <felix.dausch@plentymarkets.com>
2017-08-18 11:39:59 +0200

    ADD input properties to make portlets collapsable

--- src/app/portlet/terra-portlet.component.ts
+++ src/app/portlet/terra-portlet.component.ts
@@ -1,18 +1,79 @@
 import {
-    Component,
-    Input
+    Component, EventEmitter,
+    Input, OnChanges, Output, SimpleChanges
 } from '@angular/core';
+import { animate, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
-               selector: 'terra-portlet',
-               styles:   [require('./terra-portlet.component.scss')],
-               template: require('./terra-portlet.component.html')
-           })
-export class TerraPortletComponent
+    selector: 'terra-portlet',
+    styles:   [require('./terra-portlet.component.scss')],
+    template: require('./terra-portlet.component.html'),
+    animations: [
+        trigger('collapsedState', [
+            state('collapsed', style({
+                height: 0,
+                'padding-top': 0,
+                'padding-bottom': 0
+            })),
+            state('expanded', style({
+                height: '*'
+            })),
+            transition('collapsed <=> expanded', [
+                animate(100)
+            ])
+        ])
+    ]
+})
+export class TerraPortletComponent implements OnChanges
 {
+
     @Input() inputPortletHeader:string;
+
+    @Input() isCollapsable: boolean = false;
+
+    @Input() collapsed: boolean = false;
+
+    @Output() collapsedChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    private get collapsedState(): string
+    {
+        if ( !this.isCollapsable )
+        {
+            return 'expanded';
+        }
+
+        if ( this.collapsed )
+        {
+            return 'collapsed';
+        }
+
+        return 'expanded';
+    }
 
     constructor()
     {
+    }
+
+    public ngOnChanges( changes: SimpleChanges ): void
+    {
+        if( changes.hasOwnProperty( "isCollapsable") && !this.isCollapsable )
+        {
+            this.collapsed = false;
+            setTimeout( () => {
+                this.collapsedChange.emit( false );
+            });
+        }
+    }
+
+    public toggleCollapse(): void
+    {
+        this.collapsed = !this.collapsed;
+
+        if ( !this.isCollapsable )
+        {
+            this.collapsed = false;
+        }
+
+        this.collapsedChange.emit( this.collapsed );
     }
 }
--- src/app/terra-components.module.ts
+++ src/app/terra-components.module.ts
@@ -65,6 +65,7 @@ import { TerraMultiSplitViewComponent } from './split-view/multi/terra-multi-spl
 import { TerraSplitViewComponent } from './split-view/terra-split-view.component';
 import { CommonModule } from '@angular/common';
 import { TerraDynamicComponentLoaderComponent } from './dynamic-component-loader/terra-dynamic-component-loader.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 export { TerraAlertPanelComponent } from './alert/terra-alert-panel.component';
 export { TerraAlertComponent } from './alert/terra-alert.component';
 export { TerraButtonInterface } from './button/data/terra-button.interface';
@@ -256,6 +257,7 @@ export { TerraSyntaxEditorData } from './editor/syntax/data/terra-syntax-editor.
                   TerraSyntaxEditorComponent
               ],
               imports:         [
+                  BrowserAnimationsModule,
                   CommonModule,
                   FormsModule,
                   ReactiveFormsModule,
