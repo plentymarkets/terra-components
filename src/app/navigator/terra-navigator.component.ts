ac9ad54a84f86b8f56eb16f7bb53271901e19f70
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2017-08-30 11:43:33 +0200

    FIX code style

--- src/app/navigator/terra-navigator.component.ts
+++ src/app/navigator/terra-navigator.component.ts
@@ -73,8 +73,7 @@ export class TerraNavigatorComponent<D> implements OnInit, OnChanges
                            });
         }
 
-        this._terraNavigatorSplitViewConfig
-            .observableNodeClicked
+        this._terraNavigatorSplitViewConfig.observableNodeClicked
             .subscribe((item:TerraNavigatorNodeInterface<D>) =>
                        {
                            if(isNullOrUndefined(item.rootPath))
@@ -86,9 +85,7 @@ export class TerraNavigatorComponent<D> implements OnInit, OnChanges
 
                            if(!isNullOrUndefined(item.children))
                            {
-                               this._terraNavigatorSplitViewConfig
-                                   .modules[0]
-                                   .defaultWidth = 'col-xs-6 col-md-6 col-lg-6';
+                               this._terraNavigatorSplitViewConfig.modules[0].defaultWidth = 'col-xs-6 col-md-6 col-lg-6';
 
                                this._terraNavigatorSplitViewConfig
                                    .addModule({
@@ -114,8 +111,7 @@ export class TerraNavigatorComponent<D> implements OnInit, OnChanges
                            }
                        });
 
-        this.inputNavigatorService
-            .observableNewNodeByRootPath
+        this.inputNavigatorService.observableNewNodeByRootPath
             .subscribe((item:TerraNavigatorNodeInterface<D>) =>
                        {
                            this.addNodeAt(this.inputNodes, item.rootPath, -1, item);
@@ -124,8 +120,7 @@ export class TerraNavigatorComponent<D> implements OnInit, OnChanges
                            this.refreshNodeVisibilities(this.inputNodes);
                        });
 
-        this.inputNavigatorService
-            .observableNewNodesByRoute
+        this.inputNavigatorService.observableNewNodesByRoute
             .subscribe((item:Array<TerraNavigatorNodeInterface<D>>) =>
                        {
                            this.addNodesRecursive(item);
