c34da958fd54c37e0fe4915365861b269cc547a9
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-09-04 16:04:49 +0200

    fix order of inputs

--- src/app/dynamic-module-loader/terra-dynamic-module-loader.component.ts
+++ src/app/dynamic-module-loader/terra-dynamic-module-loader.component.ts
@@ -24,9 +24,9 @@ export class TerraDynamicModuleLoaderComponent implements AfterViewInit, OnDestr
     @ViewChild('viewChildTarget', {read: ViewContainerRef}) viewChildTarget;
     @Input() inputModule:any;
     @Input() inputMainComponentName:string;
+    @Input() inputParameter:any; // TODO: remove input if old split-view is removed
     @Input() inputInputs:Array<TerraMultiSplitViewInputInterface>;
     @Input() inputView:TerraMultiSplitViewInterface;
-    @Input() inputParameter:any; // TODO: remove input if old split-view is removed
     private _resolvedData:ModuleWithProviders;
 
     private _cmpRef:ComponentRef<any>;
