e7fa0480c8bb7cc6a81e1b043cd42a9faeabb1d9
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-09-04 09:25:18 +0200

    remove unused interface properties

--- src/app/split-view/multi/data/terra-multi-split-view.interface.ts
+++ src/app/split-view/multi/data/terra-multi-split-view.interface.ts
@@ -8,10 +8,8 @@ export class TerraMultiSplitViewInterface
     parent?:TerraMultiSplitViewInterface;
     children?:Array<TerraMultiSplitViewInterface>;
     module:ModuleWithProviders;
-    instanceKey?:any; // TODO: REMOVE!.. We don't need this anymore
     defaultWidth:string;
     focusedWidth?:string;
-    hidden?:boolean; // TODO: REMOVE!.. We don't need this
     name:string;
     mainComponentName:string;
     inputs?:Array<TerraMultiSplitViewInputInterface>;
