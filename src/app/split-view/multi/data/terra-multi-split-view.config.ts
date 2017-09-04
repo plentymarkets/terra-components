0c36c2f727e5be395fd4534b41bb3c1ad85b921d
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-09-04 16:21:04 +0200

    check for null

--- src/app/split-view/multi/data/terra-multi-split-view.config.ts
+++ src/app/split-view/multi/data/terra-multi-split-view.config.ts
@@ -46,8 +46,8 @@ export class TerraMultiSplitViewConfig
                         {
                             // TODO very ugly way, maybe add an option to use an id?
                             let hasSameParameter:boolean =
-                                JSON.stringify(child.parameter) === JSON.stringify(view.parameter)
-                                || JSON.stringify(child.inputs) === JSON.stringify(view.inputs);
+                                (child.parameter && view.parameter && JSON.stringify(child.parameter) === JSON.stringify(view.parameter))
+                                || (child.inputs && view.inputs && JSON.stringify(child.inputs) === JSON.stringify(view.inputs));
 
                             if(hasSameParameter && child.module.ngModule == view.module.ngModule)
                             {
