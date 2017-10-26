e10808dd5506ae830c4132e0a256a875b1923d54
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-10-26 12:04:47 +0200

    implement ControlValueAccessor in order to be able to use ngModel pattern

--- src/app/wysiwyg-editor/terra-wysiwyg-editor.component.ts
+++ src/app/wysiwyg-editor/terra-wysiwyg-editor.component.ts
@@ -1,25 +1,46 @@
 import {
     Component,
+    EventEmitter,
+    forwardRef,
     Input,
-    OnInit
+    OnInit,
+    Output
 } from '@angular/core';
 import { TranslationService } from 'angular-l10n';
+import {
+    ControlValueAccessor,
+    NG_VALUE_ACCESSOR
+} from '@angular/forms';
 
 @Component({
     selector: 'terra-wysiwyg-editor',
     template: require('./terra-wysiwyg-editor.component.html'),
-    styles:   [require('./terra-wysiwyg-editor.component.scss')]
+    styles:   [require('./terra-wysiwyg-editor.component.scss')],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => TerraWysiwygEditorComponent),
+        multi: true
+    }]
 })
-export class TerraWysiwygEditorComponent implements OnInit
+export class TerraWysiwygEditorComponent implements OnInit, ControlValueAccessor
 {
     @Input() inputPlaceholder:string;
 
+    @Output() ngModelChange:EventEmitter<string> = new EventEmitter();
+
     private _placeholder:string;
+    private _value:string;
+    private _onChangeCallback:() => void;
+    private _onTouchedCallback:() => void;
 
     constructor(private translation:TranslationService)
     {
         // initialize placeholder
         this._placeholder = this.translation.translate('terraEditor.insertText');
+
+        // initialize callbacks
+        this._onChangeCallback = () => {};
+        this._onTouchedCallback = () => {};
     }
 
     public ngOnInit():void
@@ -29,5 +50,20 @@ export class TerraWysiwygEditorComponent implements OnInit
         {
             this._placeholder = this.inputPlaceholder;
         }
+    }
+
+    public writeValue(value:string):void
+    {
+        this._value = value;
+    }
+
+    public registerOnChange(fn:any):void
+    {
+        this._onChangeCallback = fn;
+    }
+
+    public registerOnTouched(fn:any):void
+    {
+        this._onTouchedCallback = fn;
     }
 }
