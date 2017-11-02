aa1556c835c77f81c897d7eaab34f0c92a2e7b30
ChristianMoll <christian.moll@plentymarkets.com>
2017-11-02 15:58:06 +0100

    Made sure that the context menu stays in bounds if opened close to borders.

--- src/app/table/data-table/context-menu/terra-data-table-context-menu.component.ts
+++ src/app/table/data-table/context-menu/terra-data-table-context-menu.component.ts
@@ -16,6 +16,12 @@ export class TerraDataTableContextMenuComponent<D extends TerraBaseData>
     private _contextMenuLinkList:Array<TerraDataTableContextMenuEntryInterface<D>> = [];
     private _isShown = false;
     private clickListener:(event:Event) => void;
+    private _locationCss:any = {
+        visibility: 'hidden',
+        left:        0,
+        top:         0,
+        right:       0
+    };
 
     private _mouseLocation:{ left:number, top:number } = {
         left: 0,
@@ -26,6 +32,10 @@ export class TerraDataTableContextMenuComponent<D extends TerraBaseData>
     {
         _contextMenuService.show.subscribe(
             e => this.showMenu(e.event, e.obj));
+        
+        _contextMenuService.init.subscribe(
+            e => this._contextMenuLinkList = e
+        );
 
         this.clickListener = (event) =>
         {
@@ -34,30 +44,10 @@ export class TerraDataTableContextMenuComponent<D extends TerraBaseData>
         }
     }
 
-    get locationCss()
-    {
-        // 70 (navbar) + 46 (tabbar) + 33 (breadcrumbs)
-        let offsetTop:number = 158;
-        let offsetLeft:number;
-        let anchor = $('.context-menu#menu');
-        let isMenuAtBottom:boolean;
-        if(anchor.offset().top + anchor.height() > innerHeight)
-        {
-            isMenuAtBottom = true;
-        }
-
-        offsetLeft = anchor.closest('terra-data-table').offset().left;
-
-        return {
-            visibility: this._isShown ? 'visible' : 'hidden',
-            left:       this._mouseLocation.left - offsetLeft - 2 + 'px',
-            top:        isMenuAtBottom ? this._mouseLocation.top - offsetTop - anchor.height() + 'px' : this._mouseLocation.top - offsetTop - 2 + 'px'
-        };
-    }
-
     clickedOutside()
     {
         this._isShown = false;
+        this._locationCss = this.calcMenuPosition();
         document.removeEventListener('click', this.clickListener);
     }
 
@@ -70,8 +60,44 @@ export class TerraDataTableContextMenuComponent<D extends TerraBaseData>
             left: event.clientX,
             top:  event.clientY
         };
+        
+        this._locationCss = this.calcMenuPosition();
 
         event.stopPropagation();
         document.addEventListener('click', this.clickListener);
+    }
+    
+    private calcMenuPosition():{visibility: string, left: string, top: string}
+    {
+        // 70 (navbar) + 46 (tabbar) + 33 (breadcrumbs)
+        let offsetTop:number = 158;
+        let offsetLeft:number;
+        let anchor = $('.context-menu#menu');
+        let isMenuAtBottom:boolean;
+        let contextMenuHeight:number = anchor.height();
+        let contextMenuWidth:number = anchor.width();
+        let tableWidth:number;
+        
+        if(this._mouseLocation.top +  contextMenuHeight > innerHeight)
+        {
+            isMenuAtBottom = true;
+        }
+        
+        let dataTableElement = anchor.closest('terra-data-table');
+        
+        offsetLeft = dataTableElement.offset().left;
+        
+        tableWidth = dataTableElement.find('tbody').width();
+        
+        if(Math.abs(this._mouseLocation.left - offsetLeft - 2) +contextMenuWidth > tableWidth)
+        {
+            offsetLeft = offsetLeft + contextMenuWidth - 6
+        }
+        
+        return {
+            visibility: this._isShown ? 'visible' : 'hidden',
+            left:       this._mouseLocation.left - offsetLeft - 2 + 'px',
+            top:        isMenuAtBottom ? this._mouseLocation.top - offsetTop - contextMenuHeight + 'px' : this._mouseLocation.top - offsetTop - 2 + 'px'
+        };
     }
 }
