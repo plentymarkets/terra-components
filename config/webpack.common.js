e596497f77dc485f40837feac4be85c7d6853e8b
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2017-11-10 11:54:54 +0100

    UPDATE gitignore

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -50,7 +50,6 @@ module.exports = function (options) {
                     ]
                 },
                 {
-
                     test: /\.scss$/,
                     exclude: [/\.glob\.scss$/],
                     loaders: [
