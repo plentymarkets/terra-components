aa56ed2b70fa7a78de11aa93b39580fd6e8432f2
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-12-14 13:29:50 +0100

    remove legacy interface parameter

--- src/app/table/data-table/cell/terra-data-table-header-cell.interface.ts
+++ src/app/table/data-table/cell/terra-data-table-header-cell.interface.ts
@@ -5,6 +5,9 @@ export interface TerraDataTableHeaderCellInterface
 {
     caption:string;
     width:number;
+    /**
+     * this parameter should state the name of the corresponding data base table cell
+     */
     sortBy?:string;
     isHidden?:boolean;
     tooltipText?:string;
