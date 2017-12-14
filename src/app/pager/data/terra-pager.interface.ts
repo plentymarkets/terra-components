8a3f5a06b59cd9f4598b5bc49fa7add68ce26a8c
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2017-12-14 10:33:47 +0100

    making pager interface attributes mandatory again

--- src/app/pager/data/terra-pager.interface.ts
+++ src/app/pager/data/terra-pager.interface.ts
@@ -2,11 +2,11 @@ export interface TerraPagerInterface
 {
     page:number;
     itemsPerPage:number;
-    totalsCount?:number;
-    isLastPage?:boolean;
-    lastPageNumber?:number;
-    firstOnPage?:number;
-    lastOnPage?:number;
+    totalsCount:number;
+    isLastPage:boolean;
+    lastPageNumber:number;
+    firstOnPage:number;
+    lastOnPage:number;
     pagingUnit?:string; //Label for pager items
     entries?:Array<any>;
 }
