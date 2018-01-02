a8f9d4afeb9f45fc508fb037efaae5895328b15e
Michael Kunze <michael.kunze@plentymarkets.com>
2018-01-02 14:16:17 +0100

    fuzzy search for terra node tree

--- src/app/tree/node-tree/terra-node-tree.component.ts
+++ src/app/tree/node-tree/terra-node-tree.component.ts
@@ -5,9 +5,14 @@ import {
     OnInit
 } from '@angular/core';
 import { TerraNodeTreeConfig } from './data/terra-node-tree.config';
-import { isNullOrUndefined } from "util";
+import {
+    inspect,
+    isNullOrUndefined
+} from "util";
 import { TerraNodeInterface } from './data/terra-node.interface';
 import { TranslationService } from 'angular-l10n';
+import * as Fuse from 'fuse.js';
+import { FuseOptions } from 'fuse.js';
 
 @Component({
     selector: 'terra-node-tree',
@@ -16,6 +21,7 @@ import { TranslationService } from 'angular-l10n';
 })
 export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
 {
+    private _doExecuteFuzzySearch:boolean;
     protected _searchValue:string;
 
     /**
@@ -35,11 +41,13 @@ export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
     public ngOnInit():void
     {
         this.handleVisibility(this.inputConfig.list);
+        this.recursiveTranslateName(this.inputConfig.list);
     }
 
     private handleVisibility(nodeList:Array<TerraNodeInterface<D>>):void
     {
-        nodeList.forEach((node:TerraNodeInterface<D>)=>{
+        nodeList.forEach((node:TerraNodeInterface<D>) =>
+        {
 
             if(node.isVisible)
             {
@@ -59,7 +67,8 @@ export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
 
     private handleDefaultVisibility(nodeList:Array<TerraNodeInterface<D>>):void
     {
-        nodeList.forEach((node:TerraNodeInterface<D>)=>{
+        nodeList.forEach((node:TerraNodeInterface<D>) =>
+        {
 
             if(node.defaultVisibility)
             {
@@ -94,6 +103,7 @@ export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
         {
             this.handleDefaultVisibility(this.inputConfig.list);
             this.inputConfig.closeAllNodes();
+            this._doExecuteFuzzySearch = false;
         }
     }
 
@@ -118,17 +128,84 @@ export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
                 this.recursiveCheckList(node.children);
             }
         });
+
+        if(this._doExecuteFuzzySearch)
+        {
+            let options:FuseOptions = {
+                shouldSort:         true,
+                threshold:          0.3,
+                location:           0,
+                distance:           100,
+                maxPatternLength:   32,
+                minMatchCharLength: 1,
+                keys:               [
+                    "tags",
+                    "name"
+                ]
+            };
+
+            //cache to discard circular keys (i.e. parent)
+            let cache:Array<string> = [];
+
+            let jsonString:string =  JSON.stringify(list, function (key, value) {
+                if ((typeof value === 'undefined' ? 'undefined' : typeof(value)) === 'object' && value !== null) {
+                    if (cache.indexOf(value) !== -1) {
+                        // Circular reference found, discard key
+                        return;
+                    }
+                    // Store value in our collection
+                    cache.push(value);
+                }
+                return value;
+            });
+
+            cache = null;
+
+            let fuse:Fuse = new Fuse(JSON.parse(jsonString), options);
+
+            let foundList:Array<TerraNodeInterface<D>> = fuse.search(this._searchValue);
+
+            for(let node of foundList)
+            {
+                let fuzzyNode:TerraNodeInterface<D> = this.inputConfig.findNodeById(node.id);
+
+                this.handleNodeVisibility(fuzzyNode);
+            }
+
+            for(let node of list)
+            {
+                if(!isNullOrUndefined(node.children))
+                {
+                    this.recursiveCheckList(node.children);
+                }
+            }
+
+            this._doExecuteFuzzySearch = false;
+        }
+    }
+
+    private recursiveTranslateName(list:Array<TerraNodeInterface<D>>):void
+    {
+        for(let node of list)
+        {
+            node.name = this._translation.translate(node.name);
+
+            if(!isNullOrUndefined(node.children))
+            {
+                this.recursiveTranslateName(node.children);
+            }
+        }
     }
 
     private handleSearch(node:TerraNodeInterface<D>, value:string):void
     {
+        let tagMatchFound:boolean = false;
         let tags:Array<string> = node.tags;
         if(!isNullOrUndefined(tags))
         {
-            let tagMatchFound:boolean = false;
             tags.forEach((tag:string) =>
             {
-                if(tag.toUpperCase().includes(value.toUpperCase()))
+                if(tag.toUpperCase() === value.toUpperCase())
                 {
                     tagMatchFound = true;
                     return;
@@ -141,15 +218,20 @@ export class TerraNodeTreeComponent<D> implements OnDestroy, OnInit
             }
         }
 
-        let name:string = this._translation.translate(node.name);
+        //let name:string = this._translation.translate(node.name);
 
-        let suggestion:string = name.toUpperCase();
+        let suggestion:string = node.name.toUpperCase();
+
+        let suggestionMatch:boolean = false;
 
         // check if search string is included in the given suggestion
-        if(suggestion.includes(value.toUpperCase()))
+        if(suggestion === value.toUpperCase())
         {
+            suggestionMatch = true;
             this.handleNodeVisibility(node);
         }
+
+        this._doExecuteFuzzySearch = !suggestionMatch && !tagMatchFound;
     }
 
     private handleNodeVisibility(node:TerraNodeInterface<D>):void
