e218e86322d2236db541db932e0e4299bddb0987
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-01-05 13:27:55 +0100

    added tag to get a type for 'empty'

--- src/app/tag/terra-tag.component.ts
+++ src/app/tag/terra-tag.component.ts
@@ -11,19 +11,34 @@ import {
 })
 export class TerraTagComponent implements OnInit
 {
+    public get isActive():boolean
+    {
+        return this._isActive;
+    }
+
     @Input() inputBadge:string;
     @Input() inputIsTagged:boolean;
     @Input() inputIsTaggable:boolean;
     @Input() inputCustomClass:string;
+    @Input() inputIsClickable:boolean;
+
+    private _isActive:boolean;
 
     constructor()
     {
         this.inputIsTagged = false;
         this.inputIsTaggable = false;
         this.inputCustomClass = null;
+        this.inputIsClickable = false;
+        this._isActive = false;
     }
 
     ngOnInit()
     {
+    }
+
+    private toggleActive():void
+    {
+        this._isActive = !this._isActive;
     }
 }
