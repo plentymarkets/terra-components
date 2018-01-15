f3c2dd6ea375a78adabb599931cf30af2e8a24b0
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2018-01-15 11:36:14 +0100

    FIX magic numbers

--- src/app/helpers/terra-pdf.helper.ts
+++ src/app/helpers/terra-pdf.helper.ts
@@ -2,12 +2,13 @@ export class TerraPdfHelper
 {
     static createPdfBlob(base64String:string):Blob
     {
+        let sliceSize:number = 512;
         let byteCharacters:string = atob(base64String);
         let byteArrays:any = [];
 
-        for(let offset:number = 0; offset < byteCharacters.length; offset += 512)
+        for(let offset:number = 0; offset < byteCharacters.length; offset += sliceSize)
         {
-            let slice:string = byteCharacters.slice(offset, offset + 512);
+            let slice:string = byteCharacters.slice(offset, offset + sliceSize);
 
             let byteNumbers:Array<any> = new Array(slice.length);
             for(let i:number = 0; i < slice.length; i++)
