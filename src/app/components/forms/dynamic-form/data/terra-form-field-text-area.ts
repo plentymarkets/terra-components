8a0f4f0b023c072a5daef1a03d940773d2961773
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-02-08 14:07:11 +0100

    added terra form field text area

---
+++ src/app/components/forms/dynamic-form/data/terra-form-field-text-area.ts
@@ -0,0 +1,25 @@
+import {
+    TerraFormFieldBase,
+    TerraFormFieldBaseOptions
+} from './terra-form-field-base';
+import { TerraControlTypeEnum } from '../enum/controlType.enum';
+
+/**
+ * @author mfrank
+ */
+export interface TerraFormFieldInputTextOptions extends TerraFormFieldBaseOptions<string>
+{
+    type?:string;
+}
+
+export class TerraFormFieldTextArea extends TerraFormFieldBase<string>
+{
+    public type:string;
+
+    constructor(key:string, options:TerraFormFieldInputTextOptions = {})
+    {
+        super(key, TerraControlTypeEnum.INPUT_TEXT_AREA, options);
+
+        this.type = options['type'] || '';
+    }
+}
