fd7412efae1ce46446548ccea2c2c28267dbc9dc
Maximilian Röll <maximilian.roell@plentymarkets.com>
2018-02-15 10:51:10 +0100

    UPDATED: documentation-changelog file UPDATED: Terra portlet 	ADDED: 		* descriptions for inputs and functions 		* updated displayed examples (all new functions form terra-portlet)

--- src/app/components/layouts/portlet/example/terra-portlet.component.example.ts
+++ src/app/components/layouts/portlet/example/terra-portlet.component.example.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import { TerraButtonInterface } from '../../../buttons/button/data/terra-button.interface';
 
 @Component({
     selector: 'terra-portlet-example',
     template: require('./terra-portlet.component.example.html')
 })
-export class TerraPortletComponentExample
+export class TerraPortletComponentExample implements OnInit
 {
+    private _portletButtonList: Array<TerraButtonInterface> = [];
+
+    private buttonClickFunction()
+    {
+        alert('button clicked');
+    }
+
+    ngOnInit()
+    {
+        this._portletButtonList.push(
+            {
+                icon: 'icon-delete',
+                isSecondary: true,
+                clickFunction: ():void=> this.buttonClickFunction()
+            },
+            {
+                icon: 'icon-edit',
+                clickFunction: ():void=> this.buttonClickFunction()
+            })
+    }
 }
