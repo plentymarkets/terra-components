fb8b2d0e18688299ff160c3df5d6d013aff2372b
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-02-28 17:41:45 +0100

    added enum code refactoring

--- src/app/components/indicator/terra-indicator.component.ts
+++ src/app/components/indicator/terra-indicator.component.ts
@@ -1,25 +1,30 @@
 import {
     Component,
-    Input,
-    OnInit
+    Input
 } from '@angular/core';
+
+export enum TerraIndicatorLabelType
+{
+    DEFAULT = 'default',
+    PRIMARY = 'primary',
+    SUCCESS = 'success',
+    INFO = 'info',
+    WARNING = 'warning',
+    DANGER = 'danger',
+}
 
 @Component({
     selector: 'terra-indicator',
     styles:   [require('./terra-indicator.component.scss')],
     template: require('./terra-indicator.component.html')
 })
-export class TerraIndicatorComponent implements OnInit
+export class TerraIndicatorComponent
 {
     @Input() inputLabel:string;
     @Input() inputType:string;
 
     constructor()
     {
-        this.inputType = 'default';
-    }
-
-    ngOnInit()
-    {
+        this.inputType = TerraIndicatorLabelType.DEFAULT;
     }
 }
