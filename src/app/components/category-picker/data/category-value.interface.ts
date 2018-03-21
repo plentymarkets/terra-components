f5b4041a5f7da745094a29f0c84be1b215a055a5
Tim Wieder <tim.wieder@plentymarkets.com>
2018-03-21 16:28:23 +0100

    feature/category_picker - added category picker

---
+++ src/app/components/category-picker/data/category-value.interface.ts
@@ -0,0 +1,10 @@
+export interface CategoryValueInterface
+{
+    id:number;
+    isActive:boolean;
+    isOpen:boolean;
+    isVisible:boolean;
+    name:string;
+    tooltip:string;
+    tooltipPlacement:string;
+}
