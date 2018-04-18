644babcc3cc104808b77f533d600816eb9ccc00e
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-04-18 09:57:19 +0200

    extract l10n config to separate file

--- src/app/terra-components.module.ts
+++ src/app/terra-components.module.ts
@@ -18,10 +18,7 @@ import {
     TooltipModule
 } from 'ngx-bootstrap';
 import {
-    L10nConfig,
     L10nLoader,
-    ProviderType,
-    StorageStrategy,
     TranslationModule
 } from 'angular-l10n';
 import { QuillModule } from 'ngx-quill';
@@ -139,70 +136,7 @@ import { TerraNoteComponentExample } from './components/note/example/terra-note.
 import { TerraNodeTreeComponentExample } from './components/tree/node-tree/example/terra-node-tree.component.example';
 import { TerraMultiCheckBoxComponentExample } from './components/forms/multi-check-box/example/terra-multi-check-box.component.example';
 import { TerraCategoryPickerComponent } from './components/category-picker/terra-category-picker.component';
-
-export const l10nConfig:L10nConfig = getL10nConfig();
-
-function getL10nConfig():L10nConfig
-{
-    let langInLocalStorage:string = localStorage.getItem('plentymarkets_lang_');
-    let lang:string = null;
-
-    if(langInLocalStorage !== null)
-    {
-        lang = langInLocalStorage;
-    }
-    else
-    {
-        lang = navigator.language.slice(0, 2).toLocaleLowerCase();
-
-        if(lang !== 'de' && lang !== 'en')
-        {
-            lang = 'en';
-        }
-
-        localStorage.setItem('plentymarkets_lang_', lang);
-    }
-
-    let prefix:string = null;
-
-    // Definitions for i18n
-    if(process.env.ENV === 'production')
-    {
-        prefix = 'app/assets/lang/locale-';
-    }
-    else
-    {
-        prefix = 'src/app/assets/lang/locale-';
-    }
-
-    return {
-        locale:      {
-            languages: [
-                {
-                    code: 'en',
-                    dir:  'ltr'
-                },
-                {
-                    code: 'de',
-                    dir:  'ltr'
-                }
-            ],
-            language:  lang,
-            storage:   StorageStrategy.Cookie
-        },
-        translation: {
-            providers:            [
-                {
-                    type:   ProviderType.Static,
-                    prefix: prefix
-                }
-            ],
-            caching:              true,
-            composedKeySeparator: '.',
-            i18nPlural:           false
-        }
-    };
-}
+import { l10nConfig } from './translation/l10n.config';
 
 function createCompiler(compilerFactory:CompilerFactory):Compiler
 {
