6b3f647257f268fe0f864485016f4bdd8255652b
Tim Wieder <tim.wieder@plentymarkets.com>
2018-05-09 10:11:12 +0200

    fix/helper_for_downloads - download helper implemented

---
+++ src/app/helpers/download.helper.ts
@@ -0,0 +1,13 @@
+export class TerraDownloadHelper
+{
+    public static downloadFile(object:any, filename:string):void
+    {
+        let fileURL:string = URL.createObjectURL(object);
+
+        let link:HTMLAnchorElement = document.createElement('a');
+        link.href = fileURL;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+    }
+}
