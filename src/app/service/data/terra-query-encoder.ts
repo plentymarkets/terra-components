68d6319fda0f752f63b7d8c61a0f5241c8d9e8e9
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-07-11 11:54:02 +0200

    update description of terra-query-encoder

--- src/app/service/data/terra-query-encoder.ts
+++ src/app/service/data/terra-query-encoder.ts
@@ -2,7 +2,7 @@ import { QueryEncoder } from '@angular/http';
 
 /**
  * @author pweyrich
- * @description Re-implementation of the native QueryEncoder.. It uses javascript's encodeURIComponent-Method to encode query params.
+ * @description Custom implementation of the native QueryEncoder.. It uses javascript's encodeURIComponent-Method to encode query params.
  * Should be useless when using angular's new HttpClient along with HttpParams.
  */
 export class TerraQueryEncoder extends QueryEncoder
