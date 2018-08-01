e5ff600e25f7e3ac17c7364e2d5ca9f2a814dded
Christian Moll <christian.moll@plentymarkets.com>
2018-08-01 08:28:49 +0200

    removed unnecessary constructor

--- src/app/components/layouts/column-container/config/terra-two-columns.config.ts
+++ src/app/components/layouts/column-container/config/terra-two-columns.config.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
-import { TerraTwoColumnsContainerDirective } from '../terra-two-columns-container.directive';
 import { TerraTwoColumnsContainerComponent } from '../terra-two-columns-container.component';
 
 @Injectable()
 export class TerraTwoColumnsConfig
 {
     public component:TerraTwoColumnsContainerComponent;
-
-    constructor()
-    {
-    }
 }
