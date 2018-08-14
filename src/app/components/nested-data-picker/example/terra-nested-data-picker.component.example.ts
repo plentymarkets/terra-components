cc432a4e1b5a808941b478bfc3464e62f0e00b54
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2018-08-14 16:13:13 +0200

    FIX access modifiers

--- src/app/components/nested-data-picker/example/terra-nested-data-picker.component.example.ts
+++ src/app/components/nested-data-picker/example/terra-nested-data-picker.component.example.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { NestedPickerExampleService } from '../example/service/terra-nested-data-picker.service.example';
-import { TerraNestedDataPickerBaseService } from '../service/terra-nested-data-picker-base.service';
 
 @Component({
-    selector: 'terra-nested-data-picker-example',
-    styles:   [require('./terra-nested-data-picker.component.example.scss')],
-    template: require('./terra-nested-data-picker.component.example.html'),
+    selector:  'terra-nested-data-picker-example',
+    styles:    [require('./terra-nested-data-picker.component.example.scss')],
+    template:  require('./terra-nested-data-picker.component.example.html'),
     providers: [NestedPickerExampleService]
 })
 export class TerraNestedDataPickerComponentExample
 {
-    constructor(private terraService:NestedPickerExampleService)
+    constructor(protected terraService:NestedPickerExampleService)
     {
     }
 }
