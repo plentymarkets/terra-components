ace836676b7df3d49c770c5faca20740f0dcffb5
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-09-24 19:13:17 +0200

    redesign

--- src/app/components/info/terra-info.component.ts
+++ src/app/components/info/terra-info.component.ts
@@ -1,88 +1,27 @@
 import {
-    AfterViewInit,
     Component,
-    Input
+    Input,
 } from '@angular/core';
-import Coordinates = JQuery.Coordinates;
 import { TerraPlacementEnum } from '../../helpers/enums/terra-placement.enum';
-
-export interface Position
-{
-    width:number;
-    height:number;
-    position:Coordinates;
-}
 
 @Component({
     selector: 'terra-info',
     styles:   [require('./terra-info.component.scss')],
     template: require('./terra-info.component.html')
 })
-export class TerraInfoComponent implements AfterViewInit
+export class TerraInfoComponent
 {
-    @Input() inputTextPlacement:TerraPlacementEnum;
-    @Input() inputIsDisabled:boolean;
-    @Input() inputText:string;
-    @Input() inputReferenceId:string;
+    @Input()
+    public textPlacement:TerraPlacementEnum;
 
-    private _locationCss:any = {
-        width:  0,
-        height: 0,
-        left:   0,
-        top:    0
-    };
+    @Input()
+    public isDisabled:boolean;
+
+    @Input()
+    public text:string;
 
     constructor()
     {
-        this.inputTextPlacement = TerraPlacementEnum.LEFT;
-    }
-
-    public ngAfterViewInit():void
-    {
-        let referenceElemPosition:Position = this.getReferenceElemPosition();
-
-        console.log(referenceElemPosition.width + '  ' + referenceElemPosition.height + '  ' + referenceElemPosition.position.top + '  ' + referenceElemPosition.position.left);
-        this.setTextPosition(referenceElemPosition);
-    }
-
-    private setTextPosition(position:Position):void
-    {
-        //$('info-text').css('width', width);
-        //$('info-text').css('height', height);
-        //$('info-text').css('top', position.top);
-        //$('info-text').css('left', position.left);
-
-        this._locationCss = {
-            width:  position.width,
-            height: position.height,
-            left:   position.position.left,
-            top:    position.position.top
-        };
-    }
-
-    private getReferenceElemPosition():Position
-    {
-        let referenceElem = $('terra-info').find(this.inputReferenceId);
-
-        let position:Position;
-
-        position = {
-            width:    referenceElem.children().width(),
-            height:   referenceElem.children().height(),
-            position: referenceElem.children().position()
-        };
-
-        return position;
-    }
-
-    private updateTextPosition():void
-    {
-        let referenceElemPosition:Position = this.getReferenceElemPosition();
-        this._locationCss = {
-            width:  referenceElemPosition.width,
-            height: referenceElemPosition.height,
-            left:   referenceElemPosition.position.left,
-            top:    referenceElemPosition.position.top
-        };
+        this.textPlacement = TerraPlacementEnum.TOP;
     }
 }
