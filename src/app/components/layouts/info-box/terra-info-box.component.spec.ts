387a2d93097ac1d5407da747c229508f548f7b50
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-09-25 17:20:12 +0200

    added expectations

--- src/app/components/layouts/info-box/terra-info-box.component.spec.ts
+++ src/app/components/layouts/info-box/terra-info-box.component.spec.ts
@@ -17,21 +17,9 @@ describe('Component: TerraInfoBoxComponent', () =>
         }
     );
 
-    beforeEach(() =>
-        {
-            component.inputTagList = tagList;
-        }
-    );
-
     afterEach(() =>
         {
             component.inputTagList = [];
-        }
-    );
-
-    beforeEach(() =>
-        {
-            component.inputId = 5;
         }
     );
 
@@ -46,10 +34,50 @@ describe('Component: TerraInfoBoxComponent', () =>
         expect(component).toBeTruthy();
     });
 
-    it('should getter \'hasFooter\' return true', () =>
+    it('should getter \'hasFooter\' return true if \'inputTagList\' has entries', () =>
         {
+            component.inputTagList = tagList;
             expect(component.hasFooter).toBe(true);
-            expect(component.inputId).toBe(5);
         }
     );
+
+    it('should getter \'hasFooter\' return true if \'inputId\' has a value', () =>
+        {
+            component.inputId = 5;
+            expect(component.hasFooter).toBe(true);
+            expect(component.inputId).toEqual(jasmine.any(Number));
+            expect(component.inputId).toBeGreaterThan(0);
+        }
+    );
+
+    it('should getter \'hasFooter\' return false if \'inputTagList\' or \'inputId\' are null', () =>
+        {
+            component.inputTagList = null;
+            component.inputId = null;
+            expect(component.hasFooter).toBe(false);
+        }
+    );
+
+    it('should getter \'hasFooter\' return false if \'inputTagList\' is empty', () =>
+        {
+            component.inputTagList = [];
+            expect(component.hasFooter).toBe(false);
+        }
+    );
+
+    // it('passes if subject is true', () =>
+    // {
+    //     expect(true).toBeTruthy();
+    //     expect(false).not.toBeTruthy();
+    // });
+    //
+    // it('passes if subject is a non empty string', () =>
+    // {
+    //     expect('Should pass').toBeTruthy();
+    // });
+    //
+    // it('passes if subject is a number not equal 0', function():void
+    // {
+    //     expect(1).toBeTruthy();
+    // });
 });
