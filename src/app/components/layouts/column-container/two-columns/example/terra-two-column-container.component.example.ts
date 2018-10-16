730a6781cbf733feac1a401e160a0d40783a4002
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-10-16 09:06:12 +0200

    example

---
+++ src/app/components/layouts/column-container/two-columns/example/terra-two-column-container.component.example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'terra-2-col-example',
+    template: require('./terra-two-column-container.component.example.html')
+})
+
+export class TerraTwoColumnContainerComponentExample
+{
+    protected leftColumnWidth:number = 4;
+}
