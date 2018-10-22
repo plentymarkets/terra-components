3629808ef4f4fcf6a26d830711f91f18957f0c6f
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-10-22 16:36:12 +0200

    WIP

---
+++ src/app/components/tables/data-table/terra-data-table.component.spec.ts
@@ -0,0 +1,11 @@
+import { TerraDataTableComponent } from '../../../..';
+
+describe('TerraDataTableComponent', () =>
+{
+    let component:TerraDataTableComponent<any, any>; // TODO T and P should be defined
+
+    // it('should create', () =>
+    // {
+    //     expect(component).toBeTruthy();
+    // });
+});
