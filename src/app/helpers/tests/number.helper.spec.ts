c0f09de45a903ed7852205466149926035211335
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-11-08 17:47:23 +0100

    WIP

---
+++ src/app/helpers/tests/number.helper.spec.ts
@@ -0,0 +1,9 @@
+import { NumberHelper } from '../number.helper';
+
+describe('Helper: NumberHelper', () =>
+{
+    it('should round a given number', () =>
+    {
+        // expect(NumberHelper.round).toBeDefined();
+    });
+});
