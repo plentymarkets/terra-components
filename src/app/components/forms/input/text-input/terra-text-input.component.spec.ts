02842b98ae3788714c04c211ce92cd80da0ff86a
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-11-12 09:14:41 +0100

    wip set up for unit tests of TerraTextInputComponent

--- src/app/components/forms/input/text-input/terra-text-input.component.spec.ts
+++ src/app/components/forms/input/text-input/terra-text-input.component.spec.ts
@@ -1,11 +1,49 @@
-// /* tslint:disable:no-unused-variable */
-//
-// import { TestBed, async } from '@angular/core/testing';
-// import { PlentyTextInputComponent } from './plenty-text-input.component';
-//
-// describe('Component: TerraTextInputComponent', () => {
-//   it('should create an instance', () => {
-//     let component = new PlentyTextInputComponent();
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { TerraTextInputComponent } from './terra-text-input.component';
+import {
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
+import { TooltipModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { LocalizationModule } from 'angular-l10n';
+import { l10nConfig } from '../../../../translation/l10n.config';
+import { TerraLabelTooltipDirective } from '../../../../helpers/terra-label-tooltip.directive';
+import { HttpClientModule } from '@angular/common/http';
+
+fdescribe('Component: TerraTextInputComponent', () =>
+{
+    let component:TerraTextInputComponent;
+    let fixture:ComponentFixture<TerraTextInputComponent>;
+
+    beforeEach(() =>
+    {
+        TestBed.configureTestingModule(
+            {
+                declarations: [TerraTextInputComponent, TerraLabelTooltipDirective],
+                imports: [
+                    HttpClientModule,
+                    TooltipModule.forRoot(),
+                    FormsModule,
+                    LocalizationModule.forRoot(l10nConfig)
+                ]
+            }
+        ).compileComponents();
+    });
+
+    beforeEach(() =>
+    {
+        fixture = TestBed.createComponent(TerraTextInputComponent);
+        component = fixture.componentInstance;
+    });
+
+    it('should create an instance', () =>
+    {
+        expect(component).toBeTruthy();
+    });
+
+    //afterAll(() =>
+    //{
+    //    fixture.destroy();
+    //    TestBed.resetTestingModule();
+    //});
+});
