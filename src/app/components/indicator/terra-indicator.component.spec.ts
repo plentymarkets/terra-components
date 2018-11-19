ef44f6e6cf9059b88d0ff3b232e67cc564249c28
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-11-19 17:53:35 +0100

    add tests

--- src/app/components/indicator/terra-indicator.component.spec.ts
+++ src/app/components/indicator/terra-indicator.component.spec.ts
@@ -1,10 +1,32 @@
-/* tslint:disable:no-unused-variable */
+import {
+    TerraIndicatorComponent,
+    TerraIndicatorLabelTypeEnum
+} from './terra-indicator.component';
+import {
+    async,
+    ComponentFixture,
+    TestBed
+} from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-import { TerraIndicatorComponent } from './terra-indicator.component';
-
-describe('Component: TerraIndicatorComponent', () =>
+fdescribe('Component: TerraIndicatorComponent', () =>
 {
-    let component:any = new TerraIndicatorComponent();
+    let component:TerraIndicatorComponent;
+    let fixture:ComponentFixture<TerraIndicatorComponent>;
+
+    beforeEach(async(() =>
+    {
+        TestBed.configureTestingModule({
+            declarations: [TerraIndicatorComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() =>
+    {
+        fixture = TestBed.createComponent(TerraIndicatorComponent);
+        component = fixture.componentInstance;
+    });
 
     it('should create an instance', () =>
     {
@@ -14,6 +36,21 @@ describe('Component: TerraIndicatorComponent', () =>
     it('should inputType be defined', () =>
     {
         expect(component.inputType).toBeDefined();
-        expect(component.inputType).toEqual('default');
+        expect(component.inputType).toEqual(TerraIndicatorLabelTypeEnum.default);
+    });
+
+    it('should #inputType and #inputLabel be set properly', () =>
+    {
+        let debugElement:DebugElement = fixture.debugElement;
+        let indicatorDebugElement:DebugElement = debugElement.query(By.css('span'));
+
+        let expectedLabel:string = 'attention';
+        component.inputLabel = expectedLabel;
+        component.inputType = TerraIndicatorLabelTypeEnum.warning;
+
+        fixture.detectChanges();
+
+        expect(indicatorDebugElement.properties['className']).toEqual('label label-warning');
+        expect(indicatorDebugElement.nativeElement.textContent.trim()).toBe(expectedLabel);
     });
 });
