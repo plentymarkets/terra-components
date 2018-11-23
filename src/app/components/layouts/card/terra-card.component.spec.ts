f1ea6c5e1e044cb1d41ee8da3b7f496e60a4e4f6
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-11-23 17:20:44 +0100

    refactored test structure

--- src/app/components/layouts/card/terra-card.component.spec.ts
+++ src/app/components/layouts/card/terra-card.component.spec.ts
@@ -60,6 +60,11 @@ describe('TerraCardComponent', () =>
         cardComponent.inputImagePath = null;
     });
 
+    describe('footer content', footer);
+    describe('header content', header);
+    describe('block content', block);
+    describe('image', image);
+
     it('should create', () =>
     {
         expect(component).toBeTruthy();
@@ -72,114 +77,122 @@ describe('TerraCardComponent', () =>
         expect(cardComponent.inputPlaceholderIcon).toBeUndefined();
     });
 
-    // footer
-    it('should div-element for footer be shown if content is given', () =>
+    function footer():void
     {
-        let footerElement:DebugElement = debugElement.query(By.css('div.card-footer'));
-        let ngContentElement:DebugElement = footerElement.query(By.css('p'));
-        let footerContentElement:HTMLElement = ngContentElement.nativeElement;
-        expect(footerElement).toBeTruthy();
-        expect(cardComponent.viewChildFooter).toBeTruthy();
-        expect(cardComponent.viewChildFooter.nativeElement.children.length).toBeGreaterThan(0);
-        expect(footerContentElement.innerHTML).toEqual('card footer');
-        expect(footerElement.nativeElement.hidden).toBe(false);
-    });
+        it('should div-element for footer be shown if content is given', () =>
+        {
+            let footerElement:DebugElement = debugElement.query(By.css('div.card-footer'));
+            let ngContentElement:DebugElement = footerElement.query(By.css('p'));
+            let footerContentElement:HTMLElement = ngContentElement.nativeElement;
+            expect(footerElement).toBeTruthy();
+            expect(cardComponent.viewChildFooter).toBeTruthy();
+            expect(cardComponent.viewChildFooter.nativeElement.children.length).toBeGreaterThan(0);
+            expect(footerContentElement.innerHTML).toEqual('card footer');
+            expect(footerElement.nativeElement.hidden).toBe(false);
+        });
 
-    it('should set class selected depending on #inputIsSelected', () =>
+        it('should set class selected depending on #inputIsSelected', () =>
+        {
+            let footerElement:DebugElement = debugElement.query(By.css('div.card-footer'));
+            let terraCardElement:DebugElement = debugElement.query(By.css('div.terra-card'));
+            expect(footerElement.classes['selected']).toBeFalsy();
+            expect(terraCardElement.classes['selected']).toBeFalsy();
+
+            cardComponent.inputIsSelected = true;
+            fixture.detectChanges();
+            expect(footerElement.classes['selected']).toBeTruthy();
+            expect(terraCardElement.classes['selected']).toBeTruthy();
+        });
+    }
+
+    function header():void
     {
-        let footerElement:DebugElement = debugElement.query(By.css('div.card-footer'));
-        let terraCardElement:DebugElement = debugElement.query(By.css('div.terra-card'));
-        expect(footerElement.classes['selected']).toBeFalsy();
-        expect(terraCardElement.classes['selected']).toBeFalsy();
+        it('should show div-element for header if content is given', () =>
+        {
+            let headerElement:DebugElement = debugElement.query(By.css('div.card-header'));
+            let ngContentElement:DebugElement = headerElement.query(By.css('p'));
+            let headerContentElement:HTMLElement = ngContentElement.nativeElement;
+            expect(headerElement).toBeTruthy();
+            expect(cardComponent.viewChildHeader).toBeTruthy();
+            expect(cardComponent.viewChildHeader.nativeElement.children.length).toBeGreaterThan(0);
+            expect(headerContentElement.innerHTML).toEqual('card header');
+            expect(headerElement.nativeElement.hidden).toBe(false);
+        });
+    }
 
-        cardComponent.inputIsSelected = true;
-        fixture.detectChanges();
-        expect(footerElement.classes['selected']).toBeTruthy();
-        expect(terraCardElement.classes['selected']).toBeTruthy();
-    });
-
-    // header
-    it('should show div-element for header if content is given', () =>
+    function block():void
     {
-        let headerElement:DebugElement = debugElement.query(By.css('div.card-header'));
-        let ngContentElement:DebugElement = headerElement.query(By.css('p'));
-        let headerContentElement:HTMLElement = ngContentElement.nativeElement;
-        expect(headerElement).toBeTruthy();
-        expect(cardComponent.viewChildHeader).toBeTruthy();
-        expect(cardComponent.viewChildHeader.nativeElement.children.length).toBeGreaterThan(0);
-        expect(headerContentElement.innerHTML).toEqual('card header');
-        expect(headerElement.nativeElement.hidden).toBe(false);
-    });
+        it('should show div-element for content if content is given', () =>
+        {
+            let contentElement:DebugElement = debugElement.query(By.css('div.card-block'));
+            let ngContentElement:DebugElement = contentElement.query(By.css('p'));
+            let blockContentElement:HTMLElement = ngContentElement.nativeElement;
+            expect(contentElement).toBeTruthy();
+            expect(blockContentElement.innerHTML).toEqual('card content');
+        });
+    }
 
-    // block
-    it('should show div-element for content if content is given', () =>
+    function image():void
     {
-        let contentElement:DebugElement = debugElement.query(By.css('div.card-block'));
-        let ngContentElement:DebugElement = contentElement.query(By.css('p'));
-        let blockContentElement:HTMLElement = ngContentElement.nativeElement;
-        expect(contentElement).toBeTruthy();
-        expect(blockContentElement.innerHTML).toEqual('card content');
-    });
+        it('should show image if #inputImagePath is set', () =>
+        {
+            cardComponent.inputImagePath = '';
+            fixture.detectChanges();
+            let imageElement:DebugElement;
+            imageElement = debugElement.query(By.css('div.terra-card-image'));
 
-    // image
-    it('should show image if #inputImagePath is set', () =>
-    {
-        cardComponent.inputImagePath = '';
-        fixture.detectChanges();
-        let imageElement:DebugElement;
-        imageElement = debugElement.query(By.css('div.terra-card-image'));
+            expect(imageElement).toBeFalsy();
 
-        expect(imageElement).toBeFalsy();
+            cardComponent.inputImagePath = expectedImagePath;
+            fixture.detectChanges();
+            imageElement = debugElement.query(By.css('div.terra-card-image'));
+            expect(imageElement).toBeTruthy();
+        });
 
-        cardComponent.inputImagePath = expectedImagePath;
-        fixture.detectChanges();
-        imageElement = debugElement.query(By.css('div.terra-card-image'));
-        expect(imageElement).toBeTruthy();
-    });
+        it('should show icon if #inputPlaceholderIcon is set', () =>
+        {
+            cardComponent.inputPlaceholderIcon = expectedIcon;
+            fixture.detectChanges();
+            let iconElement:DebugElement = debugElement.query(By.css('div.terra-card-placeholder'));
 
-    it('should show icon if #inputPlaceholderIcon is set', () =>
-    {
-        cardComponent.inputPlaceholderIcon = expectedIcon;
-        fixture.detectChanges();
-        let iconElement:DebugElement = debugElement.query(By.css('div.terra-card-placeholder'));
+            expect(iconElement).toBeTruthy();
+            expect(cardComponent.inputPlaceholderIcon).toEqual(expectedIcon);
+        });
 
-        expect(iconElement).toBeTruthy();
-        expect(cardComponent.inputPlaceholderIcon).toEqual(expectedIcon);
-    });
+        it('should show no image and no icon if #inputPlaceholderIcon and #inputPlaceholderIcon are not set', () =>
+        {
+            fixture.detectChanges();
+            let imageElement:DebugElement;
+            let iconElement:DebugElement;
 
-    it('should show no image and no icon if #inputPlaceholderIcon and #inputPlaceholderIcon are not set', () =>
-    {
-        fixture.detectChanges();
-        let imageElement:DebugElement;
-        let iconElement:DebugElement;
+            iconElement = debugElement.query(By.css('div.terra-card-placeholder'));
+            imageElement = debugElement.query(By.css('div.terra-card-image'));
+            expect(iconElement).toBeFalsy();
+            expect(imageElement).toBeFalsy();
+        });
 
-        iconElement = debugElement.query(By.css('div.terra-card-placeholder'));
-        imageElement = debugElement.query(By.css('div.terra-card-image'));
-        expect(iconElement).toBeFalsy();
-        expect(imageElement).toBeFalsy();
-    });
+        it('should show only the image if both #inputPlaceholderIcon and #inputPlaceholderIcon are set', () =>
+        {
+            cardComponent.inputPlaceholderIcon = expectedIcon;
+            cardComponent.inputImagePath = expectedImagePath;
+            fixture.detectChanges();
+            let imageElement:DebugElement;
+            let iconElement:DebugElement;
 
-    it('should show only the image if both #inputPlaceholderIcon and #inputPlaceholderIcon are set', () =>
-    {
-        cardComponent.inputPlaceholderIcon = expectedIcon;
-        cardComponent.inputImagePath = expectedImagePath;
-        fixture.detectChanges();
-        let imageElement:DebugElement;
-        let iconElement:DebugElement;
+            imageElement = debugElement.query(By.css('div.terra-card-image'));
+            iconElement = debugElement.query(By.css('div.terra-card-placeholder'));
+            expect(imageElement).toBeTruthy();
+            expect(iconElement).toBeFalsy();
+        });
 
-        imageElement = debugElement.query(By.css('div.terra-card-image'));
-        iconElement = debugElement.query(By.css('div.terra-card-placeholder'));
-        expect(imageElement).toBeTruthy();
-        expect(iconElement).toBeFalsy();
-    });
-
-    it('should set style.background-image if an image is given', () =>
-    {
-        let backgroundImageElement:DebugElement;
-        cardComponent.inputImagePath = expectedImagePath;
-        fixture.detectChanges();
-        backgroundImageElement = fixture.debugElement.query(By.css('div.terra-card-image'));
-        expect(backgroundImageElement).toBeTruthy();
-        expect(backgroundImageElement.styles['background-image']).toBe(`url(${expectedImagePath})`);
-    });
+        it('should set style.background-image if an image is given', () =>
+        {
+            let backgroundImageElement:DebugElement;
+            cardComponent.inputImagePath = expectedImagePath;
+            fixture.detectChanges();
+            backgroundImageElement = fixture.debugElement.query(By.css('div.terra-card-image'));
+            expect(backgroundImageElement).toBeTruthy();
+            expect(backgroundImageElement.styles['background-image']).toBe(`url(${expectedImagePath})`);
+        });
+    }
 });
