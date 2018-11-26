fc81e032f31652a2f71406cca10df469af863bcd
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-11-26 10:12:44 +0100

    improve testing setup to support zone testing (async and fakeAsync)

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,9 @@
 import 'core-js'; // ES6 + reflect-metadata
+
 // zone.js
 import 'zone.js/dist/zone';
-import 'zone.js/dist/proxy';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/jasmine-patch';
+(window as any)['__zone_symbol__fakeAsyncPatchLock'] = true;
+import 'zone.js/dist/zone-testing';
 
 // TestBed initialization
 import { TestBed } from '@angular/core/testing';
