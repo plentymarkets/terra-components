48203652325935a170a6a7558beeac39ed13ad09
Michael Kunze <michael.kunze@plentymarkets.com>
2018-11-26 10:46:31 +0100

    imports

--- src/app/components/layouts/tag/terra-tag.component.spec.ts
+++ src/app/components/layouts/tag/terra-tag.component.spec.ts
@@ -4,7 +4,7 @@ import {
     TestBed
 } from '@angular/core/testing';
 
-import { TerraTagComponent } from '../../../..';
+import { TerraTagComponent } from './terra-tag.component';
 import {
     tagOne,
     tagTwo
