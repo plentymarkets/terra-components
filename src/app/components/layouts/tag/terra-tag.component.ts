262e4f351ed0ecbfc1a568d9a222f4594e974bb5
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-11-27 08:50:03 +0100

    remove deprecation warning

--- src/app/components/layouts/tag/terra-tag.component.ts
+++ src/app/components/layouts/tag/terra-tag.component.ts
@@ -26,7 +26,6 @@ export class TerraTagComponent implements OnChanges
 
     /**
      * Caption of the tag. If given, this is always shown.
-     * @deprecated use 'name' instead
      */
     @Input()
     public inputBadge:string;
