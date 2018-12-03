cd8ffafe9df46738a85462b71f119408e579da33
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-12-03 11:34:04 +0100

    remove focus on TerraSliderComponent test suite

--- src/app/components/forms/slider/terra-slider.component.spec.ts
+++ src/app/components/forms/slider/terra-slider.component.spec.ts
@@ -9,7 +9,7 @@ import { By } from '@angular/platform-browser';
 import { SimpleChange } from '@angular/core';
 import Spy = jasmine.Spy;
 
-fdescribe(`TerraSliderComponent`, () =>
+describe(`TerraSliderComponent`, () =>
 {
     let component:TerraSliderComponent;
     let fixture:ComponentFixture<TerraSliderComponent>;
