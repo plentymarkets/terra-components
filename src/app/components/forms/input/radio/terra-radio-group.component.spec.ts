f932a81a0334787efb7a4286b3a1404c637ebebf
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-12-04 16:43:29 +0100

    using the example to run the tests with a host

--- src/app/components/forms/input/radio/terra-radio-group.component.spec.ts
+++ src/app/components/forms/input/radio/terra-radio-group.component.spec.ts
@@ -6,48 +6,62 @@ import {
 import { TerraRadioInputComponent } from './terra-radio-input.component';
 import { TerraRadioGroupComponent } from './terra-radio-group.component';
 import Spy = jasmine.Spy;
+import { TerraRadioGroupComponentExample } from './example/terra-radio-group.component.example';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 fdescribe(`TerraRadioGroupComponent:`, () =>
 {
-    let component:TerraRadioGroupComponent;
-    let fixture:ComponentFixture<TerraRadioGroupComponent>;
+    let component:TerraRadioGroupComponentExample;
+    let fixture:ComponentFixture<TerraRadioGroupComponentExample>;
+    let radioGroupComponent:TerraRadioGroupComponent;
     beforeEach(async(() =>
     {
         TestBed.configureTestingModule({
-            declarations: [TerraRadioInputComponent, TerraRadioGroupComponent]
+            declarations: [TerraRadioInputComponent, TerraRadioGroupComponent, TerraRadioGroupComponentExample],
+            imports: [FormsModule]
         }).compileComponents();
     }));
 
     beforeEach(() =>
     {
-        fixture = TestBed.createComponent(TerraRadioGroupComponent);
+        fixture = TestBed.createComponent(TerraRadioGroupComponentExample);
         component = fixture.componentInstance;
+        radioGroupComponent = fixture.debugElement.query(By.css('terra-radio-group')).componentInstance;
     });
 
     it(`should create`, () =>
     {
         expect(component).toBeTruthy();
+        expect(radioGroupComponent).toBeTruthy();
     });
 
     it(`should initialize its inputs`, () =>
     {
-        expect(component.name).toBeUndefined();
-        expect(component.legend).toBeUndefined();
+        expect(radioGroupComponent.name).toBeUndefined();
+        expect(radioGroupComponent.legend).toBeUndefined();
+    });
+
+    it(`setting the #value should update the #value`, () =>
+    {
+        const testValue:any = {test: true};
+        radioGroupComponent.value = testValue;
+        expect(radioGroupComponent.value).toBe(testValue);
     });
 
     it(`#writeValue should update the #value of the radio group`, () =>
     {
         const testValue:any = 'check';
-        component.writeValue(testValue);
-        expect(component.value).toBe(testValue);
+        radioGroupComponent.writeValue(testValue);
+        expect(radioGroupComponent.value).toBe(testValue);
     });
 
     it(`should call the registered #changeCallback when setting the #value`, () =>
     {
-        const testValue:any = 'check';
+        const testValue:any = 1;
         let spy:Spy = jasmine.createSpy('spy');
-        component.registerOnChange(spy);
-        component.value = testValue;
+        radioGroupComponent.registerOnChange(spy);
+        radioGroupComponent.value = testValue;
         expect(spy).toHaveBeenCalledWith(testValue);
     });
 });
