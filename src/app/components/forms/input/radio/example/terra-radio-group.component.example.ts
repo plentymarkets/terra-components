c8f004a6d0f37a9ee2930f78f60a3e059a49cbbd
Daniel Trauf <daniel.trauf@plentymarkets.com>
2018-12-06 12:16:39 +0100

    Update src/app/components/forms/input/radio/example/terra-radio-group.component.example.ts
    Co-Authored-By: pweyrich <pascal.weyrich@plentymarkets.com>

--- src/app/components/forms/input/radio/example/terra-radio-group.component.example.ts
+++ src/app/components/forms/input/radio/example/terra-radio-group.component.example.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'terra-radio-group-component-example',
-    templateUrl: 'terra-radio-group.component.example.html'
+    template: require('terra-radio-group.component.example.html')
 })
 
 export class TerraRadioGroupComponentExample
