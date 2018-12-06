064860cb2a1703968b018adcdcd610e6cd85164a
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2018-12-06 12:17:47 +0100

    fix compilation error

--- src/app/components/forms/input/radio/example/terra-radio-group.component.example.ts
+++ src/app/components/forms/input/radio/example/terra-radio-group.component.example.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'terra-radio-group-component-example',
-    template: require('terra-radio-group.component.example.html')
+    template: require('./terra-radio-group.component.example.html')
 })
 
 export class TerraRadioGroupComponentExample
