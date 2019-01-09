4c0220ba3ba8aa96a056c90ebf461f8b6f09c6d8
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-01-09 14:00:24 +0100

    add selectBoxValues and checkBoxValues options

--- src/app/components/forms/dynamic-form/interfaces/terra-dynamic-form-field-options.interface.ts
+++ src/app/components/forms/dynamic-form/interfaces/terra-dynamic-form-field-options.interface.ts
@@ -1,7 +1,11 @@
 import { TerraDynamicFormFields } from './terra-dynamic-form.fields';
 import { TerraFormFieldBaseOptions } from '../data/terra-form-field-base';
+import { TerraSelectBoxValueInterface } from '../../select-box/data/terra-select-box.interface';
+import { TerraMultiCheckBoxValueInterface } from '../../multi-check-box/data/terra-multi-check-box-value.interface';
 
 export interface TerraDynamicFormFieldOptionsInterface extends TerraFormFieldBaseOptions<any>
 {
     containerEntries?:TerraDynamicFormFields;
+    selectBoxValues?:Array<TerraSelectBoxValueInterface>;
+    checkBoxValues?:Array<TerraMultiCheckBoxValueInterface>;
 }
