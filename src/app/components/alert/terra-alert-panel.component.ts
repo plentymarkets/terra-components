d411a009eb5abed3e6c9f8053fbb8a22792d53f3
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-01-14 11:29:16 +0100

    * fix issue when closing an alert via ui * clean up the listeners and subscriptions when TerraAlertPanelComponent is destroyed

--- src/app/components/alert/terra-alert-panel.component.ts
+++ src/app/components/alert/terra-alert-panel.component.ts
@@ -1,10 +1,12 @@
 import {
     Component,
+    OnDestroy,
     OnInit
 } from '@angular/core';
 import { TerraAlertComponent } from './terra-alert.component';
 import { TerraAlertInterface } from './data/terra-alert.interface';
 import { AlertService } from './alert.service';
+import { Subscription } from 'rxjs';
 
 /**
  * @author mkunze
@@ -17,25 +19,51 @@ import { AlertService } from './alert.service';
     ],
     template: require('./terra-alert-panel.component.html')
 })
-export class TerraAlertPanelComponent implements OnInit
+export class TerraAlertPanelComponent implements OnInit, OnDestroy
 {
     protected alerts:Array<TerraAlertInterface>;
     private alert:TerraAlertComponent = TerraAlertComponent.getInstance();
 
+    private addAlertSub:Subscription;
+    private closeAlertSub:Subscription;
+
+    private readonly addAlertListener:EventListener;
+    private readonly closeAlertListener:EventListener;
+
     constructor(private service:AlertService)
     {
         this.alerts = this.alert.alerts;
+
+        // init event listeners
+        this.addAlertListener = (event:CustomEvent<TerraAlertInterface>):void => this.addAlert(event.detail);
+        this.closeAlertListener = (event:CustomEvent<string>):void => this.closeAlert(event.detail);
     }
 
     public ngOnInit():void
     {
         // listen to the EventEmitters of the service
-        this.service.addAlert.subscribe((alert:TerraAlertInterface) => this.addAlert(alert));
-        this.service.closeAlert.subscribe((identifier:string) => this.closeAlert(identifier));
+        this.addAlertSub = this.service.addAlert.subscribe((alert:TerraAlertInterface) => this.addAlert(alert));
+        this.closeAlertSub = this.service.closeAlert.subscribe((identifier:string) => this.closeAlert(identifier));
 
-        // listen to events concerning alerts that are dispatched to the hosting window
-        window.addEventListener(this.service.addEvent, (event:CustomEvent<TerraAlertInterface>) => this.addAlert(event.detail));
-        window.addEventListener(this.service.closeEvent, (event:CustomEvent<string>) => this.closeAlert(event.detail));
+        // listen to events that concern alerts and are dispatched to the hosting window
+        window.addEventListener(this.service.addEvent, this.addAlertListener);
+        window.addEventListener(this.service.closeEvent, this.closeAlertListener);
+    }
+
+    public ngOnDestroy():void
+    {
+        // unsubscribe to the EventEmitters of the service
+        this.addAlertSub.unsubscribe();
+        this.closeAlertSub.unsubscribe();
+
+        // remove listeners from the hosting window
+        window.removeEventListener(this.service.addEvent, this.addAlertListener);
+        window.removeEventListener(this.service.closeEvent, this.closeAlertListener);
+    }
+
+    protected closeAlertByIndex(index:number):void
+    {
+        this.alert.closeAlert(index);
     }
 
     private addAlert(alert:TerraAlertInterface):void
