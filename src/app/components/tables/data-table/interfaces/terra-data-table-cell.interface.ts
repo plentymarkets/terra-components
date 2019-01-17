abc7e517f10fb8a8a9de1d2b7f04c9e79b133f63
Michael Frank <michael.frank@plentymarkets.com>
2019-01-17 12:03:26 +0100

    add type restriction

--- src/app/components/tables/data-table/interfaces/terra-data-table-cell.interface.ts
+++ src/app/components/tables/data-table/interfaces/terra-data-table-cell.interface.ts
@@ -1,9 +1,14 @@
+import { TerraButtonInterface } from '../../../buttons/button/data/terra-button.interface';
+import { TerraDataTableTextInterface } from './terra-data-table-text.interface';
+import { TerraTagInterface } from '../../../layouts/tag/data/terra-tag.interface';
+import { TerraHrefTypeInterface } from './terra-href-type.interface';
+
 /**
  * @author mkunze
  */
 export interface TerraDataTableCellInterface
 {
-    data:any;
+    data:string | number | TerraDataTableTextInterface | TerraHrefTypeInterface | Array<TerraButtonInterface> | Array<TerraTagInterface>;
     isHidden?:boolean;
     tooltipText?:string;
     tooltipPlacement?:string;
