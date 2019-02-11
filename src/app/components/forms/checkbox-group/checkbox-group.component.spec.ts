d649bd34f34e32f2d0907521b6c77452b8065346
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-02-11 14:31:06 +0100

    add tests for null value handling

--- src/app/components/forms/checkbox-group/checkbox-group.component.spec.ts
+++ src/app/components/forms/checkbox-group/checkbox-group.component.spec.ts
@@ -1,27 +1,41 @@
 import { CheckboxGroupComponent } from './checkbox-group.component';
 import {
     ComponentFixture,
-    fakeAsync,
     TestBed,
 } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import {
     TerraCheckboxComponent,
     TerraMultiCheckBoxComponent,
-    TerraTextInputComponent
+    TerraMultiCheckBoxValueInterface
 } from '../../../..';
-import Spy = jasmine.Spy;
 import { HttpClientModule } from '@angular/common/http';
 import { TooltipModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { LocalizationModule } from 'angular-l10n';
 import { l10nConfig } from '../../../translation/l10n.config';
+import Spy = jasmine.Spy;
 
 describe('Component: CheckboxGroupComponent', () =>
 {
     let component:CheckboxGroupComponent;
     let multicheckboxComponent:TerraMultiCheckBoxComponent;
     let fixture:ComponentFixture<CheckboxGroupComponent>;
+
+    const checkboxValues:Array<TerraMultiCheckBoxValueInterface> = [
+        {
+            caption: 'A',
+            value: 0
+        },
+        {
+            caption: 'B',
+            value: 1
+        },
+        {
+            caption: 'C',
+            value: 2
+        }
+    ];
 
     beforeEach(() =>
     {
@@ -53,23 +67,20 @@ describe('Component: CheckboxGroupComponent', () =>
         expect(component).toBeTruthy();
     });
 
+    it('should pass properties "isDisabled" and "name" to terra-multi-check-box component', () =>
+    {
+        component.isDisabled = true;
+        component.name = 'Custom name';
+        fixture.detectChanges();
+
+        expect(multicheckboxComponent.inputIsDisabled).toEqual(component.isDisabled);
+        expect(multicheckboxComponent.inputName).toEqual(component.name);
+    });
+
     it('should check all checkboxes included in value list', (done:() => void) =>
     {
         let writeValueSpy:Spy = spyOn(multicheckboxComponent, 'writeValue');
-        component.checkboxValues = [
-            {
-                caption: 'A',
-                value: 0
-            },
-            {
-                caption: 'B',
-                value: 1
-            },
-            {
-                caption: 'C',
-                value: 2
-            }
-        ];
+        component.checkboxValues = checkboxValues;
         component.writeValue([0, 2]);
         fixture.detectChanges();
 
@@ -97,13 +108,43 @@ describe('Component: CheckboxGroupComponent', () =>
         });
     });
 
-    it('should pass properties "isDisabled" and "name" to terra-multi-check-box component', () =>
+    it('should be able to check any checkbox also if `null` or `undefined` was set initially', () =>
     {
-        component.isDisabled = true;
-        component.name = 'Custom name';
+        // initialization
+        component.checkboxValues = checkboxValues;
+        component.writeValue(null);
         fixture.detectChanges();
 
-        expect(multicheckboxComponent.inputIsDisabled).toEqual(component.isDisabled);
-        expect(multicheckboxComponent.inputName).toEqual(component.name);
+        // register spy
+        let onChangeSpy:Spy = jasmine.createSpy('onChange');
+        component.registerOnChange(onChangeSpy);
+
+        // perform action -> change checkbox state
+        multicheckboxComponent.checkboxStateChanges.emit([{
+            caption: checkboxValues[1].caption,
+            value: checkboxValues[1].value,
+            selected: true
+        }]);
+        expect(onChangeSpy).toHaveBeenCalledWith([1]);
+    });
+
+    it('should call registered #onChangeCallback with `null` if all selections have been reset', () =>
+    {
+        // initialization
+        component.checkboxValues = checkboxValues;
+        component.writeValue([checkboxValues[1].value]);
+        fixture.detectChanges();
+
+        // register spy
+        let onChangeSpy:Spy = jasmine.createSpy('onChange');
+        component.registerOnChange(onChangeSpy);
+
+        // perform action -> change checkbox state
+        multicheckboxComponent.checkboxStateChanges.emit([{
+            caption: checkboxValues[1].caption,
+            value: checkboxValues[1].value,
+            selected: false
+        }]);
+        expect(onChangeSpy).toHaveBeenCalledWith(null);
     });
 });
