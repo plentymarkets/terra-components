6415958e8eb0a21ac55aa18ce68efbe7dbb3126e
Michael Kunze <michael.kunze@plentymarkets.com>
2019-02-19 08:22:46 +0100

    check if "more" is required

--- src/app/components/layouts/expandable-text/expandable-text.component.ts
+++ src/app/components/layouts/expandable-text/expandable-text.component.ts
@@ -1,17 +1,22 @@
 import {
+    AfterViewInit,
     Component,
     EventEmitter,
     Input,
     Output,
 } from '@angular/core';
 import { TranslationService } from 'angular-l10n';
+import { isNullOrUndefined } from 'util';
+
+
+let nextId:number = 0;
 
 @Component({
     selector: 'tc-expandable-text',
     styles:   [require('./expandable-text.component.scss')],
     template: require('./expandable-text.component.html')
 })
-export class ExpandableTextComponent
+export class ExpandableTextComponent implements AfterViewInit
 {
     @Input()
     public collapsed:boolean = true;
@@ -22,6 +27,12 @@ export class ExpandableTextComponent
     @Input()
     public visibleLines:number = 1;
 
+    /**
+     * @description a unique string identifier for the specific input instance.
+     */
+    protected id:string;
+    protected showCollapseButton:boolean = true;
+
     private readMoreText:string = this.translation.translate('expandable.showMore');
     private lessText:string = this.translation.translate('expandable.showLess');
 
@@ -30,11 +41,18 @@ export class ExpandableTextComponent
 
     constructor(private translation:TranslationService)
     {
+        // generate the id of the input instance
+        this.id = `tc-expandable-text_#${nextId++}`;
     }
 
     protected toggleCollapse():void
     {
         this.collapsed = !this.collapsed;
+    }
+
+    private isEllipsisActive(element:HTMLElement):boolean
+    {
+        return (element.offsetWidth < element.scrollWidth);
     }
 
     protected get collapseText():string
@@ -45,5 +63,22 @@ export class ExpandableTextComponent
         }
 
         return this.lessText;
+    }
+
+    private checkElementChildToShowButton():void
+    {
+        let expandable:HTMLDivElement = document.getElementById(this.id) as HTMLDivElement;
+
+        let lastSpan:HTMLElement = expandable.lastElementChild as HTMLElement;
+
+        this.showCollapseButton = !isNullOrUndefined(lastSpan) && this.isEllipsisActive(lastSpan);
+    }
+
+    public ngAfterViewInit():void
+    {
+        setTimeout(() =>
+        {
+            this.checkElementChildToShowButton();
+        });
     }
 }
