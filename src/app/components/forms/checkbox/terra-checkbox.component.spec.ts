319b241afbdb26b7cbc3aedb6b76b20d7e1e1549
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-02-25 16:15:44 +0100

    use nested describes

--- src/app/components/forms/checkbox/terra-checkbox.component.spec.ts
+++ src/app/components/forms/checkbox/terra-checkbox.component.spec.ts
@@ -1,7 +1,9 @@
 import { TerraCheckboxComponent } from './terra-checkbox.component';
 import {
     ComponentFixture,
-    TestBed
+    fakeAsync,
+    TestBed,
+    tick
 } from '@angular/core/testing';
 import { TooltipModule } from 'ngx-bootstrap';
 import { By } from '@angular/platform-browser';
@@ -9,7 +11,7 @@ import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Spy = jasmine.Spy;
 
-fdescribe('Component: TerraCheckboxComponent', () =>
+describe('Component: TerraCheckboxComponent', () =>
 {
     let component:TerraCheckboxComponent;
     let fixture:ComponentFixture<TerraCheckboxComponent>;
@@ -21,6 +23,7 @@ fdescribe('Component: TerraCheckboxComponent', () =>
             imports: [TooltipModule.forRoot(), FormsModule]
         }).compileComponents();
     });
+
     beforeEach(() =>
     {
         fixture = TestBed.createComponent(TerraCheckboxComponent);
@@ -32,22 +35,26 @@ fdescribe('Component: TerraCheckboxComponent', () =>
         expect(component).toBeTruthy();
     });
 
-    it('should not call change callback if a new value is set via #writeValue()', () =>
+    describe('ControlValueAccessor', () =>
     {
-        let onChangeSpy:Spy = jasmine.createSpy('onChange');
-        component.registerOnChange(onChangeSpy);
+        let onChangeSpy:Spy;
+        beforeEach(() =>
+        {
+            onChangeSpy = jasmine.createSpy('onChange');
+            component.registerOnChange(onChangeSpy);
+        });
 
-        component.writeValue(!component.value); // toggle value
-        expect(onChangeSpy).not.toHaveBeenCalled();
-    });
+        it('should not call change callback if a new value is set via #writeValue()', () =>
+        {
+            component.writeValue(!component.value); // toggle value
+            expect(onChangeSpy).not.toHaveBeenCalled();
+        });
 
-    xit('should call change callback if the value changes by clicking the checkbox', () =>
-    {
-        let onChangeSpy:Spy = jasmine.createSpy('onChange');
-        component.registerOnChange(onChangeSpy);
-
-        let inputElement:DebugElement = fixture.debugElement.query(By.css('input'));
-        inputElement.triggerEventHandler('input', true);
-        expect(onChangeSpy).toHaveBeenCalledWith(true);
+        xit('should call change callback if the value changes by clicking the checkbox', () =>
+        {
+            let inputElement:DebugElement = fixture.debugElement.query(By.css('input'));
+            inputElement.triggerEventHandler('input', true);
+            expect(onChangeSpy).toHaveBeenCalledWith(true);
+        });
     });
 });
