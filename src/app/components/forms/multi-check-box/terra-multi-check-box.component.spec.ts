961710fe33f69e8de1ffeeca0cb05aa0554e8af1
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-02-27 10:04:24 +0100

    add init test for the output

--- src/app/components/forms/multi-check-box/terra-multi-check-box.component.spec.ts
+++ src/app/components/forms/multi-check-box/terra-multi-check-box.component.spec.ts
@@ -4,14 +4,17 @@ import {
     TestBed
 } from '@angular/core/testing';
 import { CheckboxGroupComponent } from '../checkbox-group/checkbox-group.component';
-import { TerraCheckboxComponent } from '../../../..';
+import { TerraCheckboxComponent } from '../checkbox/terra-checkbox.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TooltipModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { LocalizationModule } from 'angular-l10n';
 import { l10nConfig } from '../../../translation/l10n.config';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {
+    DebugElement,
+    EventEmitter
+} from '@angular/core';
 
 fdescribe('TerraMultiCheckBoxComponent:', () =>
 {
@@ -49,11 +52,13 @@ fdescribe('TerraMultiCheckBoxComponent:', () =>
         expect(component).toBeTruthy();
     });
 
-    it(`should init its inputs`, () =>
+    it(`should init its inputs and outputs`, () =>
     {
         expect(component.collapsed).toBe(false);
         expect(component.inputIsDisabled).toBe(false);
         expect(component.inputName).toBe('terraMultiCheckBox.selectAll');
+        expect(component.checkboxStateChanges).toBeTruthy();
+        expect(component.checkboxStateChanges instanceof EventEmitter).toBe(true);
     });
 
     it('should not toggle #collapsed if #inputIsDisabled is true', () =>
