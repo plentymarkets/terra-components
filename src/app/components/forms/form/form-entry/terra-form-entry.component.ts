5a01ce3d437535a98886d9da405e7e06b9085a82
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-03-08 08:54:42 +0100

    Revert "use const VALID from angular's model declarations"
    This reverts commit 67471faa76869343785173175190dcceb254acbd.

--- src/app/components/forms/form/form-entry/terra-form-entry.component.ts
+++ src/app/components/forms/form/form-entry/terra-form-entry.component.ts
@@ -29,7 +29,6 @@ import {
 } from '@angular/forms';
 import { TerraTextInputComponent } from '../../input/text-input/terra-text-input.component';
 import { TerraFormEntryContainerDirective } from './terra-form-entry-container.directive';
-import { VALID } from '@angular/forms/src/model';
 
 @Component({
     selector:  'terra-form-entry',
@@ -90,7 +89,7 @@ export class TerraFormEntryComponent implements OnInit, OnChanges, OnDestroy, Co
         {
             if(this.componentInstance)
             {
-                this.componentInstance.isValid = status === VALID;
+                this.componentInstance.isValid = status === 'VALID';
             }
         });
 
