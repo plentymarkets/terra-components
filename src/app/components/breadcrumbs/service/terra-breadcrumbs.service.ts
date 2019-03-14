ec55570c7d100e13a90dddb5077eedb840193b4d
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-03-14 13:53:29 +0100

    fix issue with the leading slash in the initialPath...

--- src/app/components/breadcrumbs/service/terra-breadcrumbs.service.ts
+++ src/app/components/breadcrumbs/service/terra-breadcrumbs.service.ts
@@ -32,7 +32,7 @@ export class TerraBreadcrumbsService
         {
             return event instanceof NavigationEnd // navigation is done
                    && !isNullOrUndefined(this._initialPath) // initialPath is set
-                   && event.urlAfterRedirects.startsWith('/' + this._initialPath); // url starts with the initial path
+                   && event.urlAfterRedirects.startsWith(this._initialPath); // url starts with the initial path
         }).subscribe((event:NavigationEnd) =>
         {
             if(!isNullOrUndefined(this.initialRoute.children))
@@ -45,7 +45,7 @@ export class TerraBreadcrumbsService
                 let urls:Array<string> = urlParts.map((urlPart:string, index:number) => urlParts.slice(0, index + 1).join('/'));
                 urls.forEach((url:string) =>
                 {
-                    this.handleBreadcrumbForUrl(url, '/' + this._initialPath + url, '/' + cleanEventUrl);
+                    this.handleBreadcrumbForUrl(url, this._initialPath + url, '/' + cleanEventUrl);
                 });
 
                 // update breadcrumb visibility for containers that have not been checked since the url is to short
@@ -324,7 +324,7 @@ export class TerraBreadcrumbsService
     {
         let container:TerraBreadcrumbContainer = this.findBreadcrumbContainerByUrl(url);
 
-        let routerLink:string = '/' + this._initialPath + url;
+        let routerLink:string = this._initialPath + url;
 
         if(!isNullOrUndefined(container) &&
            !isNullOrUndefined(container.currentSelectedBreadcrumb) &&
