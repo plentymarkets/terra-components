c1e7da20357bb8f451295eec2a4cd343d9d34221
Daniel Trauf <daniel.trauf@plentymarkets.com>
2019-03-15 17:03:04 +0100

    add some components and modules to test module

--- src/app/components/forms/form/terra-form.component.spec.ts
+++ src/app/components/forms/form/terra-form.component.spec.ts
@@ -5,6 +5,17 @@ import {
 } from '@angular/core/testing';
 import { TerraFormComponent } from './terra-form.component';
 import { TerraFormContainerComponent } from './form-container/terra-form-container.component';
+import {
+    FormsModule,
+    ReactiveFormsModule
+} from '@angular/forms';
+import { TerraFormEntryListComponent } from './form-entry-list/terra-form-entry-list.component';
+import { TooltipModule } from 'ngx-bootstrap';
+import { TerraLabelTooltipDirective } from '../../../helpers/terra-label-tooltip.directive';
+import { TerraFormEntryComponent } from './form-entry/terra-form-entry.component';
+import { TerraButtonComponent } from '../../../..';
+import { LocalizationModule } from 'angular-l10n';
+import { l10nConfig } from '../../../translation/l10n.config';
 
 fdescribe(`TerraCardComponent:`, () =>
 {
@@ -14,8 +25,16 @@ fdescribe(`TerraCardComponent:`, () =>
     beforeEach(async(() =>
     {
         TestBed.configureTestingModule({
-            declarations: [TerraFormComponent,
-                           TerraFormContainerComponent],
+            declarations: [TerraLabelTooltipDirective,
+                           TerraButtonComponent,
+                           TerraFormEntryListComponent,
+                           TerraFormEntryComponent,
+                           TerraFormContainerComponent,
+                           TerraFormComponent],
+            imports:      [TooltipModule.forRoot(),
+                           FormsModule,
+                           ReactiveFormsModule,
+                           LocalizationModule.forRoot(l10nConfig)]
         }).compileComponents();
     }));
 
