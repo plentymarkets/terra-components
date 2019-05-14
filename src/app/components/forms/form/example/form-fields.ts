1db56be4073fb5daf96294265afbc19e22e329e2
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-14 08:50:41 +0200

    more form control extractions

--- src/app/components/forms/form/example/form-fields.ts
+++ src/app/components/forms/form/example/form-fields.ts
@@ -1,11 +1,41 @@
 import { TerraKeyValueInterface } from '../../../../models/terra-key-value.interface';
 import { TerraFormFieldInterface } from '../model/terra-form-field.interface';
 
+export const numberControl:TerraFormFieldInterface = {
+    type:         'number',
+    defaultValue: 2,
+    options:      {
+        name:        'Number',
+        required:     false
+    }
+};
+
+export const select:TerraFormFieldInterface = {
+    type:         'select',
+    defaultValue: 'option2',
+    options:      {
+        required:      true,
+        name:          'Select',
+        listBoxValues: [
+            {
+                value:    'option1',
+                caption:  'Option 1',
+                position: 0
+            },
+            {
+                value:    'option2',
+                caption:  'Option 2',
+                position: 1
+            }
+        ]
+    }
+};
+
 export const listWithChildren:TerraFormFieldInterface = {
     type:     'vertical',
     isList:   '[2,]',
     defaultValue: {
-        childSelect: 'option1',
+        childSelect: select.options.listBoxValues[0].value,
         childText: 'Placeholder',
         childNumber: 123456789
     },
@@ -13,26 +43,7 @@ export const listWithChildren:TerraFormFieldInterface = {
         name: 'Vertical'
     },
     children: {
-        childSelect:     {
-            type:         'select',
-            defaultValue: 'option2',
-            options:      {
-                required:      true,
-                name:          'Select',
-                listBoxValues: [
-                    {
-                        value:    'option1',
-                        caption:  'Option 1',
-                        position: 0
-                    },
-                    {
-                        value:    'option2',
-                        caption:  'Option 2',
-                        position: 1
-                    }
-                ]
-            }
-        },
+        childSelect: select,
         childText:          {
             type:         'text',
             isVisible:    '$listWithChildren.childSelect === "option2"',
@@ -42,14 +53,7 @@ export const listWithChildren:TerraFormFieldInterface = {
                 required:    false
             }
         },
-        childNumber: {
-            type:         'number',
-            defaultValue: '',
-            options:      {
-                name:        'Number',
-                required:     false
-            }
-        }
+        childNumber: numberControl,
     }
 };
 
