3343e034aed8ecb64f80b938909d179434138b87
Michael Frank <michael.frank@plentymarkets.com>
2019-05-20 09:43:48 +0200

    fix no-output-on-prefix, no-input-rename and no-output-rename

--- src/app/components/interactables/resizable.directive.ts
+++ src/app/components/interactables/resizable.directive.ts
@@ -4,6 +4,7 @@ import {
     EventEmitter,
     Input,
     OnChanges,
+    OnInit,
     Output,
     SimpleChanges
 } from '@angular/core';
@@ -20,37 +21,125 @@ import interact = require('interactjs');
 @Directive({
     selector: '[terraResizable]'
 })
-export class TerraResizableDirective implements OnChanges
+export class TerraResizableDirective implements OnInit, OnChanges
 {
+    @Input()
+    public resizableOptions:ResizeOptions = null;
+
+    @Input()
+    public resizableDisabled:boolean = false;
+
+    @Input()
+    public resizableGrid:false | GridOptions = false;
+
+    @Input()
+    public resizableRestrict:RestrictOptions = null;
+
+    @Input()
+    public resizableInertia:boolean | InertiaOptions = false;
+
+    @Output()
+    public resizableStart:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
+
+    @Output()
+    public resizableMove:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
+
+    @Output()
+    public resizableEnd:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
+
+    /* tslint:disable:no-output-on-prefix no-input-rename no-output-rename */
+    /**
+     * @deprecated use resizableOptions instead
+     */
     @Input('terra-resizable')
-    public options:ResizeOptions = null;
+    public set options(value:ResizeOptions)
+    {
+        console.warn('`terra-resizable` is deprecated. Please use `resizableOptions` instead.');
+        this.resizableOptions = value;
+    }
 
+    /**
+     * @deprecated use resizableDisabled instead
+     */
     @Input('terra-resizable-disabled')
-    public disabled:boolean = false;
+    public set disabled(value:boolean)
+    {
+        console.warn('`terra-resizable-disabled` is deprecated. Please use `resizableDisabled` instead.');
+        this.resizableDisabled = value;
+    }
 
+    /**
+     * @deprecated use resizableGrid instead
+     */
     @Input('terra-resizable-grid')
-    public grid:false | GridOptions = false;
+    public set grid(value:false | GridOptions)
+    {
+        console.warn('`terra-resizable-grid` is deprecated. Please use `resizableGrid` instead.');
+        this.resizableGrid = value;
+    }
 
+    /**
+     * @deprecated use resizableRestrict instead
+     */
     @Input('terra-resizable-restrict')
-    public restrict:RestrictOptions = null;
+    public set restrict(value:RestrictOptions)
+    {
+        console.warn('`terra-resizable-restrict` is deprecated. Please use `resizableRestrict` instead.');
+        this.resizableRestrict = value;
+    }
 
+    /**
+     * @deprecated use resizableInertia instead
+     */
     @Input('terra-resizable-inertia')
-    public inertia:boolean | InertiaOptions = false;
+    public set inertia(value:boolean | InertiaOptions)
+    {
+        console.warn('`terra-resizable-inertia` is deprecated. Please use `resizableInertia` instead.');
+        this.resizableInertia = value;
+    }
 
+    /**
+     * @deprecated use resizableStart instead
+     */
     @Output('terra-resizable-onStart')
     public onStart:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
 
+    /**
+     * @deprecated use resizableMove instead
+     */
     @Output('terra-resizable-onMove')
     public onMove:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
 
+    /**
+     * @deprecated use resizableEnd instead
+     */
     @Output('terra-resizable-onEnd')
     public onEnd:EventEmitter<InteractEvent> = new EventEmitter<InteractEvent>();
+    /* tslint:enable:no-output-on-prefix no-input-rename no-output-rename */
 
     private interactable:Interactable;
 
     constructor(private el:ElementRef)
     {
         this.init();
+    }
+
+    public ngOnInit():void
+    {
+        if(this.onStart.observers.length > 0)
+        {
+            console.warn('`terra-resizable-onStart` is deprecated. Please use `resizableStart` instead.');
+        }
+
+        if(this.onMove.observers.length > 0)
+        {
+            console.warn('`terra-resizable-onMove` is deprecated. Please use `resizableMove` instead.');
+        }
+
+        if(this.onEnd.observers.length > 0)
+        {
+            console.warn('`terra-resizable-onEnd` is deprecated. Please use `resizableEnd` instead.');
+        }
     }
 
     public ngOnChanges(changes:SimpleChanges):void
@@ -114,27 +203,30 @@ export class TerraResizableDirective implements OnChanges
     private init():void
     {
         let resizableConfig:any = {
-            edges:               this.options.edges,
-            invert:              this.options.invert || 'none',
-            squareResize:        !!this.options.squareResize,
-            preserveAspectRatio: !!this.options.preserveAspectRatio,
-            inertia:             this.inertia,
-            enabled:             !this.disabled,
+            edges:               this.resizableOptions.edges,
+            invert:              this.resizableOptions.invert || 'none',
+            squareResize:        !!this.resizableOptions.squareResize,
+            preserveAspectRatio: !!this.resizableOptions.preserveAspectRatio,
+            inertia:             this.resizableInertia,
+            enabled:             !this.resizableDisabled,
             onstart:             (event:InteractEvent):void =>
                                  {
                                      this.onStart.emit(event);
+                                     this.resizableStart.emit(event);
                                  },
             onmove:              (event:InteractEvent):void =>
                                  {
                                      this.onMove.emit(event);
+                                     this.resizableMove.emit(event);
                                  },
             onend:               (event:InteractEvent):void =>
                                  {
                                      this.onEnd.emit(event);
+                                     this.resizableEnd.emit(event);
                                  },
         };
 
-        if(this.grid)
+        if(this.resizableGrid)
         {
             resizableConfig.snap = {
                 targets:        [
@@ -143,14 +235,14 @@ export class TerraResizableDirective implements OnChanges
                         return this.handleSnap(x, y);
                     }
                 ],
-                endOnly:        this.grid && this.grid.endOnly,
-                relativePoints: this.grid.relativePoints
+                endOnly:        this.resizableGrid && this.resizableGrid.endOnly,
+                relativePoints: this.resizableGrid.relativePoints
             };
         }
 
-        if(this.restrict)
+        if(this.resizableRestrict)
         {
-            resizableConfig.restrict = this.restrict;
+            resizableConfig.resizableRestrict = this.resizableRestrict;
         }
 
         if(!this.interactable)
@@ -163,22 +255,22 @@ export class TerraResizableDirective implements OnChanges
 
     private handleSnap(x:number, y:number):{ x:number, y:number, range:number }
     {
-        if(this.grid)
+        if(this.resizableGrid)
         {
             let offset:{ x:number, y:number } = {
                 x: 0,
                 y: 0
             };
 
-            if(this.grid.offset)
+            if(this.resizableGrid.offset)
             {
-                offset = this.grid.offset;
+                offset = this.resizableGrid.offset;
             }
 
             return {
-                x:     Math.round((x - offset.x) / this.grid.x) * this.grid.x,
-                y:     Math.round((y - offset.y) / this.grid.y) * this.grid.y,
-                range: (this.grid.range || Infinity)
+                x:     Math.round((x - offset.x) / this.resizableGrid.x) * this.resizableGrid.x,
+                y:     Math.round((y - offset.y) / this.resizableGrid.y) * this.resizableGrid.y,
+                range: (this.resizableGrid.range || Infinity)
             };
         }
         else
