a8dfedfdad312596b4b182bda0edb60a5ae88010
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-23 15:25:50 +0200

    fix code styles

--- src/app/components/forms/form/helper/terra-form.helper.spec.ts
+++ src/app/components/forms/form/helper/terra-form.helper.spec.ts
@@ -1,23 +1,28 @@
-import { TerraFormFieldInterface } from '../../../../..';
+import { TerraFormFieldInterface } from '../model/terra-form-field.interface';
 
 describe(`TerraFormHelper:`, () =>
+{
+    const child1:TerraFormFieldInterface = {
+        type:         'text',
+        defaultValue: 'one'
+    };
+    const child2:TerraFormFieldInterface = {
+        type:         'text',
+        defaultValue: [1, 2]
+    };
+
+    const formFields:{ [key:string]:TerraFormFieldInterface } = {
+        control1: {type: 'text', defaultValue: 'one'},
+        control2: {type: 'text', defaultValue: 'two'},
+        controlWithArray: {type: 'text', defaultValue: [1, 2]},
+        controlWithObject: {type: 'text', defaultValue: {foo: 'bar'}},
+        controlWithChildren: {type: 'horizontal', children: {child1: child1, child2: child2}},
+        controlList: {type: 'number', isList: true, defaultValue: 5},
+        controlListWithoutDefaultValue: {type: 'number', isList: true}
+    };
+
+    it('should', () =>
     {
-        const child1:TerraFormFieldInterface = {type: 'text', defaultValue: 'one'};
-        const child2:TerraFormFieldInterface = {type: 'text', defaultValue: [1, 2]};
 
-        const formFields:{ [key:string]:TerraFormFieldInterface } = {
-            control1: {type: 'text', defaultValue: 'one'},
-            control2: {type: 'text', defaultValue: 'two'},
-            controlWithArray: {type: 'text', defaultValue: [1, 2]},
-            controlWithObject: {type: 'text', defaultValue: {foo: 'bar'}},
-            controlWithChildren: {type: 'horizontal', children: {child1: child1, child2: child2}},
-            controlList: {type: 'number', isList: true, defaultValue: 5},
-            controlListWithoutDefaultValue: {type: 'number', isList: true}
-        };
-
-        it('should', () =>
-        {
-
-        });
-    }
-)
+    });
+});
