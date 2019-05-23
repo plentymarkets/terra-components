332dac3a3c7a2a02eb0a67b255adfe610d865fc6
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-23 15:42:56 +0200

    add tests for fitControlsToRange method

--- src/app/components/forms/form/helper/terra-form.helper.spec.ts
+++ src/app/components/forms/form/helper/terra-form.helper.spec.ts
@@ -1,4 +1,9 @@
 import { TerraFormFieldInterface } from '../model/terra-form-field.interface';
+import { TerraFormHelper } from './terra-form.helper';
+import {
+    AbstractControl,
+    FormControl
+} from '@angular/forms';
 
 describe(`TerraFormHelper:`, () =>
 {
@@ -24,5 +29,39 @@ describe(`TerraFormHelper:`, () =>
     it('should', () =>
     {
 
+    });
+
+    describe(`fitControlsToRange()`, () =>
+    {
+        const min:number = 3;
+        const max:number = 5;
+        const listFormField:TerraFormFieldInterface = {
+            type: 'text',
+            isList: `[${min},${max}]`
+        };
+
+        it(`should add controls to the given list of #controls until the minimum of the range is reached`, () =>
+        {
+            const controls:Array<AbstractControl> = [];
+            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            expect(controls.length).toBe(min);
+        });
+
+        it(`should remove controls to the given list of #controls if it exceeds the maximum of the range`, () =>
+        {
+            const controls:Array<AbstractControl> = Array(max + 1).fill(new FormControl());
+            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            expect(controls.length).toBe(max);
+        });
+
+        it(`should not change anything on the list of #controls if its length is in the given range`, () =>
+        {
+            const count:number = min + 1;
+            const controls:Array<AbstractControl> = Array(count).fill(new FormControl());
+            const controlsCopy:Array<AbstractControl> = controls.slice();
+            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            expect(controls.length).toBe(count);
+            expect(controls).toEqual(controlsCopy);
+        });
     });
 });
