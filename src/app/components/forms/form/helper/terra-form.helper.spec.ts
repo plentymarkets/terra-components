1539602694d2d712eebcffc83d573df49a02fa65
Tim Wieder <tim.wieder@plentymarkets.com>
2019-05-23 10:35:26 +0200

    - added constants

--- src/app/components/forms/form/helper/terra-form.helper.spec.ts
+++ src/app/components/forms/form/helper/terra-form.helper.spec.ts
@@ -1,15 +1,21 @@
-import { TerraFormHelper } from './terra-form.helper';
+import { TerraFormFieldInterface } from '../../../../..';
 
 describe(`TerraFormHelper:`, () =>
     {
-        let helper:TerraFormHelper;
+        const child1:TerraFormFieldInterface = {type: 'text', defaultValue: 'one'};
+        const child2:TerraFormFieldInterface = {type: 'text', defaultValue: [1, 2]};
 
-        beforeEach(() =>
-            {
-                helper = new TerraFormHelper();
-            });
+        const formFields:{ [key:string]:TerraFormFieldInterface } = {
+            control1: {type: 'text', defaultValue: 'one'},
+            control2: {type: 'text', defaultValue: 'two'},
+            controlWithArray: {type: 'text', defaultValue: [1, 2]},
+            controlWithObject: {type: 'text', defaultValue: {foo: 'bar'}},
+            controlWithChildren: {type: 'horizontal', children: {child1: child1, child2: child2}},
+            controlList: {type: 'number', isList: true, defaultValue: 5},
+            controlListWithoutDefaultValue: {type: 'number', isList: true}
+        };
 
-        it('should', function()
+        it('should', () =>
         {
 
         });
