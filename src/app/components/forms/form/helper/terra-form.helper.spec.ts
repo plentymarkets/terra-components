06b1921fa2f4a3c1e601b3f300cc7842ba7908c4
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-24 08:16:19 +0200

    tidy up the test values

--- src/app/components/forms/form/helper/terra-form.helper.spec.ts
+++ src/app/components/forms/form/helper/terra-form.helper.spec.ts
@@ -9,49 +9,34 @@ import Spy = jasmine.Spy;
 
 describe(`TerraFormHelper:`, () =>
 {
-    const child1:TerraFormFieldInterface = {
-        type:         'text',
-        defaultValue: 'one'
-    };
-    const child2:TerraFormFieldInterface = {
-        type:         'text',
-        defaultValue: [1, 2]
-    };
-
-    const formFields:{ [key:string]:TerraFormFieldInterface } = {
-        control1: {type: 'text', defaultValue: 'one'},
-        control2: {type: 'text', defaultValue: 'two'},
-        controlWithArray: {type: 'text', defaultValue: [1, 2]},
-        controlWithObject: {type: 'text', defaultValue: {foo: 'bar'}},
-        controlWithChildren: {type: 'horizontal', children: {child1: child1, child2: child2}},
-        controlList: {type: 'number', isList: true, defaultValue: 5},
-        controlListWithoutDefaultValue: {type: 'number', isList: true}
-    };
-
-    it('should', () =>
-    {
-
-    });
+    const control1:TerraFormFieldInterface = {type: 'text', defaultValue: 'one'};
+    const control2:TerraFormFieldInterface = {type: 'text', defaultValue: 'two'};
+    const controlWithArray:TerraFormFieldInterface = {type: 'checkboxGroup', defaultValue: [1, 2]};
+    const controlWithObject:TerraFormFieldInterface = {type: 'text', defaultValue: {foo: 'bar'}};
+    const controlWithChildren:TerraFormFieldInterface = {type: 'horizontal', children: {child1: control1, child2: controlWithArray}};
+    const controlList:TerraFormFieldInterface = {type: 'number', isList: true, defaultValue: 5};
+    const min:number = 3; const max:number = 5;
+    const controlListWithRange:TerraFormFieldInterface = {type: 'text', isList: `[${min},${max}]`};
 
     describe(`createNewControl()`, () =>
     {
         it(`should create a new FormControl instance for a primitive #formField type`, () =>
         {
-            const control:AbstractControl = TerraFormHelper.createNewControl(null, child1);
+            const control:AbstractControl = TerraFormHelper.createNewControl(null, control1);
             expect(control instanceof FormControl).toBe(true);
         });
 
         it(`should set the new control's value to the passed #value`, () =>
         {
             const value:string = 'value';
-            const control:AbstractControl =  TerraFormHelper.createNewControl(value, child1);
+            const control:AbstractControl =  TerraFormHelper.createNewControl(value, control1);
             expect(control.value).toEqual(value);
         });
 
         it(`should add validators to the control`, () =>
         {
             const spy:Spy = spyOn(TerraFormHelper, 'generateValidators').and.callThrough();
-            const formField:TerraFormFieldInterface = child1;
+            const formField:TerraFormFieldInterface = control1;
             formField.options = {
                 required: true
             };
@@ -64,7 +49,7 @@ describe(`TerraFormHelper:`, () =>
         it(`should create a new FormGroup instance if a #formField has children`, () =>
         {
             const spy:Spy = spyOn(TerraFormHelper, 'parseReactiveForm').and.callThrough();
-            const formField:TerraFormFieldInterface = formFields.controlWithChildren;
+            const formField:TerraFormFieldInterface = controlWithChildren;
             const control:AbstractControl = TerraFormHelper.createNewControl(null, formField);
             expect(control instanceof FormGroup).toBe(true);
             expect(Object.values((control as FormGroup).controls).length).toBe(Object.values(formField.children).length);
@@ -74,24 +59,17 @@ describe(`TerraFormHelper:`, () =>
 
     describe(`fitControlsToRange()`, () =>
     {
-        const min:number = 3;
-        const max:number = 5;
-        const listFormField:TerraFormFieldInterface = {
-            type: 'text',
-            isList: `[${min},${max}]`
-        };
-
         it(`should add controls to the given list of #controls until the minimum of the range is reached`, () =>
         {
             const controls:Array<AbstractControl> = [];
-            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            TerraFormHelper['fitControlsToRange'](controlListWithRange, controls);
             expect(controls.length).toBe(min);
         });
 
         it(`should remove controls to the given list of #controls if it exceeds the maximum of the range`, () =>
         {
             const controls:Array<AbstractControl> = Array(max + 1).fill(new FormControl());
-            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            TerraFormHelper['fitControlsToRange'](controlListWithRange, controls);
             expect(controls.length).toBe(max);
         });
 
@@ -100,7 +78,7 @@ describe(`TerraFormHelper:`, () =>
             const count:number = min + 1;
             const controls:Array<AbstractControl> = Array(count).fill(new FormControl());
             const controlsCopy:Array<AbstractControl> = controls.slice();
-            TerraFormHelper['fitControlsToRange'](listFormField, controls);
+            TerraFormHelper['fitControlsToRange'](controlListWithRange, controls);
             expect(controls.length).toBe(count);
             expect(controls).toEqual(controlsCopy);
         });
