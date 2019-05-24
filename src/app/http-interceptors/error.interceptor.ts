b72063f3457f2f8f24d7ccadd2e173566ad4f088
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-24 14:47:22 +0200

    Inject Alert- and TranslationService

--- src/app/http-interceptors/error.interceptor.ts
+++ src/app/http-interceptors/error.interceptor.ts
@@ -5,9 +5,16 @@ import {
     HttpRequest
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AlertService } from '../components/alert/alert.service';
+import { TranslationService } from 'angular-l10n';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor
 {
+    constructor(private alertService:AlertService, private translation:TranslationService)
+    {}
+
     public intercept(req:HttpRequest<any>, next:HttpHandler):Observable<HttpEvent<any>>
     {
         return undefined;
