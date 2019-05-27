9c58fa875df740327f40777a72838df470b6aaa2
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2019-05-27 16:10:34 +0200

    add export & add to provider

--- src/app/http-interceptors/index.ts
+++ src/app/http-interceptors/index.ts
@@ -1,9 +1,20 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Provider } from '@angular/core';
 import { AuthInterceptor } from './auth.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
 
 export { AuthInterceptor } from './auth.interceptor';
+export { ErrorInterceptor } from './error.interceptor';
 
 export const httpInterceptorProviders:Array<Provider> = [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi:    true
+    },
+    {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: ErrorInterceptor,
+        multi:    true
+    }
 ];
