708e5bf87c2f7c79a9ed7908d3d641a8280e027c
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-05-31 14:45:23 +0200

    add files to src directory generated by cli v7

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { TerraComponentsModule } from './app/';
 
-if(process.env.ENV === 'production')
+import { TerraComponentsModule } from './app/terra-components.module';
+import { environment } from './environments/environment';
+
+if(environment.production)
 {
     enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(TerraComponentsModule);
+platformBrowserDynamic().bootstrapModule(TerraComponentsModule)
+                        .catch((err:any) => console.error(err));
