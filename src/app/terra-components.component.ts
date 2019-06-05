6d531f0378fa817ffc7ad1f44419236990238958
Michael Kunze <michael.kunze@plentymarkets.com>
2019-06-05 12:57:16 +0200

    ckeditor 4 test

--- src/app/terra-components.component.ts
+++ src/app/terra-components.component.ts
@@ -14,9 +14,80 @@ export class TerraComponentsComponent
 {
     private viewContainerRef:ViewContainerRef;
 
+    protected config:{};
+
     constructor(viewContainerRef:ViewContainerRef)
     {
         // You need this small hack in order to catch application root view container ref
         this.viewContainerRef = viewContainerRef;
+
+        this.config = {
+            toolbarGroups: [
+                {
+                    name:   'styles',
+                    groups: ['styles']
+                },
+                {
+                    name:   'colors',
+                    groups: ['colors']
+                },
+                {
+                    name:   'basicstyles',
+                    groups: ['basicstyles',
+                             'cleanup']
+                },
+                {
+                    name:   'paragraph',
+                    groups: ['list',
+                             'indent',
+                             'blocks',
+                             'align',
+                             'bidi',
+                             'paragraph']
+                },
+                {
+                    name:   'clipboard',
+                    groups: ['clipboard',
+                             'undo']
+                },
+                {
+                    name:   'editing',
+                    groups: ['find',
+                             'selection',
+                             'spellchecker',
+                             'editing']
+                },
+                {
+                    name:   'links',
+                    groups: ['links']
+                },
+                {
+                    name:   'document',
+                    groups: ['mode',
+                             'document',
+                             'doctools']
+                },
+                {
+                    name:   'forms',
+                    groups: ['forms']
+                },
+                {
+                    name:   'insert',
+                    groups: ['insert']
+                },
+                {
+                    name:   'tools',
+                    groups: ['tools']
+                },
+                {
+                    name:   'others',
+                    groups: ['others']
+                },
+                {
+                    name:   'about',
+                    groups: ['about']
+                }
+            ]
+        };
     }
 }
