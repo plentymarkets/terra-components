e0319bfe0e0cb5df8b31a0a89e86c526c1b06a0a
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-06-17 14:11:40 +0200

    remove provider of TerraBreadcrumbsService since it is provided by the TerraBreadcrumbsComponent directly

--- src/app/service/service-collection.ts
+++ src/app/service/service-collection.ts
@@ -1,7 +1,6 @@
 import { Provider } from '@angular/core';
 import { TerraFrontendStorageService } from '../components/file-browser/terra-frontend-storage.service';
 import { TerraDynamicFormService } from '../components/forms/dynamic-form/service/terra-dynamic-form.service';
-import { TerraBreadcrumbsService } from '../components/breadcrumbs/service/terra-breadcrumbs.service';
 import { TerraJsonToFormFieldService } from '../components/forms/dynamic-form/service/terra-json-to-form-field.service';
 import { TerraMultiSplitViewBreadcrumbsService } from '../components/split-view/multi/injectables/terra-multi-split-view-breadcrumbs.service';
 import { TerraFormFieldControlService } from '../components/forms/dynamic-form/service/terra-form-field-control.service';
@@ -14,5 +13,5 @@ export const services:Array<Provider> = [
     TerraFormFieldControlService,
     TerraJsonToFormFieldService,
     TerraMultiSplitViewBreadcrumbsService,
-    TerraBreadcrumbsService
+    TerraMultiSplitViewBreadcrumbsService
 ];
