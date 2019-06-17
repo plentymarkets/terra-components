856e34689273fddda53ae8a0c79b1569c492a3aa
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-06-17 14:14:48 +0200

    remove provider for TerraJsonToFormFieldService since it is a helper and should not be injectable

--- src/app/service/service-collection.ts
+++ src/app/service/service-collection.ts
@@ -1,6 +1,5 @@
 import { Provider } from '@angular/core';
 import { TerraDynamicFormService } from '../components/forms/dynamic-form/service/terra-dynamic-form.service';
-import { TerraJsonToFormFieldService } from '../components/forms/dynamic-form/service/terra-json-to-form-field.service';
 import { TerraMultiSplitViewBreadcrumbsService } from '../components/split-view/multi/injectables/terra-multi-split-view-breadcrumbs.service';
 import { TerraFormFieldControlService } from '../components/forms/dynamic-form/service/terra-form-field-control.service';
 import { TerraFileBrowserService } from '../components/file-browser/terra-file-browser.service';
@@ -9,6 +8,5 @@ export const services:Array<Provider> = [
     TerraFileBrowserService,
     TerraDynamicFormService,
     TerraFormFieldControlService,
-    TerraJsonToFormFieldService,
     TerraMultiSplitViewBreadcrumbsService
 ];
