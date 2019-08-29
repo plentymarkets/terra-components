c94c1e7b092fe3de11b006dfa35793036eb0bb18
Viktor Wiebe <viktor.wiebe@plentymarkets.com>
2019-08-29 15:20:16 +0200

    set sticky toolbar default to false

--- src/lib/components/toolbar/base-toolbar/terra-base-toolbar.component.ts
+++ src/lib/components/toolbar/base-toolbar/terra-base-toolbar.component.ts
@@ -20,7 +20,7 @@ export class TerraBaseToolbarComponent
      * @description makes toolbar stick to the top while scrolling
      */
     @Input()
-    public inputIsSticky:boolean = true;
+    public inputIsSticky:boolean;
 
 
     constructor()
