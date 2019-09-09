968d382b282d7040c102d4fe12c5166fc39cb7a6
Michael Kunze <michael.kunze@plentymarkets.com>
2019-09-09 09:19:25 +0200

    delete scss file

--- src/lib/components/tooltip/example/tooltip-directive.example.ts
+++ src/lib/components/tooltip/example/tooltip-directive.example.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'tc-tooltip-directive-example',
-    styles:   [require('./tooltip-directive.example.scss')],
     template: require('./tooltip-directive.example.html')
 })
 export class TooltipDirectiveExample
