4fe8ee863a7fd18d56fd240ba635b889af186926
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2019-09-13 17:18:58 +0200

    ts lint

--- src/lib/pipes/select-sort.pipe.ts
+++ src/lib/pipes/select-sort.pipe.ts
@@ -7,7 +7,7 @@ import { SelectBoxSortHelper } from '../helpers/select-box-sort.helper';
 @Pipe({name: 'selectSort'})
 export class SelectSortPipe implements PipeTransform
 {
-    transform(sortingList:Array<any>, sortingKey?:string, sortDesc?:boolean,)
+    public transform(sortingList:Array<any>, sortingKey?:string, sortDesc?:boolean):Array<any>
     {
         return SelectBoxSortHelper.sortArray(sortingList, sortingKey);
     }
