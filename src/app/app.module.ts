4261b50e92f1056bf6645964f376435014168cb8
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-09-17 14:36:40 +0200

    AppModule: undo changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { l10nConfig } from './translation/l10n.config';
 import { AppComponent } from './app.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { TerraComponentsExamplesModule } from '../lib/terra-components-examples.module';
+import { TerraComponentsModule } from '../lib';
 import { RouterModule } from '@angular/router';
 
 function createCompiler(compilerFactory:CompilerFactory):Compiler
@@ -35,7 +36,7 @@ function initL10n(l10nLoader:L10nLoader):Function
  * NOTE: It is not publicly accessible either.
  */
 @NgModule({
-    imports:      [
+    imports:   [
         RouterModule.forRoot([]),
         BrowserModule,
         BrowserAnimationsModule,
@@ -43,11 +44,8 @@ function initL10n(l10nLoader:L10nLoader):Function
         LocalizationModule.forRoot(l10nConfig),
         TerraComponentsExamplesModule
     ],
-    declarations: [
-        AppComponent,
-        ShowcaseComponent
-    ],
-    providers:    [
+    declarations: [AppComponent, ShowcaseComponent],
+    providers: [
         {
             provide:  COMPILER_OPTIONS,
             useValue: {},
@@ -70,7 +68,7 @@ function initL10n(l10nLoader:L10nLoader):Function
             deps:       [CompilerFactory]
         }
     ],
-    bootstrap:    [AppComponent]
+    bootstrap: [AppComponent]
 })
 export class AppModule
 {}
