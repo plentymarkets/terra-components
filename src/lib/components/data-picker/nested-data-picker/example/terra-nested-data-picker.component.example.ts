b14022e69cf74c1d9e153bf17f7560171cecc3d5
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2019-10-15 14:50:40 +0200

    FIX terra-nested-data-picker-example

--- src/lib/components/data-picker/nested-data-picker/example/terra-nested-data-picker.component.example.ts
+++ src/lib/components/data-picker/nested-data-picker/example/terra-nested-data-picker.component.example.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { NestedPickerExampleService } from './service/terra-nested-data-picker.service.example';
 
 @Component({
-    selector:  'terra-nested-data-picker-example',
-    styleUrls: ['./terra-nested-data-picker.component.example.scss'],
+    selector:    'terra-nested-data-picker-example',
+    styleUrls:   ['./terra-nested-data-picker.component.example.scss'],
     templateUrl: './terra-nested-data-picker.component.example.html',
-    providers: [NestedPickerExampleService]
+    providers:   [NestedPickerExampleService]
 })
 export class TerraNestedDataPickerComponentExample
 {
-    constructor(protected terraService:NestedPickerExampleService)
+    constructor(public _terraService:NestedPickerExampleService)
     {
     }
 }
