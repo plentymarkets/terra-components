772f522fd397d179a51abf0c12db9336e83cb48d
Michael Frank <michael.frank@plentymarkets.com>
2019-10-16 11:05:04 +0200

    environments tslint

---
+++ src/lib/environments/environment.interface.ts
@@ -0,0 +1,6 @@
+export interface EnvironmentInterface
+{
+    production:boolean;
+    test:boolean;
+    l10nPrefix:string;
+}
--- src/lib/environments/environment.prod.ts
+++ src/lib/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { EnvironmentInterface } from './environment.interface';
+
+export const environment:EnvironmentInterface = {
   production: true,
   test: false,
   l10nPrefix: 'assets/lang/locale-'
