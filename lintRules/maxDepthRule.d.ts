9b1085e7b32008e11101de9aaee227142bed1481
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-17 13:45:41 +0200

    move lint rules to a new repository

--- lintRules/maxDepthRule.d.ts
+++
@@ -1,5 +0,0 @@
-import * as ts from 'typescript';
-import * as Lint from 'tslint';
-export declare class Rule extends Lint.Rules.AbstractRule {
-    apply(sourceFile: ts.SourceFile): Array<Lint.RuleFailure>;
-}
