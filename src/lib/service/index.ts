017c92036b40e7cbcbdfb3a4cab899daf24c074f
Michael Frank <michael.frank@plentymarkets.com>
2019-10-18 11:25:39 +0200

    Update index.ts

--- src/lib/service/index.ts
+++ src/lib/service/index.ts
@@ -1,2 +1,2 @@
-export {ModelCache} from './model-cache';
-export {TerraBaseService} from './terra-base.service';
+export { ModelCache } from './model-cache';
+export { TerraBaseService } from './terra-base.service';
