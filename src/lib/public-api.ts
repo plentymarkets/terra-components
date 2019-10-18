92b246d0949e07fbe59f7e210a2133f4d3317b26
Michael Frank <michael.frank@plentymarkets.com>
2019-10-18 10:06:06 +0200

    Update public-api.ts
    Co-Authored-By: Pascal Weyrich <pascal.weyrich@plentymarkets.com>

--- src/lib/public-api.ts
+++ src/lib/public-api.ts
@@ -66,92 +66,117 @@ export * from './components/split-view/multi/injectables/terra-multi-split-view.
 export * from './components/split-view/multi/interfaces/terra-multi-split-view.interface';
 export * from './components/dynamic-module-loader/data/terra-split-view-component.interface';
 export * from './components/dynamic-component-loader/terra-dynamic-component-loader.component';
-export * from './components/file-browser/model/s3-storage-object.interface';
-export * from './components/file-browser/model/terra-image-metadata.interface';
-export * from './components/file-browser/model/terra-storage-object';
-export * from './components/file-browser/model/terra-storage-object-list';
-export * from './components/file-browser/model/terra-upload-item';
-export * from './components/file-browser/model/terra-upload-queue';
-export * from './components/file-browser/terra-base-storage.interface';
-export * from './components/file-browser/terra-frontend-storage.service';
-export * from './components/no-result/terra-no-result-notice.component';
-export * from './components/editors/note-editor/terra-note-editor.component';
-export * from './components/editors/code-editor/terra-code-editor.component';
-export * from './components/note/terra-note.component';
-export * from './components/forms/slider/terra-slider.component';
-export * from './components/tree/node-tree/terra-node-tree.component';
-export * from './components/tree/node-tree/data/terra-node-tree.config';
-export * from './components/tree/node-tree/data/terra-node.interface';
-export * from './components/tree/node-tree/node/terra-node.component';
-export * from './components/forms/input/time-picker/terra-time-picker.component';
-export * from './components/dynamic-module-loader/data/terra-dynamic-loaded-component-input.interface';
-export * from './components/tables/data-table/enums/terra-text-align.enum';
-export * from './components/data/terra-base-parameter.interface';
-export * from './components/info/terra-info.component';
-export * from './components/forms/dynamic-form/terra-dynamic-form.component';
-export * from './components/forms/dynamic-form/dynamic-switch/terra-dynamic-switch.component';
-export * from './components/forms/dynamic-form/data/terra-form-field-base';
-export * from './components/forms/dynamic-form/data/terra-form-field-input-text';
-export * from './components/forms/dynamic-form/data/terra-form-field-input-number';
-export * from './components/forms/dynamic-form/data/terra-form-field-select-box';
-export * from './components/forms/dynamic-form/data/terra-form-field-horizontal-container';
-export * from './components/forms/dynamic-form/data/terra-form-field-vertical-container';
-export * from './components/forms/dynamic-form/data/terra-form-field-check-box';
-export * from './components/forms/dynamic-form/data/terra-form-field-date-picker';
-export * from './components/forms/dynamic-form/data/terra-form-field-category-picker';
-export * from './components/forms/dynamic-form/handler/terra-dynamic-form-functions.handler';
-export * from './components/forms/dynamic-form/enum/terra-control-type.enum';
-export * from './components/forms/dynamic-form/service/terra-json-to-form-field.service';
-export * from './components/forms/dynamic-form/service/terra-dynamic-form.service';
-export * from './components/forms/dynamic-form/service/terra-form-field-control.service';
-export * from './components/file-browser/terra-base-private-storage.interface';
-export * from './components/file-browser/terra-file-browser.service';
-export * from './components/file-browser/terra-file-browser.component';
-export * from './components/data-picker/category-picker/terra-category-picker.component';
-export * from './components/data-picker/category-picker/service/terra-category-picker-base.service';
-export * from './models/terra-key-value.interface';
-export * from './components/data-picker/category-picker/terra-category-picker.component';
-export * from './components/data-picker/category-picker/service/terra-category-picker-base.service';
-export * from './components/data-picker/category-picker/data/category-data.interface';
-export * from './components/data-picker/category-picker/data/category-detail-data.interface';
-export * from './components/data-picker/category-picker/data/category-value.interface';
-export * from './components/data-picker/category-picker/data/category-client.interface';
-export * from './components/data-picker/nested-data-picker/terra-nested-data-picker.component';
-export * from './components/data-picker/nested-data-picker/service/terra-nested-data-picker-base.service';
-export * from './components/data-picker/nested-data-picker/data/nested-data.interface';
-export * from './components/data-picker/nested-data-picker/data/nested-value.interface';
-export * from './components/split-view/multi/injectables/terra-multi-split-view-breadcrumbs.service';
-export * from './validators/terra-decimal-validator';
-export * from './components/forms/form/terra-form.component';
-export * from './components/forms/form/form-container/terra-form-container.component';
-export * from './components/forms/form/form-entry/terra-form-entry.component';
-export * from './components/forms/form/form-entry-list/terra-form-entry-list.component';
-export * from './components/forms/form/helper/terra-form-field.helper';
-export * from './components/forms/form/model/terra-form-field.interface';
-export * from './components/forms/form/model/terra-form-scope.data';
-export * from './components/forms/form/model/terra-form-type-map.enum';
-export * from './components/forms/form/model/form-type-map';
-export * from './components/forms/form/model/terra-form-property.decorator';
-export * from './components/breadcrumbs/terra-breadcrumbs.component';
-export * from './components/breadcrumbs/terra-breadcrumb';
-export * from './components/breadcrumbs/service/terra-breadcrumbs.service';
-export * from './components/forms/tag-select/terra-tag-select.component';
-export * from './components/layouts/column-container/two-columns/terra-two-columns-container.directive';
-export * from './components/layouts/column-container/two-columns/terra-two-columns-container.component';
-export * from './components/layouts/column-container/three-columns/terra-three-columns-container.component';
-export * from './components/tables/data-table/enums/terra-href-type.enum';
-export * from './components/tables/group-function/terra-group-function.component';
-export * from './components/tables/data-table/terra-data-table-base.service';
-export * from './components/tables/data-table/table-row/table-row.component';
-export * from './components/alert/alert.service';
-export * from './service/model-cache';
+
+// TODO file browser index
+export {
+    createS3StorageObject,
+    S3StorageObjectInterface
+} from './components/file-browser/model/s3-storage-object.interface';
+export { TerraImageMetadata } from './components/file-browser/model/terra-image-metadata.interface';
+export { TerraStorageObject } from './components/file-browser/model/terra-storage-object';
+export { TerraStorageObjectList } from './components/file-browser/model/terra-storage-object-list';
+export {
+    TerraUploadItem,
+    UploadCallback
+} from './components/file-browser/model/terra-upload-item';
+export {
+    TerraUploadQueue,
+    UploadQueueUrlFactory
+}from './components/file-browser/model/terra-upload-queue';
+export { TerraBaseStorageService } from './components/file-browser/terra-base-storage.interface';
+export { TerraFrontendStorageService } from './components/file-browser/terra-frontend-storage.service';
+export { TerraFileBrowserService } from './components/file-browser/terra-file-browser.service';
+export { TerraFileBrowserComponent } from './components/file-browser/terra-file-browser.component';
+export { TerraBasePrivateStorageService } from './components/file-browser/terra-base-private-storage.interface';
+
+
+export { TerraNoResultNoticeComponent } from './components/no-result/terra-no-result-notice.component';
+export { TerraNoteEditorComponent } from './components/editors/note-editor/terra-note-editor.component';
+export { TerraCodeEditorComponent } from './components/editors/code-editor/terra-code-editor.component';
+export { TerraNoteComponent } from './components/note/terra-note.component';
+export { TerraSliderComponent } from './components/forms/slider/terra-slider.component';
+export { TerraNodeTreeComponent } from './components/tree/node-tree/terra-node-tree.component';
+export { TerraNodeTreeConfig } from './components/tree/node-tree/data/terra-node-tree.config';
+export { TerraNodeInterface } from './components/tree/node-tree/data/terra-node.interface';
+export { TerraNodeComponent } from './components/tree/node-tree/node/terra-node.component';
+export { TerraTimePickerComponent } from './components/forms/input/time-picker/terra-time-picker.component';
+export { TerraDynamicLoadedComponentInputInterface } from './components/dynamic-module-loader/data/terra-dynamic-loaded-component-input.interface';
+export { TerraTextAlignEnum } from './components/tables/data-table/enums/terra-text-align.enum';
+export { TerraBaseParameterInterface } from './components/data/terra-base-parameter.interface';
+export { TerraInfoComponent } from './components/info/terra-info.component';
+
+// TODO dynamic form deprecated? eigene index?
+export { TerraDynamicFormComponent } from './components/forms/dynamic-form/terra-dynamic-form.component';
+export { TerraDynamicSwitchComponent } from './components/forms/dynamic-form/dynamic-switch/terra-dynamic-switch.component';
+export { TerraFormFieldBase } from './components/forms/dynamic-form/data/terra-form-field-base';
+export { TerraFormFieldInputText } from './components/forms/dynamic-form/data/terra-form-field-input-text';
+export { TerraFormFieldInputNumber } from './components/forms/dynamic-form/data/terra-form-field-input-number';
+export { TerraFormFieldSelectBox } from './components/forms/dynamic-form/data/terra-form-field-select-box';
+export { TerraFormFieldHorizontalContainer } from './components/forms/dynamic-form/data/terra-form-field-horizontal-container';
+export { TerraFormFieldVerticalContainer } from './components/forms/dynamic-form/data/terra-form-field-vertical-container';
+export { TerraFormFieldCheckBox } from './components/forms/dynamic-form/data/terra-form-field-check-box';
+export { TerraFormFieldDatePicker } from './components/forms/dynamic-form/data/terra-form-field-date-picker';
+export { TerraFormFieldCategoryPicker } from './components/forms/dynamic-form/data/terra-form-field-category-picker';
+export { TerraDynamicFormFunctionsHandler } from './components/forms/dynamic-form/handler/terra-dynamic-form-functions.handler';
+export { TerraControlTypeEnum } from './components/forms/dynamic-form/enum/terra-control-type.enum';
+export { TerraJsonToFormFieldService } from './components/forms/dynamic-form/service/terra-json-to-form-field.service';
+export { TerraDynamicFormService } from './components/forms/dynamic-form/service/terra-dynamic-form.service';
+export { TerraFormFieldControlService } from './components/forms/dynamic-form/service/terra-form-field-control.service';
+
+
+export { TerraKeyValueInterface } from './models/terra-key-value.interface';
+
+// TODO Picker index?
+export { TerraCategoryPickerComponent } from './components/data-picker/category-picker/terra-category-picker.component';
+export { TerraCategoryPickerBaseService } from './components/data-picker/category-picker/service/terra-category-picker-base.service';
+export { CategoryDataInterface } from './components/data-picker/category-picker/data/category-data.interface';
+export { CategoryDetailDataInterface } from './components/data-picker/category-picker/data/category-detail-data.interface';
+export { CategoryValueInterface } from './components/data-picker/category-picker/data/category-value.interface';
+export { CategoryClientInterface } from './components/data-picker/category-picker/data/category-client.interface';
+export { TerraNestedDataPickerComponent } from './components/data-picker/nested-data-picker/terra-nested-data-picker.component';
+export { TerraNestedDataPickerBaseService } from './components/data-picker/nested-data-picker/service/terra-nested-data-picker-base.service';
+export { NestedDataInterface } from './components/data-picker/nested-data-picker/data/nested-data.interface';
+export { NestedValueInterface } from './components/data-picker/nested-data-picker/data/nested-value.interface';
+
+
+export { TerraMultiSplitViewBreadcrumbsService } from './components/split-view/multi/injectables/terra-multi-split-view-breadcrumbs.service';
+
+export { TerraValidators } from './validators/validators';
+
+
+export { TerraFormComponent } from './components/forms/form/terra-form.component';
+export { TerraFormContainerComponent } from './components/forms/form/form-container/terra-form-container.component';
+export { TerraFormEntryComponent } from './components/forms/form/form-entry/terra-form-entry.component';
+export { TerraFormEntryListComponent } from './components/forms/form/form-entry-list/terra-form-entry-list.component';
+export { TerraFormFieldHelper } from './components/forms/form/helper/terra-form-field.helper';
+export { TerraFormFieldInterface } from './components/forms/form/model/terra-form-field.interface';
+export { TerraFormScope } from './components/forms/form/model/terra-form-scope.data';
+export { TerraFormTypeMap } from './components/forms/form/model/terra-form-type-map.enum';
+export { FormTypeMap } from './components/forms/form/model/form-type-map';
+export {
+    TERRA_FORM_PROPERTY_METADATA_KEY,
+    TerraFormProperty
+} from './components/forms/form/model/terra-form-property.decorator';
+export { TerraBreadcrumbsComponent } from './components/breadcrumbs/terra-breadcrumbs.component';
+export { TerraBreadcrumb } from './components/breadcrumbs/terra-breadcrumb';
+export { TerraBreadcrumbsService } from './components/breadcrumbs/service/terra-breadcrumbs.service';
+export { TerraTagSelectComponent } from './components/forms/tag-select/terra-tag-select.component';
+export { TerraTwoColumnsContainerDirective } from './components/layouts/column-container/two-columns/terra-two-columns-container.directive';
+export { TerraTwoColumnsContainerComponent } from './components/layouts/column-container/two-columns/terra-two-columns-container.component';
+export { TerraThreeColumnsContainerComponent } from './components/layouts/column-container/three-columns/terra-three-columns-container.component';
+export { TerraHrefTypeEnum } from './components/tables/data-table/enums/terra-href-type.enum';
+export { TerraGroupFunctionComponent } from './components/tables/group-function/terra-group-function.component';
+export { TerraDataTableBaseService } from './components/tables/data-table/terra-data-table-base.service';
+export { TableRowComponent } from './components/tables/data-table/table-row/table-row.component';
+export { AlertService } from './components/alert/alert.service';
+export { ModelCache } from './service/model-cache';
 export * from './http-interceptors';
-export * from './components/editors/ck-editor/presets/ck-editor-minimum-preset';
-export * from './components/editors/ck-editor/presets/ck-editor-full-preset';
-export * from './components/editors/ck-editor/ck-editor.directive';
+export { ckEditorMinimumConfig } from './components/editors/ck-editor/presets/ck-editor-minimum-preset';
+export { ckEditorFullConfig } from './components/editors/ck-editor/presets/ck-editor-full-preset';
+export { CKEditorDirective } from './components/editors/ck-editor/ck-editor.directive';
 export * from './helpers';
-export * from './components/forms/input/radio/radio-group.component';
-export * from './components/forms/input/radio/radio-input.component';
+export { RadioGroupComponent } from './components/forms/input/radio/radio-group.component';
+export { RadioInputComponent } from './components/forms/input/radio/radio-input.component';
 export * from './models';
 export { TooltipDirective } from './components/tooltip/tooltip.directive';
-export * from './components/forms/select-box/data/allowed.colors.enum';
+export { AllowedColors } from './components/forms/select-box/data/allowed.colors.enum';
