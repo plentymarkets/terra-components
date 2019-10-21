e369d9edb6b7c74d1f8fded65252fa4d0ba9a4bd
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-21 10:57:46 +0200

    add jsdoc

--- src/lib/http-interceptors/interceptor-providers.ts
+++ src/lib/http-interceptors/interceptor-providers.ts
@@ -4,6 +4,9 @@ import { AuthInterceptor } from './auth.interceptor';
 import { ErrorInterceptor } from './error.interceptor';
 import { LoadingInterceptor } from './loading.interceptor';
 
+/**
+ * @description List of providers for all http interceptors
+ */
 export const httpInterceptorProviders:Array<Provider> = [
     {
         provide:  HTTP_INTERCEPTORS,
