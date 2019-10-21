4e842254a8eb29268392baecdbc484707580f83b
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-21 10:55:43 +0200

    fix build in plugin terra basic caused by the httpInterceptorProviders

---
+++ src/lib/http-interceptors/interceptor-providers.ts
@@ -0,0 +1,23 @@
+import { Provider } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
+import { LoadingInterceptor } from './loading.interceptor';
+
+export const httpInterceptorProviders:Array<Provider> = [
+    {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: AuthInterceptor,
+        multi:    true
+    },
+    {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: ErrorInterceptor,
+        multi:    true
+    },
+    {
+        provide:  HTTP_INTERCEPTORS,
+        useClass: LoadingInterceptor,
+        multi:    true
+    }
+];
