c650f8220c5532136e0278f57aa47ce980e78d55
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-25 10:56:58 +0200

    Add JSDoc

--- src/lib/utils/http-params.ts
+++ src/lib/utils/http-params.ts
@@ -5,6 +5,13 @@ import {
     isNullOrUndefined
 } from 'util';
 
+/**
+ * Creates an instance of HttpParams filled with given #params
+ * @param params An Object containing key-value-pairs that should be transferred to HttpParams
+ * @param arrayAsArray A switch to change encoding of array-like values.
+ *        You can choose between the default 'list=a,b,c' (false) or 'list[]=a&list[]=b&list[]=c' (true) encoding.
+ * @returns An instance of HttpParams with the same key-value-pairs as delivered by #params
+ */
 export function createHttpParams(params:Params, arrayAsArray:boolean = false):HttpParams
 {
     if(isNullOrUndefined(params))
