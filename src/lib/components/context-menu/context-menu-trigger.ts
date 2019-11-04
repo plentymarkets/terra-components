d37e4a5a6ff8120cdef01b0dafb484367f9493ca
Michael Kunze <michael.kunze@plentymarkets.com>
2019-11-04 13:31:42 +0100

    using enum instead of class

--- src/lib/components/context-menu/context-menu-trigger.ts
+++ src/lib/components/context-menu/context-menu-trigger.ts
@@ -1,6 +1,6 @@
-export class ContextMenuTrigger
+export enum ContextMenuTrigger
 {
-    public static hover:string = 'hover';
-    public static leftClick:string = 'leftClick';
-    public static rightClick:string = 'rightClick';
+    hover,
+    leftClick,
+    rightClick
 }
