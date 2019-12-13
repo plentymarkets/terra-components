161ad3cad69cc532923f3c0455d5ad832bbac5ad
Daniel Trauf <daniel.trauf@plentymarkets.com>
2019-12-13 11:31:05 +0100

    unit tests for container fluid and row div elements

--- src/lib/components/forms/form/form-container/terra-form-container.component.spec.ts
+++ src/lib/components/forms/form/form-container/terra-form-container.component.spec.ts
@@ -8,16 +8,30 @@ import {
     NO_ERRORS_SCHEMA
 } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { TerraFormScope } from '../../../..';
+import { FormGroup } from '@angular/forms';
+import { TerraFormFieldInterface } from '../model/terra-form-field.interface';
+import { TerraFormScope } from '../model/terra-form-scope.data';
 
 fdescribe('TerraFormContainerComponent: ', () =>
 {
     let fixture:ComponentFixture<TerraFormContainerComponent>;
     let component:TerraFormContainerComponent;
+
+    const formFields:{ [key:string]:TerraFormFieldInterface } = {
+        control1: {
+            type:         'text',
+            defaultValue: 'one'
+        },
+        control2: {
+            type:         'text',
+            defaultValue: 'two'
+        }
+    };
+
     beforeEach(() =>
     {
         fixture = TestBed.configureTestingModule({
-            schemas: [NO_ERRORS_SCHEMA],
+            schemas:      [NO_ERRORS_SCHEMA],
             declarations: [TerraFormContainerComponent]
         }).createComponent(TerraFormContainerComponent);
 
@@ -28,6 +42,10 @@ fdescribe('TerraFormContainerComponent: ', () =>
     {
         // initialisation of the component's mandatory inputs
         component.inputScope = new TerraFormScope();
+
+        component.inputFormFields = formFields;
+        component.width = 'col-12';
+        component.inputFormGroup = new FormGroup({});
 
         fixture.detectChanges();
     });
@@ -40,12 +58,13 @@ fdescribe('TerraFormContainerComponent: ', () =>
     it('form entries should be wrapped by div with classes `container-fluid` and `row`', () =>
     {
         let debugElement:DebugElement = fixture.debugElement;
-        let containerFluidDebugElements:Array<DebugElement> = debugElement.queryAll(By.css('div'));
+        let formDebugElement:DebugElement = debugElement.query(By.css('div'));
+        let containerFluidDebugElement:DebugElement = formDebugElement.query(By.css('div.container-fluid'));
 
-        // containerFluidDebugElements.some((element:DebugElement) =>
-        // {
-        //     element;
-        // });
-        // expect(containerFluidDebugElement).toBeDefined();
+        expect(containerFluidDebugElement).toBeTruthy();
+
+        let rowDebugElement:DebugElement = containerFluidDebugElement.query(By.css('div.row'));
+
+        expect(rowDebugElement).toBeTruthy();
     });
 });
