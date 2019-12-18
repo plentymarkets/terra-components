6d0f4f67226e1cefad55d1079d82a20ebc86a6c1
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-12-18 10:09:09 +0100

    add dependencies to the providers to make them actually work

--- src/lib/http-interceptors/index.ts
+++ src/lib/http-interceptors/index.ts
@@ -3,11 +3,22 @@ import { Provider } from '@angular/core';
 import { AuthInterceptor } from './auth.interceptor';
 import { ErrorInterceptor } from './error.interceptor';
 import { LoadingInterceptor } from './loading.interceptor';
+import {
+    AlertService,
+    TerraLoadingSpinnerService
+} from '..';
+import {
+    LocaleService,
+    TranslationService
+} from 'angular-l10n';
 
 export { AuthInterceptor } from './auth.interceptor';
 export { ErrorInterceptor } from './error.interceptor';
 export { LoadingInterceptor } from './loading.interceptor';
 
+/**
+ * @description List of providers for all http interceptors
+ */
 export const httpInterceptorProviders:Array<Provider> = [
     {
         provide:  HTTP_INTERCEPTORS,
@@ -17,11 +28,13 @@ export const httpInterceptorProviders:Array<Provider> = [
     {
         provide:  HTTP_INTERCEPTORS,
         useClass: ErrorInterceptor,
-        multi:    true
+        multi:    true,
+        deps:     [AlertService, TranslationService, LocaleService]
     },
     {
         provide:  HTTP_INTERCEPTORS,
         useClass: LoadingInterceptor,
-        multi:    true
+        multi:    true,
+        deps:     [TerraLoadingSpinnerService]
     }
 ];
