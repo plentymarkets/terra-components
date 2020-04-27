e5a45f62ceeb2f32728285b935c4642e9759d961
Michael Frank <m_frank84@outlook.de>
2020-04-27 12:30:49 +0200

    make src/lib/components/note prettier

--- src/lib/components/note/terra-note.component.ts
+++ src/lib/components/note/terra-note.component.ts
@@ -1,24 +1,20 @@
-import {
-    Component,
-    Input
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector:    'terra-note',
-    styleUrls:   ['./terra-note.component.scss'],
-    templateUrl: './terra-note.component.html'
+  selector: 'terra-note',
+  styleUrls: ['./terra-note.component.scss'],
+  templateUrl: './terra-note.component.html'
 })
-export class TerraNoteComponent
-{
-    @Input()
-    public inputIsSelected:boolean;
+export class TerraNoteComponent {
+  @Input()
+  public inputIsSelected: boolean;
 
-    @Input()
-    public inputId:number;
+  @Input()
+  public inputId: number;
 
-    @Input()
-    public inputHeaderText:string;
+  @Input()
+  public inputHeaderText: string;
 
-    @Input()
-    public inputNoteText:string;
+  @Input()
+  public inputNoteText: string;
 }
