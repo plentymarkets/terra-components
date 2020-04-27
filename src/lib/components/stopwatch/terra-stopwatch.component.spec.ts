81086d1f951af29c4973184232ad1442253bc93b
Michael Frank <m_frank84@outlook.de>
2020-04-27 12:31:28 +0200

    make src/lib/components/stopwatch prettier

--- src/lib/components/stopwatch/terra-stopwatch.component.spec.ts
+++ src/lib/components/stopwatch/terra-stopwatch.component.spec.ts
@@ -1,11 +1,11 @@
 import { TerraStopwatchComponent } from './terra-stopwatch.component';
 import {
-    async,
-    ComponentFixture,
-    discardPeriodicTasks,
-    fakeAsync,
-    TestBed,
-    tick
+  async,
+  ComponentFixture,
+  discardPeriodicTasks,
+  fakeAsync,
+  TestBed,
+  tick
 } from '@angular/core/testing';
 import { TerraButtonComponent } from '../buttons/button/terra-button.component';
 import { FormsModule } from '@angular/forms';
@@ -17,147 +17,126 @@ import { Router } from '@angular/router';
 import { MockRouter } from '../../testing/mock-router';
 import Spy = jasmine.Spy;
 
-describe('Component: TerraStopwatchComponent', () =>
-{
-    let component:TerraStopwatchComponent;
-    let fixture:ComponentFixture<TerraStopwatchComponent>;
-    const ticks:number = 2;
-    const ticksInMilliseconds:number = ticks * 1000 + 1;
-    const router:MockRouter = new MockRouter();
+describe('Component: TerraStopwatchComponent', () => {
+  let component: TerraStopwatchComponent;
+  let fixture: ComponentFixture<TerraStopwatchComponent>;
+  const ticks: number = 2;
+  const ticksInMilliseconds: number = ticks * 1000 + 1;
+  const router: MockRouter = new MockRouter();
 
-    beforeEach(async(() =>
-    {
-        TestBed.configureTestingModule({
-            declarations: [
-                TerraStopwatchComponent,
-                TerraButtonComponent,
-                TooltipDirective
-            ],
-            imports:      [
-                FormsModule,
-                HttpClientModule,
-                LocalizationModule.forRoot(l10nConfig)
-            ],
-            providers:    [
-                {
-                    provide:  Router,
-                    useValue: router
-                }]
-        }).compileComponents();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TerraStopwatchComponent, TerraButtonComponent, TooltipDirective],
+      imports: [FormsModule, HttpClientModule, LocalizationModule.forRoot(l10nConfig)],
+      providers: [
+        {
+          provide: Router,
+          useValue: router
+        }
+      ]
+    }).compileComponents();
+  }));
 
-    beforeEach(() =>
-    {
-        fixture = TestBed.createComponent(TerraStopwatchComponent);
-        component = fixture.componentInstance;
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TerraStopwatchComponent);
+    component = fixture.componentInstance;
+  });
 
-    it('should create', () =>
-    {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should initialise its inputs and outputs', () =>
-    {
-        expect(component.autoPlay).toBe(false);
-        expect(component.isSmall).toBe(false);
-        expect(component.controls).toBe(false);
-        expect(component.isRunning).toBe(false);
-        expect(component.seconds).toBe(0);
-    });
+  it('should initialise its inputs and outputs', () => {
+    expect(component.autoPlay).toBe(false);
+    expect(component.isSmall).toBe(false);
+    expect(component.controls).toBe(false);
+    expect(component.isRunning).toBe(false);
+    expect(component.seconds).toBe(0);
+  });
 
-    it('should update #isRunning when starting, stopping and resetting the watch', () =>
-    {
-        component.start();
-        expect(component.isRunning).toBe(true);
-        component.stop();
-        expect(component.isRunning).toBe(false);
-        component.start();
-        expect(component.isRunning).toBe(true);
-        component.reset();
-        expect(component.isRunning).toBe(false);
-    });
+  it('should update #isRunning when starting, stopping and resetting the watch', () => {
+    component.start();
+    expect(component.isRunning).toBe(true);
+    component.stop();
+    expect(component.isRunning).toBe(false);
+    component.start();
+    expect(component.isRunning).toBe(true);
+    component.reset();
+    expect(component.isRunning).toBe(false);
+  });
 
-    it('should reset seconds value when calling the #reset method', () =>
-    {
-        component.seconds = 2;
-        expect(component.seconds).toBe(2);
-        component.reset();
-        expect(component.seconds).toBe(0);
-    });
+  it('should reset seconds value when calling the #reset method', () => {
+    component.seconds = 2;
+    expect(component.seconds).toBe(2);
+    component.reset();
+    expect(component.seconds).toBe(0);
+  });
 
-    it('should call the #stop method when calling #reset', () =>
-    {
-        let spy:Spy = spyOn(component, 'stop');
-        component.reset();
-        expect(spy).toHaveBeenCalled();
-    });
+  it('should call the #stop method when calling #reset', () => {
+    let spy: Spy = spyOn(component, 'stop');
+    component.reset();
+    expect(spy).toHaveBeenCalled();
+  });
 
-    it('should start the watch when calling the #start method', fakeAsync(() =>
-    {
-        component.start();
-        tick(ticksInMilliseconds);
-        expect(component.seconds).toBe(ticks);
+  it('should start the watch when calling the #start method', fakeAsync(() => {
+    component.start();
+    tick(ticksInMilliseconds);
+    expect(component.seconds).toBe(ticks);
 
-        discardPeriodicTasks();
-    }));
+    discardPeriodicTasks();
+  }));
 
-    it('should start automatically if #autoPlay is true', fakeAsync(() =>
-    {
-        let spy:Spy = spyOn(component, 'start').and.callThrough();
-        component.autoPlay = true;
-        component.ngOnInit();
+  it('should start automatically if #autoPlay is true', fakeAsync(() => {
+    let spy: Spy = spyOn(component, 'start').and.callThrough();
+    component.autoPlay = true;
+    component.ngOnInit();
 
-        expect(spy).toHaveBeenCalled();
-        tick(ticksInMilliseconds);
-        expect(component.seconds).toBeGreaterThan(0);
+    expect(spy).toHaveBeenCalled();
+    tick(ticksInMilliseconds);
+    expect(component.seconds).toBeGreaterThan(0);
 
-        discardPeriodicTasks();
-    }));
+    discardPeriodicTasks();
+  }));
 
-    it('should not start automatically if #autoPlay is false', fakeAsync(() =>
-    {
-        let spy:Spy = spyOn(component, 'start').and.callThrough();
-        component.autoPlay = false;
-        component.ngOnInit();
+  it('should not start automatically if #autoPlay is false', fakeAsync(() => {
+    let spy: Spy = spyOn(component, 'start').and.callThrough();
+    component.autoPlay = false;
+    component.ngOnInit();
 
-        expect(spy).not.toHaveBeenCalled();
-        tick(ticksInMilliseconds);
-        expect(component.seconds).toBe(0);
-    }));
+    expect(spy).not.toHaveBeenCalled();
+    tick(ticksInMilliseconds);
+    expect(component.seconds).toBe(0);
+  }));
 
-    it('should start and stop', fakeAsync(() =>
-    {
-        component.start();
-        tick(ticksInMilliseconds);
-        expect(component.seconds).toBe(ticks);
+  it('should start and stop', fakeAsync(() => {
+    component.start();
+    tick(ticksInMilliseconds);
+    expect(component.seconds).toBe(ticks);
 
-        component.stop();
-        tick(ticksInMilliseconds);
-        expect(component.seconds).toEqual(ticks);
-    }));
+    component.stop();
+    tick(ticksInMilliseconds);
+    expect(component.seconds).toEqual(ticks);
+  }));
 
-    it('should not start the timer again, if it is already running', () =>
-    {
-        spyOn(window, 'setInterval').and.callThrough();
-        component.start();
-        expect(component.isRunning).toBe(true);
+  it('should not start the timer again, if it is already running', () => {
+    spyOn(window, 'setInterval').and.callThrough();
+    component.start();
+    expect(component.isRunning).toBe(true);
 
-        component.start();
-        expect(component.isRunning).toBe(true);
-        expect(window.setInterval).toHaveBeenCalledTimes(1);
-    });
+    component.start();
+    expect(component.isRunning).toBe(true);
+    expect(window.setInterval).toHaveBeenCalledTimes(1);
+  });
 
-    it('should not clear the timer on #stop() if it is not running', () =>
-    {
-        spyOn(window, 'clearInterval').and.callThrough();
-        expect(component.isRunning).toBe(false);
-        component.stop();
-        expect(window.clearInterval).not.toHaveBeenCalled();
+  it('should not clear the timer on #stop() if it is not running', () => {
+    spyOn(window, 'clearInterval').and.callThrough();
+    expect(component.isRunning).toBe(false);
+    component.stop();
+    expect(window.clearInterval).not.toHaveBeenCalled();
 
-        component.start();
-        expect(component.isRunning).toBe(true);
-        component.stop();
-        expect(window.clearInterval).toHaveBeenCalled();
-    });
+    component.start();
+    expect(component.isRunning).toBe(true);
+    component.stop();
+    expect(window.clearInterval).toHaveBeenCalled();
+  });
 });
--- src/lib/components/stopwatch/terra-stopwatch.component.ts
+++ src/lib/components/stopwatch/terra-stopwatch.component.ts
@@ -1,166 +1,146 @@
-import {
-    Component,
-    EventEmitter,
-    Input,
-    OnDestroy,
-    OnInit,
-    Output
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Language } from 'angular-l10n';
 import { isNullOrUndefined } from 'util';
 
 @Component({
-    selector: 'terra-stopwatch',
-    styleUrls: [ './terra-stopwatch.component.scss'],
-    templateUrl: './terra-stopwatch.component.html',
+  selector: 'terra-stopwatch',
+  styleUrls: ['./terra-stopwatch.component.scss'],
+  templateUrl: './terra-stopwatch.component.html'
 })
-export class TerraStopwatchComponent implements OnInit, OnDestroy
-{
-    /**
-     * @description If true, the start, pause and reset control will show
-     */
-    @Input()
-    public controls:boolean = false;
+export class TerraStopwatchComponent implements OnInit, OnDestroy {
+  /**
+   * @description If true, the start, pause and reset control will show
+   */
+  @Input()
+  public controls: boolean = false;
 
-    /**
-     * @description If true, stopwatch starts if component initialize
-     */
-    @Input()
-    public autoPlay:boolean = false;
+  /**
+   * @description If true, stopwatch starts if component initialize
+   */
+  @Input()
+  public autoPlay: boolean = false;
 
-    /**
-     * @description If true, buttons are small
-     */
-    @Input()
-    public isSmall:boolean = false;
+  /**
+   * @description If true, buttons are small
+   */
+  @Input()
+  public isSmall: boolean = false;
 
-    /**
-     * @description set the current value of the stopwatch
-     */
-    @Input()
-    public set seconds(value:number)
-    {
-        this._secondsValue = value;
-        this.secondsChange.emit(this.seconds);
+  /**
+   * @description set the current value of the stopwatch
+   */
+  @Input()
+  public set seconds(value: number) {
+    this._secondsValue = value;
+    this.secondsChange.emit(this.seconds);
+  }
+
+  /**
+   * @description returns the current value of the stopwatch
+   */
+  public get seconds(): number {
+    return this._secondsValue || 0;
+  }
+
+  /**
+   * @description notification if the current value of the stopwatch changes. If the stopwatch is running it emits a new value every
+   *     single second.
+   */
+  @Output()
+  public secondsChange: EventEmitter<number> = new EventEmitter<number>();
+
+  @Language()
+  public _lang: string;
+
+  public readonly _langPrefix: string = 'terraStopwatch.';
+
+  private _timer: number = null;
+  private _secondsValue: number = 0;
+
+  /**
+   * @description initialisation routine. Starts the stopwatch if autoPlay is set.
+   */
+  public ngOnInit(): void {
+    if (this.autoPlay) {
+      this.start();
     }
+  }
 
-    /**
-     * @description returns the current value of the stopwatch
-     */
-    public get seconds():number
-    {
-        return this._secondsValue || 0;
+  public ngOnDestroy(): void {
+    // implementation is required by angular-l10n. See https://robisim74.github.io/angular-l10n/spec/getting-the-translation/#messages
+  }
+
+  /**
+   * @description states whether the stop watch is currently running
+   */
+  public get isRunning(): boolean {
+    return !isNullOrUndefined(this._timer);
+  }
+
+  /**
+   * @description starts the stopwatch
+   */
+  public start(): void {
+    if (!this.isRunning) {
+      this._timer = window.setInterval(() => this.incrementSeconds(), 1000);
     }
+  }
 
-    /**
-     * @description notification if the current value of the stopwatch changes. If the stopwatch is running it emits a new value every
-     *     single second.
-     */
-    @Output()
-    public secondsChange:EventEmitter<number> = new EventEmitter<number>();
-
-    @Language()
-    public _lang:string;
-
-    public readonly _langPrefix:string = 'terraStopwatch.';
-
-    private _timer:number = null;
-    private _secondsValue:number = 0;
-
-    /**
-     * @description initialisation routine. Starts the stopwatch if autoPlay is set.
-     */
-    public ngOnInit():void
-    {
-        if(this.autoPlay)
-        {
-            this.start();
-        }
+  /**
+   * @description stops the stopwatch
+   */
+  public stop(): void {
+    if (this.isRunning) {
+      window.clearInterval(this._timer);
+      this._timer = null;
     }
+  }
 
-    public ngOnDestroy():void
-    {
-        // implementation is required by angular-l10n. See https://robisim74.github.io/angular-l10n/spec/getting-the-translation/#messages
-    }
+  /**
+   * @description resets the stopwatch
+   */
+  public reset(): void {
+    this.stop();
+    this.seconds = 0;
+  }
 
-    /**
-     * @description states whether the stop watch is currently running
-     */
-    public get isRunning():boolean
-    {
-        return !isNullOrUndefined(this._timer);
-    }
+  /**
+   * @description returns the current stopwatch value in a time string format
+   */
+  public get _timeString(): string {
+    return this.format(this.seconds);
+  }
 
-    /**
-     * @description starts the stopwatch
-     */
-    public start():void
-    {
-        if(!this.isRunning)
-        {
-            this._timer = window.setInterval(() => this.incrementSeconds(), 1000);
-        }
-    }
+  private format(timerSeconds: number): string {
+    let hours: number = this.getHours(timerSeconds);
+    let minutes: number = this.getMinutes(timerSeconds);
+    let seconds: number = this.getSeconds(timerSeconds);
+    return (
+      this.getDigitString(hours) +
+      ':' +
+      this.getDigitString(minutes) +
+      ':' +
+      this.getDigitString(seconds)
+    );
+  }
 
-    /**
-     * @description stops the stopwatch
-     */
-    public stop():void
-    {
-        if(this.isRunning)
-        {
-            window.clearInterval(this._timer);
-            this._timer = null;
-        }
-    }
+  private getDigitString(value: number): string {
+    return (value < 10 ? '0' : '') + value;
+  }
 
-    /**
-     * @description resets the stopwatch
-     */
-    public reset():void
-    {
-        this.stop();
-        this.seconds = 0;
-    }
+  private getHours(seconds: number): number {
+    return Math.floor((seconds / 3600) % 24);
+  }
 
-    /**
-     * @description returns the current stopwatch value in a time string format
-     */
-    public get _timeString():string
-    {
-        return this.format(this.seconds);
-    }
+  private getMinutes(seconds: number): number {
+    return Math.floor((seconds / 60) % 60);
+  }
 
-    private format(timerSeconds:number):string
-    {
-        let hours:number = this.getHours(timerSeconds);
-        let minutes:number = this.getMinutes(timerSeconds);
-        let seconds:number = this.getSeconds(timerSeconds);
-        return this.getDigitString(hours) + ':' + this.getDigitString(minutes) + ':' + this.getDigitString(seconds);
-    }
+  private getSeconds(seconds: number): number {
+    return seconds % 60;
+  }
 
-    private getDigitString(value:number):string
-    {
-        return (value < 10 ? '0' : '') + value;
-    }
-
-    private getHours(seconds:number):number
-    {
-        return Math.floor(seconds / 3600 % 24);
-    }
-
-    private getMinutes(seconds:number):number
-    {
-        return Math.floor(seconds / 60 % 60);
-    }
-
-    private getSeconds(seconds:number):number
-    {
-        return seconds % 60;
-    }
-
-    private incrementSeconds():void
-    {
-        this.seconds += 1;
-    }
+  private incrementSeconds(): void {
+    this.seconds += 1;
+  }
 }
