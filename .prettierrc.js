c3395397ecbeef3f35d66e5fe76979f4d0a5f274
Michael Frank <m_frank84@outlook.de>
2020-04-27 09:34:38 +0200

    add prettier and a config

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: 'typescript',
+    //filePath: '', // path to a parser...?
+    printWidth: 100, // d=80
+    tabWidth: 2, // d
+    useTabs: false, // d
+    semi: true, // d
+    singleQuote: true, // d=false
+    quoteProps: 'as-needed', // d
+    jsxSingleQuote: false, // d
+    trailingComma: 'none', // d='es5'
+    bracketSpacing: true, // d
+    jsxBracketSameLine: true, // d=false
+    arrowParens: 'always', // d
+    rangeStart: 0, // d
+    rangeEnd: Infinity, // d
+    requirePragma: false, // d requires (or not) an indicator which tells if the file should be reformatted
+    insertPragma: false, // d add an Pragma, if the file is formatted
+    proseWrap: 'preserve', // d
+    htmlWhitespaceSensitivity: 'css', // d
+    // vueIndentScriptAndStyle: false // d only useful for vue...
+}
