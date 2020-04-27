e4575f19a7bdb0c3ed8c2a88ba875224974d2fa5
Michael Frank <m_frank84@outlook.de>
2020-04-27 12:17:54 +0200

    make src/lib/utils prettier

--- src/lib/utils/http-param-encoder.ts
+++ src/lib/utils/http-param-encoder.ts
@@ -1,21 +1,15 @@
-import {
-    HttpParameterCodec,
-    HttpUrlEncodingCodec
-} from '@angular/common/http';
+import { HttpParameterCodec, HttpUrlEncodingCodec } from '@angular/common/http';
 
 /** @description A custom encoder for http params. */
-class HttpParamEncoder extends HttpUrlEncodingCodec
-{
-    public encodeKey(key:string):string
-    {
-        return encodeURIComponent(key);
-    }
+class HttpParamEncoder extends HttpUrlEncodingCodec {
+  public encodeKey(key: string): string {
+    return encodeURIComponent(key);
+  }
 
-    public encodeValue(value:string):string
-    {
-        return encodeURIComponent(value);
-    }
+  public encodeValue(value: string): string {
+    return encodeURIComponent(value);
+  }
 }
 
 /** @description Instance of HttpParamEncoder, a custom encoder for http params. */
-export const httpParamEncoder:HttpParameterCodec = new HttpParamEncoder();
+export const httpParamEncoder: HttpParameterCodec = new HttpParamEncoder();
--- src/lib/utils/http-params.spec.ts
+++ src/lib/utils/http-params.spec.ts
@@ -2,46 +2,41 @@ import { createHttpParams } from './http-params';
 import { HttpParams } from '@angular/common/http';
 import { Params } from '@angular/router';
 
-describe(`httpParams:`, () =>
-{
-    it(`should return empty instance of httpParams if params is null or undefined`, () =>
-    {
-        let httpParams:HttpParams = createHttpParams(null);
-        expect(httpParams).toBeDefined();
-        expect(httpParams instanceof HttpParams).toBe(true);
-        expect(httpParams.keys().length).toBe(0);
-    });
+describe(`httpParams:`, () => {
+  it(`should return empty instance of httpParams if params is null or undefined`, () => {
+    let httpParams: HttpParams = createHttpParams(null);
+    expect(httpParams).toBeDefined();
+    expect(httpParams instanceof HttpParams).toBe(true);
+    expect(httpParams.keys().length).toBe(0);
+  });
 
-    it(`should return the expected params`, () =>
-    {
-        let params:Params = {
-            type:  'long',
-            value: 2
-        };
-        let httpParams:HttpParams = createHttpParams(params);
-        expect(httpParams.keys().length).toBe(2);
-        expect(httpParams.has('type')).toBe(true);
-        expect(httpParams.has('value')).toBe(true);
-    });
+  it(`should return the expected params`, () => {
+    let params: Params = {
+      type: 'long',
+      value: 2
+    };
+    let httpParams: HttpParams = createHttpParams(params);
+    expect(httpParams.keys().length).toBe(2);
+    expect(httpParams.has('type')).toBe(true);
+    expect(httpParams.has('value')).toBe(true);
+  });
 
-    it(`should return httpParams in the form 'list=a,b,c' `, () =>
-    {
-        const array:Array<string> = ['a', 'b', 'c'];
-        let params:Params = {
-            type:  array
-        };
-        let httpParams:HttpParams = createHttpParams(params, false);
-        expect(httpParams.get('type')).toEqual(array);
-    });
+  it(`should return httpParams in the form 'list=a,b,c' `, () => {
+    const array: Array<string> = ['a', 'b', 'c'];
+    let params: Params = {
+      type: array
+    };
+    let httpParams: HttpParams = createHttpParams(params, false);
+    expect(httpParams.get('type')).toEqual(array);
+  });
 
-    it(`should return httpParams in the form list[]=a&list[]=b&list[]=c' `, () =>
-    {
-        const array:Array<string> = ['a', 'b', 'c'];
-        let params:Params = {
-            type:  array
-        };
-        let httpParams:HttpParams = createHttpParams(params, true);
-        const values:Array<string> = httpParams.getAll('type[]');
-        expect(values).toEqual(array);
-    });
+  it(`should return httpParams in the form list[]=a&list[]=b&list[]=c' `, () => {
+    const array: Array<string> = ['a', 'b', 'c'];
+    let params: Params = {
+      type: array
+    };
+    let httpParams: HttpParams = createHttpParams(params, true);
+    const values: Array<string> = httpParams.getAll('type[]');
+    expect(values).toEqual(array);
+  });
 });
