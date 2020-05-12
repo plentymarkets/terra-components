671836d4a2ad0f7e31d45e85934e7a10a62e38b5
Michael Frank <m_frank84@outlook.de>
2020-05-12 14:10:43 +0200

    add table paging data source

--- src/lib/service/table/table-data-source.ts
+++ src/lib/service/table/table-data-source.ts
@@ -17,13 +17,13 @@ export abstract class TableDataSource<T> extends DataSource<T>
 {
     public data:Array<T> = [];
 
-    private search$:Subject<void> = new Subject();
-    private disconnect$:Subject<void> = new Subject();
+    protected _search$:Subject<void> = new Subject();
+    protected _disconnect$:Subject<void> = new Subject();
 
     public connect(collectionViewer:CollectionViewer):Observable<Array<T>>
     {
-        return this.search$.pipe(
-            takeUntil(this.disconnect$),
+        return this._search$.pipe(
+            takeUntil(this._disconnect$),
             debounceTime(400),
             switchMap(() => this.request()),
             tap((data:Array<T>) => this.data = data)
@@ -32,13 +32,13 @@ export abstract class TableDataSource<T> extends DataSource<T>
 
     public disconnect(collectionViewer:CollectionViewer):void
     {
-        this.disconnect$.next();
-        this.disconnect$.complete();
+        this._disconnect$.next();
+        this._disconnect$.complete();
     }
 
     public search():void
     {
-        this.search$.next();
+        this._search$.next();
     }
 
     public abstract request():Observable<Array<T>>;
---
+++ src/lib/service/table/table-paging-data-source.ts
@@ -0,0 +1,48 @@
+import { TableDataSource } from './table-data-source';
+import {
+    EMPTY,
+    Observable
+} from 'rxjs';
+import { CollectionViewer } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { merge } from 'rxjs';
+import {
+    debounceTime,
+    switchMap,
+    takeUntil,
+    tap
+} from 'rxjs/operators';
+
+export abstract class TablePagingDataSource<T> extends TableDataSource<T>
+{
+    public paginator:MatPaginator;
+
+    public connect(collectionViewer:CollectionViewer):Observable<Array<T>>
+    {
+        return merge(
+            this._search$,
+            this.paginator ? this.paginator.page : EMPTY
+        ).pipe(
+            takeUntil(this._disconnect$),
+            debounceTime(400),
+            tap(() => console.log(this.pageIndex, this.itemsPerPage)),
+            switchMap(() => this.request()),
+            tap((data:Array<T>) => this.data = data)
+        );
+    }
+
+    public disconnect(collectionViewer:CollectionViewer):void
+    {
+        super.disconnect(collectionViewer);
+    }
+
+    public get pageIndex():number
+    {
+        return this.paginator ? this.paginator.pageIndex + 1 : undefined;
+    }
+
+    public get itemsPerPage():number
+    {
+        return this.paginator ? this.paginator.pageSize : undefined;
+    }
+}
