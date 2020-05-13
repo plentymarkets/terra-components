0cc7e95983d3a7ff268c4257c6d9ef546dfa206c
Michael Frank <m_frank84@outlook.de>
2020-05-13 08:40:16 +0200

    add jsdoc

--- src/lib/service/table/table-paging-data-source.ts
+++ src/lib/service/table/table-paging-data-source.ts
@@ -9,22 +9,38 @@ import {
 } from '@angular/material/paginator';
 import { EventEmitter } from '@angular/core';
 
+/**
+ * Base class for a data when a pagination is needed.
+ * @extends TableDataSource<T>
+ */
 export abstract class TablePagingDataSource<T> extends TableDataSource<T>
 {
     public paginator:MatPaginator;
 
-    protected paging():Observable<never> | EventEmitter<PageEvent>
-    {
-        return this.paginator ? this.paginator.page : EMPTY;
-    }
-
+    /**
+     * Get the current page index or undefined if no paginator is specified
+     * @returns {number}
+     */
     public get pageIndex():number
     {
         return this.paginator ? this.paginator.pageIndex + 1 : undefined;
     }
 
+    /**
+     * Get the current items pet page or undefined if no paginator is specified
+     * @returns {number}
+     */
     public get itemsPerPage():number
     {
         return this.paginator ? this.paginator.pageSize : undefined;
+    }
+
+    /**
+     * @override
+     * Return the paginator or an empty observable
+     */
+    protected paging():Observable<never> | EventEmitter<PageEvent>
+    {
+        return this.paginator ? this.paginator.page : EMPTY;
     }
 }
