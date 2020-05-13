6b0906e3104195421c59e25be00a881ae3f4d7a2
Michael Kunze <michael.kunze@plentymarkets.com>
2020-05-13 10:09:37 +0200

    Update table-paging-sorting-data-source.ts

--- src/lib/service/table/table-paging-sorting-data-source.ts
+++ src/lib/service/table/table-paging-sorting-data-source.ts
@@ -13,6 +13,8 @@ import {
     Observable
 } from 'rxjs';
 import { EventEmitter } from '@angular/core';
+import { CollectionViewer } from '@angular/cdk/collections';
+import { takeUntil } from 'rxjs/operators';
 
 /**
  * Data Source base class for a data table with pagination and sorting.
@@ -29,6 +31,27 @@ export abstract class TablePagingSortingDataSource<T> extends TableDataSource<T>
     public sort:MatSort;
 
     /**
+     * @description Connects a collection viewer (such as a data-table) to this data source. Note that
+     * the stream provided will be accessed during change detection and should not directly change
+     * values that are bound in template views.
+     * @param collectionViewer The component that exposes a view over the data provided by this
+     *     data source.
+     * @returns Observable that emits a new value when the data changes.
+     */
+    public connect(collectionViewer:CollectionViewer):Observable<Array<T>>
+    {
+        // If the user changes the sort order, reset back to the first page.
+        if(this.sort)
+        {
+            this.sort.sortChange.pipe(
+                takeUntil(this._disconnect$)
+            ).subscribe(() => this.paginator.pageIndex = 0);
+        }
+
+        return super.connect(collectionViewer);
+    }
+
+    /**
      * @description Return the page event or an empty observable.
      * @override
      * @returns EventEmitter<PageEvent> or Observable<never>
@@ -37,6 +60,7 @@ export abstract class TablePagingSortingDataSource<T> extends TableDataSource<T>
     {
         return this.paginator ? this.paginator.page : EMPTY;
     }
+
     /**
      * @description Return the sort event or an empty observable.
      * @returns EventEmitter<Sort> or Observable<never>
