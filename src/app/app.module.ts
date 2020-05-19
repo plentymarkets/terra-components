95a4ec98f23a28231e1ef2d46c4d082100401a0e
Michael Frank <m_frank84@outlook.de>
2020-05-19 10:32:01 +0200

    run prettier in src/app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,5 @@
-import {
-    APP_INITIALIZER,
-    NgModule
-} from '@angular/core';
-import {
-    L10nLoader,
-    LocalizationModule
-} from 'angular-l10n';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { L10nLoader, LocalizationModule } from 'angular-l10n';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,9 +9,8 @@ import { ShowcaseComponent } from './showcase/showcase.component';
 import { TerraComponentsExamplesModule } from '../lib/terra-components-examples.module';
 import { RouterModule } from '@angular/router';
 
-export function initL10n(l10nLoader:L10nLoader):Function
-{
-    return ():Promise<void> => l10nLoader.load();
+export function initL10n(l10nLoader: L10nLoader): Function {
+    return (): Promise<void> => l10nLoader.load();
 }
 
 /**
@@ -26,7 +19,7 @@ export function initL10n(l10nLoader:L10nLoader):Function
  * NOTE: It is not publicly accessible either.
  */
 @NgModule({
-    imports:      [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot([]),
@@ -34,19 +27,15 @@ export function initL10n(l10nLoader:L10nLoader):Function
         LocalizationModule.forRoot(l10nConfig),
         TerraComponentsExamplesModule
     ],
-    declarations: [
-        AppComponent,
-        ShowcaseComponent
-    ],
-    providers:    [
+    declarations: [AppComponent, ShowcaseComponent],
+    providers: [
         {
-            provide:    APP_INITIALIZER,
+            provide: APP_INITIALIZER,
             useFactory: initL10n,
-            deps:       [L10nLoader],
-            multi:      true
+            deps: [L10nLoader],
+            multi: true
         }
     ],
-    bootstrap:    [AppComponent]
+    bootstrap: [AppComponent]
 })
-export class AppModule
-{}
+export class AppModule {}
