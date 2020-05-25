ab7d2bd2055d8dfa2df4833d4b7b10d9dfd707d9
Tim Wieder <tim.wieder@plentymarkets.com>
2020-05-25 13:24:54 +0200

    - added tests for data-source

--- src/lib/components/table/table-data-source.spec.ts
+++ src/lib/components/table/table-data-source.spec.ts
@@ -1,15 +1,15 @@
 import { TableDataSource } from './table-data-source';
 import { RequestParameterInterface } from './request-parameter.interface';
-import { TerraPagerInterface } from '../pager/data/terra-pager.interface';
 import {
     Observable,
     of
 } from 'rxjs';
 import { TerraFilter } from './filter';
+import Spy = jasmine.Spy;
 
 class TestDataSource extends TableDataSource<{}>
 {
-    public request(requestParams:RequestParameterInterface):Observable<Array<{}>> | Observable<TerraPagerInterface<{}>>
+    public request(requestParams:RequestParameterInterface):Observable<Array<{}>>
     {
         return of(new Array({}));
     }
@@ -20,7 +20,7 @@ interface FilterParams
     id:number;
 }
 
-describe('TableDataSource', () =>
+fdescribe('TableDataSource', () =>
 {
     let dataSource:TestDataSource;
     let filter:TerraFilter<FilterParams>;
@@ -29,6 +29,7 @@ describe('TableDataSource', () =>
     {
         dataSource = new TestDataSource();
         filter = new TerraFilter<FilterParams>();
+        dataSource.filter = filter;
     });
 
     it('should create', () =>
@@ -52,5 +53,49 @@ describe('TableDataSource', () =>
     it('should have data after search', () =>
     {
         dataSource.connect(undefined).subscribe((result:Array<{}>) => expect(result).toBe([]));
+    });
+
+    it('data should be refreshes after a request', () =>
+    {
+        dataSource.request({}).subscribe((result:Array<{}>) =>
+        {
+            dataSource.data = result;
+            expect(dataSource.data).toEqual([{}]);
+        });
+    });
+
+    it('request should be triggered after search method has been called', () =>
+    {
+        let spy:Spy = spyOn(dataSource, 'request');
+
+        dataSource.search();
+
+        dataSource.filter.search$.subscribe(() =>
+        {
+            expect(spy).toHaveBeenCalled();
+        });
+    });
+
+    // xit('request should be triggered after filter.search has emitted', () =>
+    // {
+    //    dataSource.data = [{}];
+    //
+    //    dataSource.filter.search();
+    //
+    //    expect(dataSource.request({})).toHaveBeenCalled();
+    // });
+
+    it('should kill the observables on disconnect', () =>
+    {
+        dataSource.data = [{id:123}];
+
+        let spy:Spy = spyOn(dataSource, 'request');
+
+        dataSource.disconnect(undefined);
+
+        dataSource.search();
+
+        expect(spy).not.toHaveBeenCalled();
+        expect(dataSource.data).toEqual([{id:123}]);
     });
 });
