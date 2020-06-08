09359d91be79cff1b955524292684341932c4b8c
Michael Frank <m_frank84@outlook.de>
2020-06-08 09:02:51 +0200

    feature(prettier): apply prettier to src/lib/testing

--- src/lib/testing/mock-change-detector-ref.ts
+++ src/lib/testing/mock-change-detector-ref.ts
@@ -1,15 +1,14 @@
 import { ChangeDetectorRef } from '@angular/core';
 import { noop } from 'rxjs';
 
-export class MockChangeDetectorRef extends ChangeDetectorRef
-{
-    public markForCheck:() => void = noop;
+export class MockChangeDetectorRef extends ChangeDetectorRef {
+    public markForCheck: () => void = noop;
 
-    public checkNoChanges:() => void = noop;
+    public checkNoChanges: () => void = noop;
 
-    public detach:() => void = noop;
+    public detach: () => void = noop;
 
-    public detectChanges:() => void = noop;
+    public detectChanges: () => void = noop;
 
-    public reattach:() => void = noop;
+    public reattach: () => void = noop;
 }
