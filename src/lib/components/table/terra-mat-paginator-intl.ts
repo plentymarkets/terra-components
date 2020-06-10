18b13090e720811b28c2ff759e2258aa020e77c4
ChristianMoll <christian.moll@plentymarkets.com>
2020-06-10 10:29:00 +0200

    add terra mat paginator intl

---
+++ src/lib/components/table/terra-mat-paginator-intl.ts
@@ -0,0 +1,43 @@
+import { MatPaginatorIntl } from '@angular/material';
+import { Injectable } from '@angular/core';
+import {
+    TranslationService
+} from 'angular-l10n';
+
+@Injectable()
+export class TerraMatPaginatorIntl extends MatPaginatorIntl
+{
+    constructor(private translation:TranslationService)
+    {
+        super();
+        // this.translate.onLangChange.subscribe((e:Event) =>
+        // {
+        //     this.getAndInitTranslations();
+        // });
+        this.getAndInitTranslations();
+    }
+
+    private getAndInitTranslations():void
+    {
+        this.itemsPerPageLabel = this.translation.translate('terraMatPaginatorIntl.itemsPerPage');
+        this.nextPageLabel = this.translation.translate('terraMatPaginatorIntl.nextPage');
+        this.previousPageLabel = this.translation.translate('terraMatPaginatorIntl.previousPage');
+        this.firstPageLabel = this.translation.translate('terraMatPaginatorIntl.firstPage');
+        this.lastPageLabel = this.translation.translate('terraMatPaginatorIntl.lastPage');
+        this.changes.next();
+    }
+
+    public getRangeLabel = (page:number, pageSize:number, length:number):string =>
+    {
+        let ofLabel:string = this.translation.translate('terraMatPaginatorIntl.ofLabel');
+
+        if(length === 0 || pageSize === 0)
+        {
+            return `0 / ${length}`;
+        }
+        length = Math.max(length, 0);
+        const startIndex:number = page * pageSize;
+        const endIndex:number = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+        return startIndex + 1 + ' - ' + endIndex + ' ' + ofLabel + ' ' + length;
+    }
+}
