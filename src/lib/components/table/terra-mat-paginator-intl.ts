2bc09d50d6d84993591674be92a8f96c41fab473
ChristianMoll <christian.moll@plentymarkets.com>
2020-06-12 19:05:42 +0200

    linting

--- src/lib/components/table/terra-mat-paginator-intl.ts
+++ src/lib/components/table/terra-mat-paginator-intl.ts
@@ -12,18 +12,8 @@ export class TerraMatPaginatorIntl extends MatPaginatorIntl
         super();
         this.translation.translationChanged().subscribe(() =>
         {
-            this.getAndInitTranslations()
+            this.getAndInitTranslations();
         });
-    }
-
-    private getAndInitTranslations():void
-    {
-        this.itemsPerPageLabel = this.translation.translate('terraMatPaginatorIntl.itemsPerPage');
-        this.nextPageLabel = this.translation.translate('terraMatPaginatorIntl.nextPage');
-        this.previousPageLabel = this.translation.translate('terraMatPaginatorIntl.previousPage');
-        this.firstPageLabel = this.translation.translate('terraMatPaginatorIntl.firstPage');
-        this.lastPageLabel = this.translation.translate('terraMatPaginatorIntl.lastPage');
-        this.changes.next();
     }
 
     public getRangeLabel = (page:number, pageSize:number, length:number):string =>
@@ -38,5 +28,15 @@ export class TerraMatPaginatorIntl extends MatPaginatorIntl
         const startIndex:number = page * pageSize;
         const endIndex:number = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
         return `${startIndex + 1} – ${endIndex} ${ofLabel} ${length}`;
+    }
+
+    private getAndInitTranslations():void
+    {
+        this.itemsPerPageLabel = this.translation.translate('terraMatPaginatorIntl.itemsPerPage');
+        this.nextPageLabel = this.translation.translate('terraMatPaginatorIntl.nextPage');
+        this.previousPageLabel = this.translation.translate('terraMatPaginatorIntl.previousPage');
+        this.firstPageLabel = this.translation.translate('terraMatPaginatorIntl.firstPage');
+        this.lastPageLabel = this.translation.translate('terraMatPaginatorIntl.lastPage');
+        this.changes.next();
     }
 }
