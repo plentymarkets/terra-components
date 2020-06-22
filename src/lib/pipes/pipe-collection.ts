ded3140d05230aab826e98d23cb00237ea143afc
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-06-22 14:05:25 +0200

    move pipe-collection

--- src/lib/pipes/pipe-collection.ts
+++ src/lib/pipes/pipe-collection.ts
@@ -1,5 +1,5 @@
 import { Type } from '@angular/core';
-import { SelectSortPipe } from '../pipes/select-sort.pipe';
+import { SelectSortPipe } from './select-sort.pipe';
 
 export const pipes:Array<Type<any>> = [
     SelectSortPipe
