66def8d7ccddde4655fc8af67086a3feba152c86
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-06-22 13:50:20 +0200

    fix unit tests for SelectSortDirective

--- src/lib/components/forms/select-box/directive/select-sort.directive.spec.ts
+++ src/lib/components/forms/select-box/directive/select-sort.directive.spec.ts
@@ -1,26 +1,16 @@
-import Spy = jasmine.Spy;
 import { SimpleChange } from '@angular/core';
-import {
-    ComponentFixture,
-    TestBed
-} from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { LocalizationModule } from 'angular-l10n';
 import { SelectSortDirective } from './select-sort.directive';
-import { By } from '@angular/platform-browser';
-import { TerraSelectBoxComponent } from '../terra-select-box.component';
 import {
     SortHelper,
-    TerraSelectBoxValueInterface,
-    TooltipDirective
+    TerraSelectBoxValueInterface
 } from '../../../..';
+import Spy = jasmine.Spy;
+
 
 /* tslint:disable-next-line:max-function-line-count */
 describe('SelectSortDirective: ', () =>
 {
-    let component:TerraSelectBoxComponent;
     let directive:SelectSortDirective;
-    let fixture:ComponentFixture<TerraSelectBoxComponent>;
 
     const listBoxValue1:TerraSelectBoxValueInterface = {
         caption: 'Value 01',
@@ -37,74 +27,57 @@ describe('SelectSortDirective: ', () =>
 
     beforeEach(() =>
     {
-        TestBed.configureTestingModule({
-            imports:      [
-                FormsModule,
-                LocalizationModule
-            ],
-            declarations: [
-                TooltipDirective,
-                TerraSelectBoxComponent,
-                SelectSortDirective
-            ]
-        }).compileComponents();
-    });
+        directive = new SelectSortDirective();
 
-    beforeEach(() =>
-    {
-        fixture = TestBed.createComponent(TerraSelectBoxComponent);
-        component = fixture.componentInstance;
-        directive = fixture.debugElement.query(By.directive(SelectSortDirective)).nativeElement as SelectSortDirective;
-
-        component.inputListBoxValues = []; // this also resets the selectedValue to null
+        directive.inputListBoxValues = []; // this also resets the selectedValue to null
     });
 
     it(`should enable sorting by default`, () =>
     {
         const spy:Spy = spyOn(SortHelper, 'sortArray').and.callThrough();
         expect(directive.disableSorting).toBe(false);
-        component.inputListBoxValues = [listBoxValue2, listBoxValue3, listBoxValue1];
+        directive.inputListBoxValues = [listBoxValue2, listBoxValue3, listBoxValue1];
 
-        component.ngOnChanges({
-            inputListBoxValues: new SimpleChange([], component.inputListBoxValues, false)
+        directive.ngOnChanges({
+            inputListBoxValues: new SimpleChange([], directive.inputListBoxValues, false)
         });
 
-        expect(spy).toHaveBeenCalledWith(component.inputListBoxValues, directive.sortDirection, 'caption');
-        expect(component.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2, listBoxValue3]);
+        expect(spy).toHaveBeenCalledWith(directive.inputListBoxValues, directive.sortDirection, 'caption');
+        expect(directive.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2, listBoxValue3]);
     });
 
     it(`should not sort #inputListBoxValues if #disableSorting is true`, () =>
     {
         const spy:Spy = spyOn(SortHelper, 'sortArray').and.callThrough();
-        component.inputListBoxValues = [listBoxValue1, listBoxValue2];
+        directive.inputListBoxValues = [listBoxValue1, listBoxValue2];
         directive.disableSorting = true;
 
-        component.ngOnChanges({
+        directive.ngOnChanges({
             disableSorting:     new SimpleChange(false, true, false),
-            inputListBoxValues: new SimpleChange([], component.inputListBoxValues, false)
+            inputListBoxValues: new SimpleChange([], directive.inputListBoxValues, false)
         });
 
         expect(spy).not.toHaveBeenCalled();
-        expect(component.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2]);
+        expect(directive.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2]);
     });
 
     it(`should sort #inputListBoxValues depending on the #sortDirection prop`, () =>
     {
         const spy:Spy = spyOn(SortHelper, 'sortArray').and.callThrough();
-        component.inputListBoxValues = [listBoxValue3, listBoxValue1, listBoxValue2];
+        directive.inputListBoxValues = [listBoxValue3, listBoxValue1, listBoxValue2];
 
-        component.ngOnChanges({
-            inputListBoxValues: new SimpleChange([], component.inputListBoxValues, false)
+        directive.ngOnChanges({
+            inputListBoxValues: new SimpleChange([], directive.inputListBoxValues, false)
         });
 
-        expect(component.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2, listBoxValue3]);
+        expect(directive.inputListBoxValues).toEqual([listBoxValue1, listBoxValue2, listBoxValue3]);
 
         directive.sortDirection = 'desc';
-        component.ngOnChanges({
+        directive.ngOnChanges({
             sortDirection: new SimpleChange('asc', 'desc', false)
         });
 
-        expect(spy).toHaveBeenCalledWith(component.inputListBoxValues, directive.sortDirection, 'caption');
-        expect(component.inputListBoxValues).toEqual([listBoxValue3, listBoxValue2, listBoxValue1]);
+        expect(spy).toHaveBeenCalledWith(directive.inputListBoxValues, directive.sortDirection, 'caption');
+        expect(directive.inputListBoxValues).toEqual([listBoxValue3, listBoxValue2, listBoxValue1]);
     });
 });
