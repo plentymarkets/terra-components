06c7cdbc135965293d02a15e714c6414c69ac691
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-06-24 14:15:31 +0200

    adding all peer dependencies of terra-components to the dependencies of the installing app

--- src/lib/schematics/ng-add/index.ts
+++ src/lib/schematics/ng-add/index.ts
@@ -1,17 +1,70 @@
 import {
+    chain,
     Rule,
     SchematicContext,
     Tree
 } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+    addPackageJsonDependency,
+    NodeDependency,
+    NodeDependencyType
+} from 'schematics-utilities';
+
+export function getPeerDependencies(tree:Tree):Array<NodeDependency>
+{
+    const pathToPackageJson:string = 'src/lib/package.json';
+    if(!tree.exists(pathToPackageJson))
+    {
+        return [];
+    }
+
+    const packageJson:any = JSON.parse(tree.read(pathToPackageJson)!.toString('utf8'));
+
+    if(packageJson.peerDependencies )
+    {
+        return Object.keys(packageJson.peerDependencies).map((dep:string) =>
+        {
+            return {
+                type: NodeDependencyType.Default,
+                name: dep,
+                version: packageJson.peerDependencies[dep]
+            };
+        });
+    }
+
+    return [];
+}
+
+function addPackageJsonDependencies():Rule
+{
+    return (host:Tree, context:SchematicContext):Tree =>
+    {
+        const dependencies:Array<NodeDependency> = getPeerDependencies(host);
+        dependencies.forEach((dependency:NodeDependency) =>
+        {
+            addPackageJsonDependency(host, dependency);
+            context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+        });
+
+        return host;
+    };
+}
+
+function installPackageJsonDependencies():Rule
+{
+    return (host:Tree, context:SchematicContext):Tree =>
+    {
+        context.addTask(new NodePackageInstallTask());
+        return host;
+    };
+}
 
 /** @description schematic for ng add support */
 export function ngAdd():Rule
 {
-    return (tree:Tree, context:SchematicContext):Tree =>
-    {
-        // For now, just return the tree and install the lib using any package manager
-        context.addTask(new NodePackageInstallTask());
-        return tree;
-    };
+    return chain([
+        addPackageJsonDependencies(),
+        installPackageJsonDependencies(),
+    ]);
 }
