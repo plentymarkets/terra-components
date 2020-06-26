6ec0886abc2d0b6f1950ef901e6679eb3a412381
Michael Frank <m_frank84@outlook.de>
2020-06-26 14:50:36 +0200

    migrate checkbox basic

--- src/lib/schematics/checkbox-migration/index.ts
+++ src/lib/schematics/checkbox-migration/index.ts
@@ -1,47 +1,90 @@
 import {
     Rule,
     SchematicContext,
+    SchematicsException,
     Tree,
     UpdateRecorder
 } from '@angular-devkit/schematics';
-import { from } from 'rxjs';
 import { LoggerApi } from '@angular-devkit/core/src/logger';
+import { getProjectTsConfigPaths } from '@angular/core/schematics/utils/project_tsconfig_paths';
+import { createMigrationProgram } from '../utils/compiler-hosts';
+import * as ts from 'typescript';
+import { relative } from 'path';
 
-const componentPath:string = './src/app/app.component.html';
-// const componentPathTest:string = 'test.component.ts';
+// const componentPath:string = './src/app/app.component.html';
+//
+// const queryString:string = 'Foobar!';
+// const replaceString:string = 'Foo Bar!!';
 
-const queryString:string = 'Foobar!';
-const replaceString:string = 'Foo Bar!!';
+let logger:LoggerApi;
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function checkboxMigration(_options:any):Rule
 {
-    return (tree:Tree, context:SchematicContext) =>
+    return (tree:Tree, context:SchematicContext):void =>
     {
-        return from(runMigration(tree, context).then(() => tree)) as any;
+        const { buildPaths, testPaths }:{[key:string]:Array<string>} = getProjectTsConfigPaths(tree);
+        const basePath:string = process.cwd();
+        const allPaths:Array<string> = [...buildPaths, ...testPaths];
+
+        logger = context.logger;
+
+
+        if(!allPaths.length)
+        {
+            throw new SchematicsException(
+                'Could not find any tsconfig file. Cannot migrate dynamic queries.');
+        }
+
+        for(const tsconfigPath of allPaths)
+        {
+            logger.info(tsconfigPath);
+            runCkeckboxMigration(tree, tsconfigPath, basePath);
+        }
     };
 }
 
-async function runMigration(tree:Tree, context:SchematicContext)
+function runCkeckboxMigration(tree:Tree, tsconfigPath:string, basePath:string):void
 {
-    const logger:LoggerApi = context.logger;
-    // tree.create(componentPathTest, 'console.log("Foobar!")');
+    const program:ts.Program = createMigrationProgram(tree, tsconfigPath, basePath).program;
+    // const typeChecker:ts.TypeChecker = program.getTypeChecker();
+    const sourceFiles:Array<ts.SourceFile> = program.getSourceFiles().filter(
+        (f:ts.SourceFile) => !f.isDeclarationFile && !program.isSourceFileFromExternalLibrary(f));
+    // const printer = ts.createPrinter();
 
-    logger.info(` Content before update: ${tree.read(componentPath)} `);
+    // program.getSourceFiles().forEach((sourceFile:ts.SourceFile) => {
+    //     logger.info(sourceFile.fileName);
+    // });
 
-    if(tree.exists(componentPath))
+    // logger.info(` Content before update: ${tree.read(relative(basePath, sourceFiles[4].referencedFiles.toString()))} `);
+
+    sourceFiles.forEach((sourceFile:ts.SourceFile) =>
     {
-        let update:UpdateRecorder = tree.beginUpdate(componentPath!);
-        let buffer:Buffer | number = tree.read(componentPath) || 0;
+        const fileName:string = relative(basePath, sourceFile.fileName);
+        if(isComponent(fileName))
+        {
+            const templateFileName:string = fileName.replace('component.ts', 'component.html');
+            if(tree.exists(templateFileName))
+            {
+                let buffer:Buffer | number = tree.read(templateFileName) || 0;
+                let index:number = buffer.toString().indexOf('terra-checkbox');
+                let update:UpdateRecorder = tree.beginUpdate(templateFileName!);
 
-        const startIndex:number = buffer.toString().indexOf(queryString);
+                while(index)
+                {
+                    update.remove(index, 'terra-checkbox'.length);
+                    update.insertRight(index, 'mat-checkbox');
+                    index = buffer.toString().indexOf('terra-checkbox');
+                }
 
-        update.remove(startIndex, queryString.length);
-        update.insertRight(startIndex, replaceString);
+                tree.commitUpdate(update);
+            }
+        }
+    });
+}
 
-        tree.commitUpdate(update);
-    }
-
-    logger.info(` Content after update: ${tree.read('hello.component.ts')} `);
+function isComponent(fileName:string):boolean
+{
+    return fileName.endsWith('component.ts');
 }
