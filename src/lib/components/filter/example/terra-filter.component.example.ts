8af7e9a5ab94ba8386641eb6221e6fb2dde49dc1
Michael Frank <m_frank84@outlook.de>
2020-06-29 09:42:41 +0200

    feature(prettier): apply prettier to src/lib/components/filter

--- src/lib/components/filter/example/terra-filter.component.example.ts
+++ src/lib/components/filter/example/terra-filter.component.example.ts
@@ -1,46 +1,35 @@
-import {
-    Component,
-    OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TerraSelectBoxValueInterface } from '../../forms/select-box/data/terra-select-box.interface';
 
 @Component({
-    selector:    'terra-filter-example',
-    styleUrls:   ['./terra-filter.component.example.scss'],
-    templateUrl: './terra-filter.component.example.html',
+    selector: 'terra-filter-example',
+    styleUrls: ['./terra-filter.component.example.scss'],
+    templateUrl: './terra-filter.component.example.html'
 })
-export class TerraFilterComponentExample implements OnInit
-{
-    public _name:string = '';
+export class TerraFilterComponentExample implements OnInit {
+    public _name: string = '';
 
-    public _listBoxValues:Array<TerraSelectBoxValueInterface> = [];
-    public _selectedListBoxValue:number = 1;
+    public _listBoxValues: Array<TerraSelectBoxValueInterface> = [];
+    public _selectedListBoxValue: number = 1;
 
-    public ngOnInit():void
-    {
-        for(let i:number = 1; i < 4; i++)
-        {
-            this._listBoxValues.push(
-                {
-                    value:   'test' + i,
-                    caption: 'Test' + i
-                }
-            );
+    public ngOnInit(): void {
+        for (let i: number = 1; i < 4; i++) {
+            this._listBoxValues.push({
+                value: 'test' + i,
+                caption: 'Test' + i
+            });
         }
     }
 
-    public _onResetBtnClicked():void
-    {
+    public _onResetBtnClicked(): void {
         return;
     }
 
-    public _onSubmit():void
-    {
+    public _onSubmit(): void {
         this._onSearchBtnClicked();
     }
 
-    public _onSearchBtnClicked():void
-    {
+    public _onSearchBtnClicked(): void {
         alert('filtered for ' + this._selectedListBoxValue);
     }
 }
