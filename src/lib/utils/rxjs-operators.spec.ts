4e641a512ff372d035f1ff710a99d928ba0191f4
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-07-03 15:02:24 +0200

    WIP

---
+++ src/lib/utils/rxjs-operators.spec.ts
@@ -0,0 +1,36 @@
+import {
+    Component,
+    OnDestroy
+} from '@angular/core';
+import {
+    interval,
+    Subscription
+} from 'rxjs';
+import { takeUntilDestroyed } from './rxjs-operators';
+
+@Component({
+    template: ''
+})
+class TestComponent implements OnDestroy
+{
+    public subscription:Subscription = interval(100).pipe(
+        takeUntilDestroyed(this)
+    ).subscribe();
+
+    public ngOnDestroy():void
+    {
+        console.log('destroyed');
+    }
+}
+
+xdescribe('', () =>
+{
+    const component:TestComponent = new TestComponent();
+    it('', () =>
+    {
+        spyOn(console, 'log');
+        component.ngOnDestroy();
+        expect(component.subscription.closed).toBe(true);
+        expect(console.log).toHaveBeenCalledWith('destroyed');
+    });
+});
