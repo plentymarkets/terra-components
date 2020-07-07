55e30e14a466d2e37a3bbc2c21292a7d2114813a
ChristianMoll <christian.moll@plentymarkets.com>
2020-07-07 10:06:42 +0200

    remove remnants of terra overlay

--- src/lib/components/table/table-column-settings/table-column-settings.component.ts
+++ src/lib/components/table/table-column-settings/table-column-settings.component.ts
@@ -3,7 +3,6 @@ import {
     OnInit
 } from '@angular/core';
 import { ColumnInterface } from './column.interface';
-import { TerraOverlayButtonInterface } from '../../..';
 import { Language } from 'angular-l10n';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -16,8 +15,6 @@ export class TableColumnSettingsComponent implements OnInit
 {
     public _columns:Array<ColumnInterface> = [];
     public _selectedColumns:Array<ColumnInterface> = [];
-    public _primaryButtonInterface:TerraOverlayButtonInterface;
-    public _secondaryButtonInterface:TerraOverlayButtonInterface;
 
     @Language()
     public _lang:string;
