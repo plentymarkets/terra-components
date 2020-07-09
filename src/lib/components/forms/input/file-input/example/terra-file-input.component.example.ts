3d9adf2c9826c98d00139df9d37e9e51d315b1cc
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-07-09 13:30:27 +0200

    provide frontendStorageServiceStub instead of the real instance, to prevent fetching data from the server

--- src/lib/components/forms/input/file-input/example/terra-file-input.component.example.ts
+++ src/lib/components/forms/input/file-input/example/terra-file-input.component.example.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { terraFrontendStorageServiceStub } from '../../../../../testing/file-input/terra-frontend-storage-service-stub';
+import { TerraFrontendStorageService } from '../../../../../components/file-browser/terra-frontend-storage.service';
 
 @Component({
     selector: 'terra-file-input-example',
     styleUrls: [ './terra-file-input.component.example.scss'],
     templateUrl: './terra-file-input.component.example.html',
+    providers: [{
+        provide: TerraFrontendStorageService,
+        useValue: terraFrontendStorageServiceStub
+    }]
 })
 export class TerraFileInputComponentExample
 {
