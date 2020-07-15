1293985c06fe2d2d243d698386b09c20d09d1bf8
Michael Kunze <michael.kunze@plentymarkets.com>
2020-07-15 12:06:19 +0200

    change sorting

--- src/lib/components/table/table-column-settings/dialog/table-settings-dialog.component.ts
+++ src/lib/components/table/table-column-settings/dialog/table-settings-dialog.component.ts
@@ -15,7 +15,7 @@ import {
 } from '@angular/cdk/drag-drop';
 
 @Component({
-    selector:    'tc-table-settings-dialog',
+    selector: 'tc-table-settings-dialog',
     templateUrl: './table-settings-dialog.component.html'
 })
 export class TableSettingsDialogComponent implements OnInit
@@ -35,24 +35,18 @@ export class TableSettingsDialogComponent implements OnInit
     public ngOnInit():void
     {
         this._selectedColumns = this.data.selectedColumns;
-        this._columns = this._sort();
+        this._sort(this.data.columns);
     }
 
 
     public _onDrop(event:CdkDragDrop<Array<MatColumnDef>>):void
     {
         moveItemInArray(this._columns, event.previousIndex, event.currentIndex);
-        // let index:number = this._selectedColumns.indexOf(event.item.data);
-        //
-        // if(index > -1)
-        // {
-        //     // moveItemInArray(this._selectedColumns, )
-        // }
 
-        console.log(event.item.data);
+        this._sort(this._columns);
     }
 
-    public _updateSelectedList():{sortedList:Array<string>, columns:Array<MatColumnDef>}
+    public _updateSelectedList():{ sortedList:Array<string>, columns:Array<MatColumnDef> }
     {
         let sortedList:Array<string> = [];
 
@@ -66,15 +60,18 @@ export class TableSettingsDialogComponent implements OnInit
             }
         });
 
-        return {sortedList: sortedList, columns: this._columns};
+        return {
+            sortedList: sortedList,
+            columns:    this._columns
+        };
     }
 
-    public _sort():Array<MatColumnDef>
+    public _sort(cols:Array<MatColumnDef>):void
     {
         let selectedList:Array<MatColumnDef> = [];
         let unselectedList:Array<MatColumnDef> = [];
 
-        this.data.columns.forEach((col:MatColumnDef) =>
+        cols.forEach((col:MatColumnDef) =>
         {
             let index:number = this._selectedColumns.indexOf(col.name);
 
@@ -88,6 +85,6 @@ export class TableSettingsDialogComponent implements OnInit
             }
         });
 
-        return selectedList.concat(unselectedList);
+        this._columns = selectedList.concat(unselectedList);
     }
 }
