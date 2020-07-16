fe32e394100b7504de76c5c9adf442faa4eb65e3
Michael Frank <m_frank84@outlook.de>
2020-07-16 13:42:21 +0200

    Update index.ts

--- src/lib/components/table/index.ts
+++ src/lib/components/table/index.ts
@@ -5,7 +5,4 @@ export { TableSortingDataSource } from './table-sorting-data-source';
 export { TablePagingSortingDataSource } from './table-paging-sorting-data-source';
 export { RequestParameterInterface } from './request-parameter.interface';
 export { TerraMatPaginatorIntl } from './terra-mat-paginator-intl';
-
 export { TableSettingsComponent } from './table-settings/table-settings.component';
-export { TableSettingsDialogComponent } from './table-settings/dialog/table-settings-dialog.component';
-export { TableSettingsDialogData } from './table-settings/interface/table-settings-dialog-data.interface';
