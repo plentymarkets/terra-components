d790c29d5ba84af5de23397812f3c46ba309e4d9
Daniel Trauf <daniel.trauf@plentymarkets.com>
2020-07-22 15:55:23 +0200

    add specs for ngOnInit (not working) and sort method

--- src/lib/components/table/table-settings/dialog/table-settings-dialog.component.spec.ts
+++ src/lib/components/table/table-settings/dialog/table-settings-dialog.component.spec.ts
@@ -16,13 +16,18 @@ import { By } from '@angular/platform-browser';
 import { MatColumnDef } from '@angular/material/table';
 import { MockButtonComponent } from '../../../../testing/mock-button';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import Spy = jasmine.Spy;
 
 
-const column:MatColumnDef = new MatColumnDef();
-column.name = 'TestName';
+const column1:MatColumnDef = new MatColumnDef();
+column1.name = 'TestName1';
+const column2:MatColumnDef = new MatColumnDef();
+column2.name = 'TestName2';
+const column3:MatColumnDef = new MatColumnDef();
+column3.name = 'TestName3';
 let mockDialogData:TableSettingsDialogData = {
-    columns:         [column],
-    selectedColumns: []
+    columns:         [column1, column2, column3],
+    selectedColumns: [column2.name, column3.name]
 };
 
 describe('TableSettingsDialogComponent', () =>
@@ -60,15 +65,33 @@ describe('TableSettingsDialogComponent', () =>
         expect(component).toBeTruthy();
     });
 
+    xit('should assign the array of selected columns and columns by `OnInit` life cycle hook', () =>
+    {
+        spyOn(component, 'ngOnInit').and.callThrough();
+        let spy:Spy = spyOn(component, '_sort');
+        expect(component._selectedColumns).toEqual(component.data.selectedColumns);
+        expect(spy).toHaveBeenCalledWith(component.data.columns);
+    });
+
     it('should render list options', () =>
     {
         const options:Array<DebugElement> = fixture.debugElement.queryAll(By.css('mat-list-option'));
-        expect(options.length).toBe(1);
+        expect(options.length).toBe(3);
     });
 
-    it('should render column name in option', () =>
+    it('should render column names in options', () =>
     {
-        const option:any = fixture.debugElement.query(By.css('mat-list-option')).nativeElement;
-        expect(option.textContent).toBe(' TestName ');
+        const option:Array<any> = fixture.debugElement.queryAll(By.css('mat-list-option'));
+        expect(option[0].nativeElement.textContent).toContain('TestName');
+    });
+
+    it('should sort the list of column names by selection and append unselected after selected', () =>
+    {
+        expect(component._columns[2]).toEqual(column1);
+    });
+
+    it('should call `moveItemInArray` to change position of column and call `_sort` with changed column list', () =>
+    {
+        pending();
     });
 });
