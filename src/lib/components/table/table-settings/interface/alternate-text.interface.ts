2144d80ddb4f5e4a5b3b8bed89b8d0b1022f9fb0
Tim Wieder <tim.wieder@plentymarkets.com>
2020-07-28 14:43:32 +0200

    - some changes to the alternative text interface

--- src/lib/components/table/table-settings/interface/alternate-text.interface.ts
+++ src/lib/components/table/table-settings/interface/alternate-text.interface.ts
@@ -1,9 +1,6 @@
 export interface AlternateTextInterface
 {
     key:string; // This should be the ID of the ColumnDef for example
-    value:
-        {
-            altText:string; // The text that can be displayed alternatively to the ID
-            selected:boolean;
-        };
+    altText:string; // The text that can be displayed alternatively to the ID
+    selected:boolean;
 }
