73058cbbc9668cccdde9dd453eb3ee2639dfe91c
Michael Frank <m_frank84@outlook.de>
2020-08-10 08:25:19 +0200

    feature(prettier): apply prettier to src/lib/component/no-result

--- src/lib/components/no-result/example/terra-no-result-notice.component.example.ts
+++ src/lib/components/no-result/example/terra-no-result-notice.component.example.ts
@@ -1,34 +1,26 @@
-import {
-    Component,
-    OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TerraButtonInterface } from '../../buttons/button/data/terra-button.interface';
 
 @Component({
     selector: 'terra-no-result-notice-example',
-    styleUrls: [ './terra-no-result-notice.component.example.scss'],
+    styleUrls: ['./terra-no-result-notice.component.example.scss'],
     templateUrl: './terra-no-result-notice.component.example.html'
 })
-export class TerraNoResultNoticeComponentExample implements OnInit
-{
-    public _buttons:Array<TerraButtonInterface> = [];
-    public _result:boolean;
+export class TerraNoResultNoticeComponentExample implements OnInit {
+    public _buttons: Array<TerraButtonInterface> = [];
+    public _result: boolean;
 
-    public checkResult(value:boolean):void
-    {
+    public checkResult(value: boolean): void {
         this._result = value;
     }
 
-    public ngOnInit():void
-    {
+    public ngOnInit(): void {
         this._buttons.push({
-            caption:       'Search',
-            icon:          'icon-add',
-            clickFunction: ():void =>
-                           {
-                               this._result = true;
-                           }
+            caption: 'Search',
+            icon: 'icon-add',
+            clickFunction: (): void => {
+                this._result = true;
+            }
         });
     }
-
 }
