5da41bb46f75d7abdc145f77473c771681287eb4
Daniel Trauf <daniel.trauf@plentymarkets.com>
2020-10-02 14:59:58 +0200

    fix grammar

--- src/lib/validators/terra-decimal-validator.ts
+++ src/lib/validators/terra-decimal-validator.ts
@@ -5,7 +5,7 @@ import { isNullOrUndefined } from 'util';
  * @author mfrank
  */
 const separator: string = '.';
-const maxSplittedParts: number = 2;
+const maxSplitParts: number = 2;
 const beforeSeparator: number = 0;
 const afterSeparator: number = 1;
 
@@ -13,13 +13,12 @@ export function terraDecimalValidator(maxLength: number, decimals: number): Vali
     return (control: AbstractControl): ValidationErrors | null => {
         const value: number = control.value as number;
         if (!isNullOrUndefined(value)) {
-            const splittedValue: Array<string> = value.toString().split(separator);
-            const invalidSeparatorCount: boolean = splittedValue.length > maxSplittedParts;
-            const invalidMaxLengthBeforeSeparator: boolean =
-                splittedValue[beforeSeparator].length > maxLength - decimals;
-            const invalidMaxLengthAfterSeparator: boolean = isNullOrUndefined(splittedValue[afterSeparator])
+            const splitValue: Array<string> = value.toString().split(separator);
+            const invalidSeparatorCount: boolean = splitValue.length > maxSplitParts;
+            const invalidMaxLengthBeforeSeparator: boolean = splitValue[beforeSeparator].length > maxLength - decimals;
+            const invalidMaxLengthAfterSeparator: boolean = isNullOrUndefined(splitValue[afterSeparator])
                 ? false
-                : splittedValue[afterSeparator].length > decimals;
+                : splitValue[afterSeparator].length > decimals;
 
             if (invalidSeparatorCount) {
                 return { terraDecimal: 'Invalid amount of separators' };
