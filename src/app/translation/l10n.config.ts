5fd3c97550a1e040a03409893de751bdb9ed7610
Michael Frank <m_frank84@outlook.de>
2020-10-27 11:33:20 +0100

    create new config

--- src/app/translation/l10n.config.ts
+++ src/app/translation/l10n.config.ts
@@ -1,40 +1,54 @@
-import { L10nConfig, LogLevel, ProviderType, StorageStrategy } from 'angular-l10n';
+import { L10nConfig } from 'angular-l10n';
 import { environment } from 'src/lib/environments/environment';
+import { i18nTerraComponents } from 'src/lib/translations/components/i18n-terra-components';
 
-const logLevel: LogLevel = environment.test ? LogLevel.Off : LogLevel.Warn;
 export const l10nConfig: L10nConfig = {
-    logger: {
-        level: logLevel
+    format: 'language-region',
+    providers: [{ name: 'terra-components', asset: i18nTerraComponents }],
+    cache: true,
+    keySeparator: '.',
+    defaultLocale: {
+        language: 'de-DE',
+        currency: 'EUR'
     },
-    locale: {
-        languages: [
-            {
-                code: 'en',
-                dir: 'ltr'
-            },
-            {
-                code: 'de',
-                dir: 'ltr'
-            }
-        ],
-        language: 'en',
-        defaultLocale: {
-            languageCode: 'de',
-            countryCode: 'DE'
-        },
-        currency: 'EUR',
-        storage: StorageStrategy.Local,
-        storageNames: { defaultLocale: 'plentymarkets_lang_' }
-    },
-    translation: {
-        providers: [
-            {
-                type: ProviderType.Static,
-                prefix: 'assets/lang/locale-'
-            }
-        ],
-        caching: true,
-        composedKeySeparator: '.',
-        i18nPlural: false
-    }
+    schema: [
+        { locale: { language: 'de-DE', currency: 'EUR' }, dir: 'ltr', text: 'Deutsch' },
+        { locale: { language: 'en-EN', currency: 'GBP' }, dir: 'ltr', text: 'English' }
+    ]
 };
+
+// const logLevel: LogLevel = environment.test ? LogLevel.Off : LogLevel.Warn;
+
+// export const l10nConfig: L10nConfig = {
+//     locale: {
+//         languages: [
+//             {
+//                 code: 'en',
+//                 dir: 'ltr'
+//             },
+//             {
+//                 code: 'de',
+//                 dir: 'ltr'
+//             }
+//         ],
+//         language: 'en',
+//         defaultLocale: {
+//             languageCode: 'de',
+//             countryCode: 'DE'
+//         },
+//         currency: 'EUR',
+//         storage: StorageStrategy.Local,
+//         storageNames: { defaultLocale: 'plentymarkets_lang_' }
+//     },
+//     translation: {
+//         providers: [
+//             {
+//                 type: ProviderType.Static,
+//                 prefix: 'assets/lang/locale-'
+//             }
+//         ],
+//         caching: true,
+//         composedKeySeparator: '.',
+//         i18nPlural: false
+//     }
+// };
