3417d883116cf9eb453132a15d9bc9bf07855005
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-10-27 14:38:57 +0100

    remove unused interact directives and interfaces

--- src/lib/components/interactables/interact.module.ts
+++ src/lib/components/interactables/interact.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { TerraDraggableDirective } from './draggable.directive';
-import { TerraDropzoneDirective } from './dropzone.directive';
-import { TerraResizableDirective } from './resizable.directive';
 
 @NgModule({
     imports: [],
-    declarations: [TerraDraggableDirective, TerraDropzoneDirective, TerraResizableDirective],
-    exports: [TerraDraggableDirective, TerraDropzoneDirective, TerraResizableDirective]
+    declarations: [TerraDraggableDirective],
+    exports: [TerraDraggableDirective]
 })
 export class TerraInteractModule {}
