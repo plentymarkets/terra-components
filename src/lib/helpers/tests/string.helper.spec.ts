3e07738b7e60f4409de92ff697c0d61f6d875b32
Daniel Trauf <daniel.trauf@plentymarkets.com>
2020-11-09 15:21:30 +0100

    remove deprecated util function

--- src/lib/helpers/tests/string.helper.spec.ts
+++ src/lib/helpers/tests/string.helper.spec.ts
@@ -2,20 +2,16 @@ import { StringHelper } from '../string.helper';
 
 describe('StringHelper:', () => {
     it('Method: isNullUndefinedOrEmpty() should validate the variable', () => {
-        // undefined
         let testVar: string = undefined;
         expect(StringHelper.isNullUndefinedOrEmpty(testVar)).toBe(true);
 
-        // null
         testVar = null;
         expect(StringHelper.isNullUndefinedOrEmpty(testVar)).toBe(true);
 
-        // empty
         testVar = '';
         expect(StringHelper.isNullUndefinedOrEmpty(testVar)).toBe(true);
 
         testVar = 'Test';
-
         expect(StringHelper.isNullUndefinedOrEmpty(testVar)).toBe(false);
     });
 });
