a7e06db899fa787f03747ab0af67b95a7cc2c379
Tim Wieder <tim.wieder@plentymarkets.com>
2020-11-10 12:14:21 +0100

    Update user-language.ts

--- src/app/translation/user-language.ts
+++ src/app/translation/user-language.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { L10nConfig, L10nUserLanguage, L10N_CONFIG } from 'angular-l10n';
-import { StringHelper } from '../../lib/helpers';
+import { L10N_CONFIG, L10nConfig, L10nUserLanguage } from 'angular-l10n';
 
 @Injectable()
 export class DefaultUserLanguage implements L10nUserLanguage {
@@ -8,6 +7,6 @@ export class DefaultUserLanguage implements L10nUserLanguage {
 
     public async get(): Promise<string | null> {
         const lang: string = localStorage.getItem('plentymarkets_lang_') || this.config.defaultLocale.language;
-        return Promise.resolve(langInLocalStorage);
+        return Promise.resolve(lang);
     }
 }
