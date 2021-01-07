bdbebec4ff1c584ba5887e493613b71bec4cce42
Felix Dausch <felix.dausch@plentymarkets.com>
2021-01-07 17:58:19 +0100

    Apply prettier

--- src/lib/components/forms/form/helper/terra-form-field.helper.spec.ts
+++ src/lib/components/forms/form/helper/terra-form-field.helper.spec.ts
@@ -4,48 +4,17 @@ import { listWithChildren } from '../example/form-fields';
 import { TerraKeyValueInterface } from '../../../../models';
 
 describe(`TerraFormFieldHelper:`, () => {
-    const child1: TerraFormFieldInterface = {
-        type: 'text',
-        defaultValue: 'one'
-    };
-    const child2: TerraFormFieldInterface = {
-        type: 'text',
-        defaultValue: [1, 2]
-    };
+    const child1: TerraFormFieldInterface = { type: 'text', defaultValue: 'one' };
+    const child2: TerraFormFieldInterface = { type: 'text', defaultValue: [1, 2] };
 
     const formFields: { [key: string]: TerraFormFieldInterface } = {
-        control1: {
-            type: 'text',
-            defaultValue: 'one'
-        },
-        control2: {
-            type: 'text',
-            defaultValue: 'two'
-        },
-        controlWithArray: {
-            type: 'text',
-            defaultValue: [1, 2]
-        },
-        controlWithObject: {
-            type: 'text',
-            defaultValue: { foo: 'bar' }
-        },
-        controlWithChildren: {
-            type: 'horizontal',
-            children: {
-                child1: child1,
-                child2: child2
-            }
-        },
-        controlList: {
-            type: 'number',
-            isList: true,
-            defaultValue: 5
-        },
-        controlListWithoutDefaultValue: {
-            type: 'number',
-            isList: true
-        }
+        control1: { type: 'text', defaultValue: 'one' },
+        control2: { type: 'text', defaultValue: 'two' },
+        controlWithArray: { type: 'text', defaultValue: [1, 2] },
+        controlWithObject: { type: 'text', defaultValue: { foo: 'bar' } },
+        controlWithChildren: { type: 'horizontal', children: { child1: child1, child2: child2 } },
+        controlList: { type: 'number', isList: true, defaultValue: 5 },
+        controlListWithoutDefaultValue: { type: 'number', isList: true }
     };
 
     describe(`parseDefaultValue() `, () => {
