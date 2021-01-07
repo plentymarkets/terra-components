86b34b76dec757ded1e05d4edc15d1c270d3a004
Felix Dausch <felix.dausch@plentymarkets.com>
2021-01-07 14:36:14 +0100

    reformat code with prettier

--- src/lib/components/forms/form/helper/terra-form-field.helper.spec.ts
+++ src/lib/components/forms/form/helper/terra-form-field.helper.spec.ts
@@ -4,17 +4,48 @@ import { listWithChildren } from '../example/form-fields';
 import { TerraKeyValueInterface } from '../../../../models';
 
 describe(`TerraFormFieldHelper:`, () => {
-    const child1: TerraFormFieldInterface = { type: 'text', defaultValue: 'one' };
-    const child2: TerraFormFieldInterface = { type: 'text', defaultValue: [1, 2] };
+    const child1: TerraFormFieldInterface = {
+        type: 'text',
+        defaultValue: 'one'
+    };
+    const child2: TerraFormFieldInterface = {
+        type: 'text',
+        defaultValue: [1, 2]
+    };
 
     const formFields: { [key: string]: TerraFormFieldInterface } = {
-        control1: { type: 'text', defaultValue: 'one' },
-        control2: { type: 'text', defaultValue: 'two' },
-        controlWithArray: { type: 'text', defaultValue: [1, 2] },
-        controlWithObject: { type: 'text', defaultValue: { foo: 'bar' } },
-        controlWithChildren: { type: 'horizontal', children: { child1: child1, child2: child2 } },
-        controlList: { type: 'number', isList: true, defaultValue: 5 },
-        controlListWithoutDefaultValue: { type: 'number', isList: true }
+        control1: {
+            type: 'text',
+            defaultValue: 'one'
+        },
+        control2: {
+            type: 'text',
+            defaultValue: 'two'
+        },
+        controlWithArray: {
+            type: 'text',
+            defaultValue: [1, 2]
+        },
+        controlWithObject: {
+            type: 'text',
+            defaultValue: { foo: 'bar' }
+        },
+        controlWithChildren: {
+            type: 'horizontal',
+            children: {
+                child1: child1,
+                child2: child2
+            }
+        },
+        controlList: {
+            type: 'number',
+            isList: true,
+            defaultValue: 5
+        },
+        controlListWithoutDefaultValue: {
+            type: 'number',
+            isList: true
+        }
     };
 
     describe(`parseDefaultValue() `, () => {
@@ -85,8 +116,7 @@ describe(`TerraFormFieldHelper:`, () => {
             expect(composedArrayDefault).toEqual(Array(min).fill(groupDefaultValue));
         });
 
-        it(`should parse #isList correctly`, () =>
-        {
+        it(`should parse #isList correctly`, () => {
             expect(TerraFormFieldHelper.getListRange(false)).toBe([0, Infinity]);
             expect(TerraFormFieldHelper.getListRange(true)).toBe([0, Infinity]);
             expect(TerraFormFieldHelper.getListRange('')).toBe([0, Infinity]);
