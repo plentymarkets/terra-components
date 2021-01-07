37ed3ae954dc25c6050781592d1a403a3eb11795
Felix Dausch <felix.dausch@plentymarkets.com>
2021-01-07 14:14:33 +0100

    FIX remove whitespaces before parsing range for list entries

--- src/lib/components/forms/form/helper/terra-form-field.helper.ts
+++ src/lib/components/forms/form/helper/terra-form-field.helper.ts
@@ -221,7 +221,8 @@ export class TerraFormFieldHelper {
         let max: number;
 
         if (isString(range)) {
-            let match: RegExpExecArray = /^\[(\d*),(\d*)]$/.exec(range as string);
+            range = range.replace(/\s/g, '');
+            let match: RegExpExecArray = /^\[(\d*)\s*,\s*(\d*)]$/.exec(range as string);
             if (match !== null) {
                 min = parseInt(match[1], 10);
                 max = parseInt(match[2], 10);
