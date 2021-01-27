9479e9e98e8a8ec3fa186cbe00a9993d1a271772
Michael Kunze <michael.kunze@plentymarkets.com>
2021-01-27 14:08:16 +0100

    Update route-data-registry.ts

--- src/lib/route-data/route-data-registry.ts
+++ src/lib/route-data/route-data-registry.ts
@@ -1,7 +1,8 @@
 import { RouteDataInterface } from './route-data.interface';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { extractRouteDataFromRouterConfig, RouteData } from '../utils/route-data';
+import { extractRouteDataFromRouterConfig } from '../utils/route-data';
+import { RouteData } from './route-data-function-types';
 
 @Injectable({
     providedIn: 'root'
