d35606453e398807631db9fed32ce2eb7c1c535b
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-01-29 14:43:31 +0100

    add export of RouteDataRegistryInitializer

--- src/lib/route-data/index.ts
+++ src/lib/route-data/index.ts
@@ -1,4 +1,5 @@
 export { LabelFunction } from './route-data-types';
 export { RouteDataInterface } from './route-data.interface';
 export { RouteDataRegistry } from './route-data-registry';
+export { RouteDataRegistryInitializer } from './route-data-registry-initializer';
 export { extractRouteDataFromRouterConfig, createRouteDataRegistryProviders } from './utils';
