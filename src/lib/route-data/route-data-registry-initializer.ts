eb5a58d3fc6a41da004ab6ef2ddb58af1c336dd4
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-01-29 15:15:51 +0100

    add JSDoc

--- src/lib/route-data/route-data-registry-initializer.ts
+++ src/lib/route-data/route-data-registry-initializer.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { extractRouteDataFromRouterConfig } from './utils';
 import { RouteDataRegistry } from './route-data-registry';
 
+/**
+ * A class that initializes the RouteDataRegistry with the data attached to the configured routes of the router.
+ */
 @Injectable()
 export class RouteDataRegistryInitializer<T extends RouteDataInterface> {
     constructor(router: Router, routeDataRegistry: RouteDataRegistry<T>) {
