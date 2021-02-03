95aaf5071e2fc5b598f9de270b3cd64259d00a26
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-02-03 15:42:00 +0100

    wip

--- src/lib/route-data/utils/routes.ts
+++ src/lib/route-data/utils/routes.ts
@@ -3,6 +3,7 @@ import { RouteData } from '../route-data-types';
 import { RouteDataInterface } from '../route-data.interface';
 import { normalizeRoutePath } from './route-path';
 
+export const redirectedRoutePrefix: string = 'redirect:';
 /**
  * Extracts the data of all given #routes (including children) into a flat key-value object.
  * @param routes
@@ -17,7 +18,9 @@ export function extractRouteDataFromRouterConfig<T extends RouteDataInterface>(r
         routeData[normalizedRoutePath] = route.data as T;
         const nestedRouteData: RouteData<T> = route.children ? extractRouteDataFromRouterConfig(route.children) : {};
         Object.entries(nestedRouteData).forEach(([key, value]: [string, T]) => {
-            routeData[normalizedRoutePath + '/' + key] = value;
+            // TODO(pweyrich): this may have to be adjusted.. not sure if it works like that..
+            const routePath: string = (key === '' ? redirectedRoutePrefix : '') + normalizedRoutePath + '/' + key;
+            routeData[routePath] = value;
         });
     });
 
