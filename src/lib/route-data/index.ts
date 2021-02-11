37fc5a13f1f82e611e3793d218453fdf55c005fb
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-02-11 15:05:23 +0100

    remove RouteDataRegistryInitializer

--- src/lib/route-data/index.ts
+++ src/lib/route-data/index.ts
@@ -1,6 +1,5 @@
 export { LabelFunction, RouteData, ReadonlyRouteData } from './route-data-types';
 export { RouteDataInterface } from './route-data.interface';
 export { RouteDataRegistry } from './route-data-registry';
-export { RouteDataRegistryInitializer } from './route-data-registry-initializer';
 export { extractRouteDataFromRouterConfig, findMatchingRoutePath } from './utils'; // TODO: should not be exported if it is not needed!!
 export * from './route-data.module';
