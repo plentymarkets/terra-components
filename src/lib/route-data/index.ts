a2724a4872d7475ae7b4575911f581086c022fad
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-02-11 16:38:04 +0100

    fix export of RouteDataModule

--- src/lib/route-data/index.ts
+++ src/lib/route-data/index.ts
@@ -1,5 +1,5 @@
 export { LabelFunction, RouteData, ReadonlyRouteData } from './route-data-types';
 export { RouteDataInterface } from './route-data.interface';
 export { RouteDataRegistry } from './route-data-registry';
+export { RouteDataModule } from './route-data.module';
 export { extractRouteDataFromRouterConfig, findMatchingRoutePath } from './utils'; // TODO: should not be exported if it is not needed!!
-export * from './route-data.module';
