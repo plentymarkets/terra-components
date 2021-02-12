a35e353a1c6c88b5d3198127174bf2759298a105
Michael Kunze <michael.kunze@plentymarkets.com>
2021-02-12 08:22:12 +0100

    Update index.ts

--- src/lib/route-data/index.ts
+++ src/lib/route-data/index.ts
@@ -2,4 +2,4 @@ export { LabelFunction, RouteData, ReadonlyRouteData } from './route-data-types'
 export { RouteDataInterface } from './route-data.interface';
 export { RouteDataRegistry } from './route-data-registry';
 export { RouteDataRegistryInitializer } from './route-data-registry-initializer';
-export { extractRouteDataFromRouterConfig, findMatchingRoutePath } from './utils'; // TODO: should not be exported if it is not needed!!
+export { extractRouteDataFromRouterConfig, findMatchingRoutePath, getChildren } from './utils';
