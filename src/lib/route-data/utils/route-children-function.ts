24d5bb34ea716c54580e0d3e139d4819acc0afa8
Tim Wieder <tim.wieder@plentymarkets.com>
2021-02-12 07:14:49 +0100

    Update route-children-function.ts

--- src/lib/route-data/utils/route-children-function.ts
+++ src/lib/route-data/utils/route-children-function.ts
@@ -4,6 +4,6 @@ import { Route, Routes } from '@angular/router';
  * This function returns the children of a given route wether they are lazy loaded or not
  * @param route The route to which the children or the lazy loaded children should be returned
  */
-export function getChildren(route: Route): Routes {
+export function getChildren(route: Route): Routes | undefined {
     return route.children || route['_loadedConfig']?.routes;
 }
