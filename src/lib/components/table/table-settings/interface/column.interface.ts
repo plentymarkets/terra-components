b619347c01d0998d119664ee81348912cdfab20e
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-03-11 14:37:05 +0100

    create property sticky to hide drag handle

--- src/lib/components/table/table-settings/interface/column.interface.ts
+++ src/lib/components/table/table-settings/interface/column.interface.ts
@@ -7,4 +7,12 @@ export interface ColumnInterface {
      * The text that can be displayed alternatively to the ID.
      */
     label: string;
+    /**
+     * Indication of whether a column is sticky.
+     */
+    sticky?: boolean;
+    /**
+     * Indication of whether an end column is sticky.
+     */
+    stickyEnd?: boolean;
 }
