29c9f459509ac1802bce9718882a945dfac6c711
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2021-04-09 14:24:46 +0200

    prettier

--- src/lib/components/forms/form/form-components/number-input/number-input.component.ts
+++ src/lib/components/forms/form/form-components/number-input/number-input.component.ts
@@ -1,64 +1,57 @@
-import {
-    Component,
-    Input
-} from '@angular/core';
-import {
-    ControlValueAccessor,
-    NG_VALUE_ACCESSOR
-} from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NumberInputInterface } from './number-input.interface';
-import { noop } from "rxjs";
+import { noop } from 'rxjs';
 
 let nextId: number = 0;
 
 @Component({
-    selector:    'number-input',
+    selector: 'number-input',
     templateUrl: './number-input.component.html',
-    providers:   [
+    providers: [
         {
-            provide:     NG_VALUE_ACCESSOR,
+            provide: NG_VALUE_ACCESSOR,
             useExisting: NumberInputComponent,
-            multi:       true
+            multi: true
         }
     ]
 })
-export class NumberInputComponent implements ControlValueAccessor, NumberInputInterface
-{
+export class NumberInputComponent implements ControlValueAccessor, NumberInputInterface {
     @Input()
-    public emptyMessage:string;
+    public emptyMessage: string;
 
     @Input()
-    public invalidMessage:string;
+    public invalidMessage: string;
 
     @Input()
-    public isDisabled:boolean;
+    public isDisabled: boolean;
 
     @Input()
-    public isRequired:boolean;
+    public isRequired: boolean;
 
     @Input()
-    public maxLength:number;
+    public maxLength: number;
 
     /** @description Set the maximum number value allowed. */
     @Input()
-    public maxValue:number;
+    public maxValue: number;
 
     /**  @description Set a minimum number of characters allowed. */
     @Input()
-    public minLength:number;
+    public minLength: number;
 
     /** @description Set the minimum number value allowed. */
     @Input()
-    public minValue:number;
+    public minValue: number;
 
     @Input()
-    public name:string;
+    public name: string;
 
     @Input()
-    public tooltipPlacement:string;
+    public tooltipPlacement: string;
 
     @Input()
-    public tooltipText:string;
+    public tooltipText: string;
 
     public isValid: boolean;
     public regex: string;
@@ -74,8 +67,7 @@ export class NumberInputComponent implements ControlValueAccessor, NumberInputIn
     private _onTouchedCallback: () => void = noop;
     private _onChangeCallback: (_: any) => void = noop;
 
-    constructor(private _inputRegex:string)
-    {
+    constructor(private _inputRegex: string) {
         this.regex = _inputRegex;
         this.isValid = true;
 
@@ -83,18 +75,15 @@ export class NumberInputComponent implements ControlValueAccessor, NumberInputIn
         this._id = `number-input_#${nextId++}`;
     }
 
-    public registerOnChange(fn:any):void
-    {
+    public registerOnChange(fn: any): void {
         this._onChangeCallback = fn;
     }
 
-    public registerOnTouched(fn:any):void
-    {
+    public registerOnTouched(fn: any): void {
         this._onTouchedCallback = fn;
     }
 
-    public writeValue(value:any):void
-    {
+    public writeValue(value: any): void {
         if (value !== this._innerValue) {
             this._innerValue = value;
         }
--- src/lib/components/forms/form/form-components/number-input/number-input.interface.ts
+++ src/lib/components/forms/form/form-components/number-input/number-input.interface.ts
@@ -1,11 +1,10 @@
 import { TerraFormComponentBaseInterface } from '../terra-form-component-base.interface';
 
-export interface NumberInputInterface extends TerraFormComponentBaseInterface
-{
-    emptyMessage:string;
-    invalidMessage:string;
-    maxLength:number;
-    maxValue:number;
-    minLength:number;
-    minValue:number;
+export interface NumberInputInterface extends TerraFormComponentBaseInterface {
+    emptyMessage: string;
+    invalidMessage: string;
+    maxLength: number;
+    maxValue: number;
+    minLength: number;
+    minValue: number;
 }
