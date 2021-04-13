f5e8d65af30326fd70e8da1cf33593efe064c35a
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-04-13 15:43:09 +0200

    interface that defines the needed inputs for the select component

---
+++ src/lib/components/forms/form/form-components/select/select.interface.ts
@@ -0,0 +1,8 @@
+import { TerraFormComponentBaseInterface } from '../terra-form-component-base.interface';
+import { TerraSelectBoxValueInterface } from '../../../select-box/data/terra-select-box.interface';
+
+export interface SelectInterface extends TerraFormComponentBaseInterface {
+    isSmall?: boolean; // TODO: Should we still support this?
+    openOnTop?: boolean; // TODO: Do we still need to support this? Material handles this automatically
+    listBoxValues: Array<TerraSelectBoxValueInterface>; // TODO: Maybe use an inline interface instead?
+}
