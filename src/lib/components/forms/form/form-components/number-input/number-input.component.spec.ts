a57ffb4126816168c04e5d07008a0708c9ce468d
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-04-13 09:50:27 +0200

    some specs using the MatInputHarness

--- src/lib/components/forms/form/form-components/number-input/number-input.component.spec.ts
+++ src/lib/components/forms/form/form-components/number-input/number-input.component.spec.ts
@@ -1,29 +1,91 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NumberInputComponent } from './number-input.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MockTooltipDirective } from '../../../../../testing/mock-tooltip.directive';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
 
 describe('NumberInputComponent', () => {
     let fixture: ComponentFixture<NumberInputComponent>;
     let component: NumberInputComponent;
-    let inputDebugElement: DebugElement;
-    let inputElement: HTMLInputElement;
+    let loader: HarnessLoader;
+    let input: MatInputHarness;
 
-    beforeEach(() => {
-        fixture = TestBed.configureTestingModule({
-            imports: [MatFormFieldModule, MatInputModule, NoopAnimationsModule],
-            declarations: [NumberInputComponent]
-        }).createComponent(NumberInputComponent);
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            imports: [MatFormFieldModule, MatInputModule, NoopAnimationsModule, FormsModule],
+            declarations: [NumberInputComponent, MockTooltipDirective]
+        });
 
+        fixture = TestBed.createComponent(NumberInputComponent);
         component = fixture.componentInstance;
-        inputDebugElement = fixture.debugElement.query(By.css('input'));
-        inputElement = inputDebugElement.nativeElement;
+        loader = TestbedHarnessEnvironment.loader(fixture);
+
+        fixture.detectChanges();
+
+        input = await loader.getHarness(MatInputHarness);
     });
 
-    it('should create', () => {
+    it('should create', async () => {
         expect(component).toBeTruthy();
+        expect(input).toBeTruthy();
+    });
+
+    it('should set required validation when #isRequired is set', async () => {
+        expect(await input.isRequired()).toBe(false);
+        component.isRequired = true;
+        fixture.detectChanges();
+
+        expect(await input.isRequired()).toBe(true);
+    });
+
+    it('should disable the input when #isDisabled is set', async () => {
+        expect(await input.isDisabled()).toBe(false);
+        component.isDisabled = true;
+        fixture.detectChanges();
+
+        expect(await input.isDisabled()).toBe(true);
+    });
+
+    it('should have #name as label of the input', async () => {
+        const formField: MatFormFieldHarness = await loader.getHarness(MatFormFieldHarness);
+        expect(await formField.getLabel()).toBe('');
+
+        component.name = 'My Label';
+        fixture.detectChanges();
+        expect(await formField.getLabel()).toBe(component.name);
+    });
+
+    it('should update the value of the input when writing a new value via `writeValue()`', async () => {
+        const value: number = 4;
+        component.writeValue(value);
+        expect(component.value).toEqual(value);
+
+        fixture.detectChanges();
+        expect(await input.getValue()).toEqual(value.toString());
+    });
+
+    xit('should call registered change callback whenever the value of the input is changed by the user', async () => {
+        const onChangeCallback: jasmine.Spy = jasmine.createSpy('onChange');
+        component.registerOnChange(onChangeCallback);
+
+        const value: number = 2;
+        await input.setValue(value.toString());
+
+        expect(onChangeCallback).toHaveBeenCalledWith(value);
+    });
+
+    it('should call registered touched callback whenever the input was blurred', async () => {
+        const onTouchedCallback: jasmine.Spy = jasmine.createSpy('onTouched');
+        component.registerOnTouched(onTouchedCallback);
+
+        await input.blur();
+
+        expect(onTouchedCallback).toHaveBeenCalled();
     });
 });
