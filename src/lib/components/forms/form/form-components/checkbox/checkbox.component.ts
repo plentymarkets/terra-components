f896046448486a863f56add0563a1f838e6fd801
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-04-19 15:14:00 +0200

    create checkbox component

---
+++ src/lib/components/forms/form/form-components/checkbox/checkbox.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'tc-checkbox',
+    templateUrl: './checkbox.component.html'
+})
+export class CheckboxComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
