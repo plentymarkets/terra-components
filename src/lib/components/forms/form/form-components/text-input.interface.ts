b8dac8f743544f358a96ce89e7a64fbd8221c4a0
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-04-20 11:15:31 +0200

    Create text-input.interface.ts

---
+++ src/lib/components/forms/form/form-components/text-input.interface.ts
@@ -0,0 +1,9 @@
+import { TerraFormComponentBaseInterface } from './terra-form-component-base.interface';
+
+export interface TextInputInterface extends TerraFormComponentBaseInterface {
+    isPassword: boolean;
+    isIban: boolean;
+    isReadonly: boolean;
+    minValue: number;
+    maxValue: number;
+}
