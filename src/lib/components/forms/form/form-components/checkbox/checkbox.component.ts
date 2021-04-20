090da97f81f593d0ba7a4049ee6ee28ddec852f3
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-04-20 10:31:56 +0200

    Update checkbox.component.ts

--- src/lib/components/forms/form/form-components/checkbox/checkbox.component.ts
+++ src/lib/components/forms/form/form-components/checkbox/checkbox.component.ts
@@ -1,11 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TerraPlacementEnum } from '../../../../../helpers';
+import { noop } from 'rxjs';
 
 @Component({
     selector: 'tc-checkbox',
     templateUrl: './checkbox.component.html'
 })
-export class CheckboxComponent implements OnInit {
-    constructor() {}
+export class CheckboxComponent {
+    @Input()
+    public name: string = '';
 
-    ngOnInit(): void {}
+    @Input()
+    public isDisabled: boolean = false;
+
+    @Input()
+    public isRequired: boolean = false;
+
+    @Input()
+    public tooltipText: string;
+
+    @Input()
+    public tooltipPlacement: TerraPlacementEnum = TerraPlacementEnum.TOP;
+
+    @Input()
+    public icon: string;
+
+    @Input()
+    public isIndeterminate: boolean;
+
+    public value: any;
+
+    public _onTouchedCallback: () => void = noop;
+
+    public _onChangeCallback: (_: any) => void = noop;
+
+    public registerOnChange(fn: (_: any) => void): void {
+        //
+    }
+
+    public registerOnTouched(fn: () => void): void {
+        //
+    }
+
+    public writeValue(value: any): void {
+        //
+    }
 }
