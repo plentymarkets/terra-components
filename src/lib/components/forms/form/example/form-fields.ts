96d45d9a7c56102cb0640efeddccc3288f7067f5
Maximilian Scharf <maximilian.scharf@web.de>
2021-04-23 15:41:39 +0200

    Update src/lib/components/forms/form/example/form-fields.ts
    Co-authored-by: Niklas Wolf <niklas.wolf@plentymarkets.com>

--- src/lib/components/forms/form/example/form-fields.ts
+++ src/lib/components/forms/form/example/form-fields.ts
@@ -125,7 +125,7 @@ export const formFields: TerraKeyValueInterface<TerraFormFieldInterface> = {
         type: 'date',
         defaultValue: '2019-01-01T00:00:00+01:00',
         options: {
-            name: 'Test Datum',
+            name: 'Test date',
             displayDateFormat: 'DD.MM.YYYY'
         }
     },
