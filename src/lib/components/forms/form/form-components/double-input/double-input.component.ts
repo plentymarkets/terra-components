2fd626e07f4bafce4fa9fbcb6c590f3e02af4942
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-04-28 17:59:15 +0200

    component wip

--- src/lib/components/forms/form/form-components/double-input/double-input.component.ts
+++ src/lib/components/forms/form/form-components/double-input/double-input.component.ts
@@ -1,12 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { DoubleInputInterface } from './double-input.interface';
+import { TerraPlacementEnum, TerraRegex } from '../../../../../helpers';
+import { noop } from 'rxjs';
 
 @Component({
     selector: 'tc-double-input',
     templateUrl: './double-input.component.html',
-    styleUrls: ['./double-input.component.scss']
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: DoubleInputComponent,
+            multi: true
+        }
+    ]
 })
-export class DoubleInputComponent implements OnInit {
-    constructor() {}
+export class DoubleInputComponent implements ControlValueAccessor, DoubleInputInterface, OnInit {
+    /** If true, the input will be disabled. Default false. */
+    @Input()
+    public isDisabled: boolean = false;
 
-    ngOnInit(): void {}
+    /** If true, a * indicates that the value is required. Default false. */
+    @Input()
+    public isRequired: boolean = false;
+
+    /** Set the label. */
+    @Input()
+    public name: string = '';
+
+    /** Set the tooltip placement (bottom, top, left, right). Default top. */
+    @Input()
+    public tooltipPlacement: TerraPlacementEnum = TerraPlacementEnum.TOP;
+
+    /** Set the tooltip. */
+    @Input()
+    public tooltipText: string = '';
+
+    /** If true, the value will be right-aligned. */
+    @Input()
+    public isPriceInput: boolean = false;
+
+    /** Set the decimal count. Default is 2 (0.01). */
+    @Input()
+    public set decimalCount(value: number) {
+        TerraRegex.getDouble(value);
+    }
+
+    /** The internal data model */
+    public value: number;
+
+    public _regex: string;
+    public _step: number;
+
+    /** Placeholders for the callbacks which are later provided by the Control Value Accessor. */
+    public _onTouchedCallback: () => void = noop;
+    public _onChangeCallback: (_: number) => void = noop;
+
+    public ngOnInit(): void {
+        this._step = 1 / Math.pow(10, this.decimalCount);
+    }
+
+    /** Registers a callback function that is called when the control's value changes in the UI.*/
+    public registerOnChange(fn: (_: number) => void): void {
+        this._onChangeCallback = fn;
+    }
+
+    /** Registers a callback function that is called by the forms API on initialization to update the form model on blur. */
+    public registerOnTouched(fn: () => void): void {
+        this._onTouchedCallback = fn;
+    }
+
+    /** Writes a new value to the element.*/
+    public writeValue(value: number): void {
+        this.value = value;
+    }
 }
