6ee18c132afe60c095f03bb55b488058dad7a924
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-03 14:59:22 +0200

    generate component

---
+++ src/lib/components/forms/form/form-components/multi-select/multi-select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tc-multi-select',
+    templateUrl: './multi-select.component.html',
+    styleUrls: ['./multi-select.component.scss']
+})
+export class MultiSelectComponent {}
