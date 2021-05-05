ea4f60ff903e0885fb70dd647c52759189972b6b
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-05 15:59:28 +0200

    guard checking if interval is given

--- src/lib/components/forms/form/form-components/slider/precision.ts
+++ src/lib/components/forms/form/form-components/slider/precision.ts
@@ -1,13 +1,12 @@
 /** @description A function, that calculates the precision on the given interval.
  * It will be used to format the value before it is displayed in the thumb label. */
 export function calculatePrecision(interval: number): number {
-    if (interval) {
-        const intervalLength: number = interval.toString().length;
-        const intervalTrunc: number = Math.trunc(interval).toString().length;
-        const hasFractionDigits: boolean = intervalLength === intervalTrunc;
-
-        return hasFractionDigits ? 0 : intervalLength - intervalTrunc - 1;
-    } else {
+    if (!interval) {
         return 0;
     }
+    const intervalDigits: number = interval.toString().length;
+    const integerDigits: number = Math.trunc(interval).toString().length;
+    const fractionDigits: number = intervalDigits - integerDigits;
+
+    return fractionDigits === 0 ? 0 : fractionDigits - 1;
 }
