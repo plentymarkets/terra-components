07239594e2b25d6c45ed2045225fe3c305c7b48b
z.hajj-hassan <ziyad.hajj-hassan@plentymarkets.com>
2021-05-06 13:21:13 +0200

    create unit test

--- src/lib/components/forms/form/form-components/suggestion/suggestion.component.ts
+++ src/lib/components/forms/form/form-components/suggestion/suggestion.component.ts
@@ -1,9 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TerraPlacementEnum } from '../../../../../helpers/enums/terra-placement.enum';
+import { TerraSuggestionBoxValueInterface } from '../../../suggestion-box/data/terra-suggestion-box.interface';
+import { noop } from 'rxjs';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
     selector: 'tc-suggestion',
-    templateUrl: './suggestion.component.html'
+    templateUrl: './suggestion.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: SuggestionComponent,
+            multi: true
+        }
+    ]
 })
-export class SuggestionComponent implements OnInit {
-    public ngOnInit(): void {}
+export class SuggestionComponent {
+    @Input()
+    public name: string = '';
+
+    @Input()
+    public isRequired: boolean = false;
+
+    @Input()
+    public isDisabled: boolean = false;
+
+    @Input()
+    public tooltipText: string = '';
+
+    @Input()
+    public tooltipPlacement: TerraPlacementEnum = TerraPlacementEnum.TOP;
+
+    @Input()
+    public listBoxValues: Array<TerraSuggestionBoxValueInterface> = [];
+
+    /** @description Stores a callback function which is executed whenever the select was blurred. */
+    public _onTouchedCallback: () => void = noop;
+
+    /** @description Stores a callback function which is executed whenever the value of the select changes. */
+    public _onChangeCallback: (_: any) => void = noop;
+
+    /** @description Internal model. The value of the selectBox. */
+    public value: any;
+
+    /** @description Writes a new value to the element.*/
+    public writeValue(value: any): void {
+        this.value = value;
+    }
+
+    /** @description Registers a callback function that is called when the control's value changes in the UI.*/
+    public registerOnChange(fn: (_: any) => void): void {
+        this._onChangeCallback = fn;
+    }
+
+    /** @description Registers a callback function that is called by the forms API on initialization to update the form model on blur.*/
+    public registerOnTouched(fn: () => void): void {
+        this._onTouchedCallback = fn;
+    }
 }
--- src/lib/terra-components.module.ts
+++ src/lib/terra-components.module.ts
@@ -17,6 +17,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocomplete, MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
     imports: [
@@ -35,7 +38,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatButtonModule,
         DragDropModule,
         MatIconModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatOptionModule,
+        MatAutocompleteModule
     ],
     declarations: [...components, ...directives, ...pipes],
     exports: [...exportedComponents, ...exportedDirectives, ...pipes]
