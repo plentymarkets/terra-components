0673d186f36495adb87c59041032bd37aed5f074
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-06 10:50:13 +0200

    Update src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.ts
    Co-authored-by: johannabecker <38043330+johannabecker@users.noreply.github.com>

--- src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.ts
+++ src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 /** @description
  * A pipe that returns how often to show ticks. It's relative to the step so that a tick always appears on a step.
- * A tick could be set by an interval. If interval is undefined, the ticks will be calculated automatically.
+ * A tick can be set by an interval. If the interval is undefined, the ticks will be calculated automatically.
  * If showTicks is false, the interval will be set to 0. */
 @Pipe({
     name: 'tickInterval'
