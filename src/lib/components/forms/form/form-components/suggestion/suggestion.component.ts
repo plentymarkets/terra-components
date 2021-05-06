ad18265f993c5db570046f10974a7858c2335d6b
z.hajj-hassan <ziyad.hajj-hassan@plentymarkets.com>
2021-05-06 08:22:22 +0200

    create suggestion component

---
+++ src/lib/components/forms/form/form-components/suggestion/suggestion.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'tc-suggestion',
+    templateUrl: './suggestion.component.html'
+})
+export class SuggestionComponent implements OnInit {
+    public ngOnInit(): void {}
+}
