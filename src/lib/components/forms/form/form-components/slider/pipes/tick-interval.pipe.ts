4d8df33425b75afb6b5294f7987c658aced40d6f
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-07 11:00:48 +0200

    JsDoc

--- src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.ts
+++ src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.ts
@@ -1,14 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 /**
- * A pipe that returns how often to show ticks. It's related to the step so that a tick always appears on a step.
- * A tick can be set by an interval. If the interval is undefined, the ticks will be calculated automatically.
- * If showTicks is false, the interval will be set to 0. */
+ * A pipe that that determines the tick interval for the material slider.
+ * Depending on the given interval and the flag showTicks, 0, 1 or 'auto' is returned. */
 @Pipe({
     name: 'tickInterval'
 })
 export class TickIntervalPipe implements PipeTransform {
-    public transform(showTicks: boolean, interval: number): number | 'auto' {
+    public transform(showTicks: boolean, interval: number): 0 | 1 | 'auto' {
         return showTicks ? (interval ? 1 : 'auto') : 0;
     }
 }
