1a523d10399be8112fdb68193d4f8fe74de9f3e7
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-05-07 11:11:51 +0200

    improve tests for the tick-interval.pipe

--- src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.spec.ts
+++ src/lib/components/forms/form/form-components/slider/pipes/tick-interval.pipe.spec.ts
@@ -9,17 +9,18 @@ describe('TickIntervalPipe', () => {
 
     it('should return 1 when showTicks is true and any valid interval (>0) is given', () => {
         expect(pipe.transform(true, 1)).toEqual(1);
+        expect(pipe.transform(true, 2)).toEqual(1);
     });
 
-    it(`should return 'auto' if the interval is undefined`, () => {
+    it(`should return 'auto' if the interval is 0, null or undefined`, () => {
+        expect(pipe.transform(true, 0)).toEqual('auto');
+        expect(pipe.transform(true, null)).toEqual('auto');
         expect(pipe.transform(true, undefined)).toEqual('auto');
     });
 
-    it('should return 0 if showTicks is false and the interval is undefined', () => {
+    it('should return 0 if showTicks is false no matter what interval is given', () => {
         expect(pipe.transform(false, undefined)).toEqual(0);
-    });
-
-    it('should return 0 if showTicks is false, but the interval is given', () => {
-        expect(pipe.transform(false, 1)).toEqual(0);
+        expect(pipe.transform(false, 0)).toEqual(0);
+        expect(pipe.transform(false, 2)).toEqual(0);
     });
 });
