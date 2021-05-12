108410a7edbd9cfea955e20a5edc9cc09a987358
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-05-12 09:24:42 +0200

    Update src/lib/components/forms/form/form-components/suggestion/suggestion.component.spec.ts
    Co-authored-by: Pascal Weyrich <pascal.weyrich@plentymarkets.com>

--- src/lib/components/forms/form/form-components/suggestion/suggestion.component.spec.ts
+++ src/lib/components/forms/form/form-components/suggestion/suggestion.component.spec.ts
@@ -95,13 +95,6 @@ describe('SuggestionComponent', () => {
         expect(await input.isRequired()).toBe(true);
     });
 
-    it('should disable the input when #isDisabled is set', async () => {
-        expect(await autoComplete.isDisabled()).toBe(false);
-        component.isDisabled = true;
-        fixture.detectChanges();
-
-        expect(await autoComplete.isDisabled()).toBe(true);
-    });
 
     it('should enable the input when #isDisabled is set to false', async () => {
         expect(await autoComplete.isDisabled()).toBe(false);
