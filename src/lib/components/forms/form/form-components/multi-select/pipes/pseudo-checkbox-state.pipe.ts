02787c9b884eae1608bf693313e9ac83360e86bd
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-06-10 09:14:52 +0200

    JSDoc PseudoCheckboxPipe

--- src/lib/components/forms/form/form-components/multi-select/pipes/pseudo-checkbox-state.pipe.ts
+++ src/lib/components/forms/form/form-components/multi-select/pipes/pseudo-checkbox-state.pipe.ts
@@ -1,6 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { MatOption, MatPseudoCheckboxState } from '@angular/material/core';
 
+/**
+ * Evaluates the state of a PseudoCheckbox by comparing a list of selected options with the total list of options.
+ * When no options are selected, 'unchecked' is returned.
+ * When more than one, but not all options are selected, 'indeterminate' is returned.
+ * When all options are selected, 'checked' is returned.
+ */
 @Pipe({
     name: 'pseudoCheckboxState'
 })
