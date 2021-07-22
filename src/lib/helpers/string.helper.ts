c326c284e42fe5a05956fcd61c5dc4d32d2d57ba
Dominik Reuting <dominik.reuting@gmail.com>
2021-07-22 13:34:43 +0200

    deprecate StringHelper

--- src/lib/helpers/string.helper.ts
+++ src/lib/helpers/string.helper.ts
@@ -1,3 +1,4 @@
+/** @deprecated since v11. Use `str === null || str === undefined || str.length === 0` instead. */
 export class StringHelper {
     public static isNullUndefinedOrEmpty(str: string): boolean {
         return str === null || str === undefined || str.length === 0;
