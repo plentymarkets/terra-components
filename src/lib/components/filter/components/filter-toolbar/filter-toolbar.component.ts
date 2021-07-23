d1fed81e1734778f8bcee93c64192b2fc9470357
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-07-23 15:34:07 +0200

    wip

--- src/lib/components/filter/components/filter-toolbar/filter-toolbar.component.ts
+++ src/lib/components/filter/components/filter-toolbar/filter-toolbar.component.ts
@@ -1,8 +1,19 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    Output,
+    SimpleChanges,
+    ViewChild
+} from '@angular/core';
 import { FilterChipDefDirective } from '../../directives/filter-chip-def.directive';
 import { FilterMenuDirective } from '../../directives/filter-menu.directive';
 import { Observable } from 'rxjs';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { switchMap } from 'rxjs/operators';
+import { FilterContainerDirective } from '../../directives/filter-container.directive';
 
 /** A toolbar providing necessary elements for applying filters to a table. */
 @Component({
@@ -10,10 +21,10 @@ import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
     templateUrl: './filter-toolbar.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FilterToolbarComponent {
+export class FilterToolbarComponent implements OnChanges {
     /** Reference to the FilterMenuDirective containing all information needed to render the chips */
     @Input()
-    public filterMenu: FilterMenuDirective;
+    public filterMenu: FilterMenuDirective | undefined;
 
     /** Fires when the user clicks the search button in the filter toolbar */
     @Output()
@@ -21,15 +32,21 @@ export class FilterToolbarComponent {
 
     /** Reference to the MatMenuTrigger */
     @ViewChild(MatMenuTrigger)
-    public menuTrigger: MatMenuTrigger;
+    public menuTrigger!: MatMenuTrigger;
 
     /** List of chip definitions retrieved by the FilterContainerDirective */
-    public get chips$(): Observable<Array<FilterChipDefDirective>> | undefined {
-        return this.filterMenu.container ? this.filterMenu.container.chips$ : undefined;
+    public _chips$: Observable<Array<FilterChipDefDirective>> | undefined;
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.hasOwnProperty('filterMenu')) {
+            this._chips$ = this.filterMenu?.container$?.pipe(
+                switchMap((container: FilterContainerDirective) => container.chips$)
+            );
+        }
     }
 
     /** Reference to the material menu containing the filter form */
-    public get menu(): MatMenu {
-        return this.filterMenu.menu;
+    public get menu(): MatMenu | undefined {
+        return this.filterMenu?.menu;
     }
 }
--- src/lib/components/filter/directives/filter-menu.directive.ts
+++ src/lib/components/filter/directives/filter-menu.directive.ts
@@ -1,6 +1,8 @@
 import { Directive } from '@angular/core';
 import { MatMenu } from '@angular/material/menu';
 import { FilterContainerDirective } from './filter-container.directive';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 /** A directive collecting references to the form, menu and the filter-container */
 @Directive({
@@ -8,13 +10,29 @@ import { FilterContainerDirective } from './filter-container.directive';
     exportAs: 'terraFilterMenu'
 })
 export class FilterMenuDirective {
+    /** A Stream that emits whenever the FilterContainerDirective is set. */
+    public container$!: Observable<FilterContainerDirective>;
+
     /** Reference to the filter container. Gives access to the chip definitions. */
-    public container: FilterContainerDirective;
+    public get container(): FilterContainerDirective | undefined {
+        return this._container$.getValue();
+    }
+    public set container(value: FilterContainerDirective | undefined) {
+        this._container$.next(value);
+    }
+    private _container$: BehaviorSubject<FilterContainerDirective | undefined> = new BehaviorSubject<
+        FilterContainerDirective | undefined
+    >(undefined);
 
     constructor(
         /** Reference to the material menu. */
         public menu: MatMenu
     ) {
+        this.container$ = this._container$.asObservable().pipe(
+            filter((container: FilterContainerDirective | undefined): container is FilterContainerDirective => {
+                return container instanceof FilterContainerDirective;
+            })
+        );
         // add our custom class to the menu panel
         this.menu.panelClass = 'terra-filter-menu';
     }
