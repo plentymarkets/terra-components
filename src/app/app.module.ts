296f97ecdc820b098736e219ec12a85a376704a8
liviu.muraru.plentymarkets <liviu.muraru@plentymarkets.com>
2021-10-11 11:01:37 +0300

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { L10nIntlModule, L10nLoader, L10nTranslationModule } from 'angular-l10n';
+import { L10nLoader, L10nTranslationModule } from 'angular-l10n';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { l10nConfig } from './translation/l10n.config';
 import { AppComponent } from './app.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
-import { TerraComponentsExamplesModule } from '../lib/terra-components-examples.module';
 import { RouterModule } from '@angular/router';
 import { DefaultUserLanguage } from './translation/user-language';
-import { TerraFilterModule } from 'src/lib';
-import { MatIconModule } from '@angular/material/icon';
 
 export function initL10n(l10nLoader: L10nLoader): Function {
     return (): Promise<void> => l10nLoader.init();
@@ -27,8 +24,7 @@ export function initL10n(l10nLoader: L10nLoader): Function {
         BrowserAnimationsModule,
         RouterModule.forRoot([]),
         HttpClientModule,
-        L10nTranslationModule.forRoot(l10nConfig, { userLanguage: DefaultUserLanguage }),
-        L10nIntlModule
+        L10nTranslationModule.forRoot(l10nConfig, { userLanguage: DefaultUserLanguage })
     ],
     declarations: [AppComponent, ShowcaseComponent],
     providers: [
