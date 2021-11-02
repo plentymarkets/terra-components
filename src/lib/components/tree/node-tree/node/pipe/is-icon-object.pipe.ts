6ec98750cd7e2ae4c12ccfad26350f4a68e3d9f1
Michael Kunze <michael.kunze@plentymarkets.com>
2021-11-02 13:47:41 +0100

    Update is-icon-object.pipe.ts

--- src/lib/components/tree/node-tree/node/pipe/is-icon-object.pipe.ts
+++ src/lib/components/tree/node-tree/node/pipe/is-icon-object.pipe.ts
@@ -5,7 +5,7 @@ import { TerraRouteIcon } from '../../../../../types/terra-route-icon.type';
     name: 'isIconObject'
 })
 export class IsIconObjectPipe implements PipeTransform {
-    transform(value: TerraRouteIcon): value is { fontSet: 'material-icons' | 'plentyicons'; fontIcon: string } {
+    public transform(value: TerraRouteIcon): value is { fontSet: 'material-icons' | 'plentyicons'; fontIcon: string } {
         return typeof value === 'object';
     }
 }
