041bb8b4ddd7b18c31bf4c7609dc3aff4773adf0
Michael Kunze <michael.kunze@plentymarkets.com>
2021-11-02 10:27:27 +0100

    add mat icons to node tree

--- src/lib/pipes/pipe-collection.ts
+++ src/lib/pipes/pipe-collection.ts
@@ -2,6 +2,7 @@ import { Type } from '@angular/core';
 import { SelectSortPipe } from './select-sort.pipe';
 import { TickIntervalPipe } from '../components/forms/form/form-components/slider/pipes/tick-interval.pipe';
 import { PseudoCheckboxStatePipe } from '../components/forms/form/form-components/multi-select/pipes/pseudo-checkbox-state.pipe';
+import { IsIconObjectPipe } from '../components/tree/node-tree/node/pipe/is-icon-object.pipe';
 
 export const exportedPipes: Array<Type<any>> = [SelectSortPipe];
-export const pipes: Array<Type<any>> = [...exportedPipes, TickIntervalPipe, PseudoCheckboxStatePipe];
+export const pipes: Array<Type<any>> = [...exportedPipes, TickIntervalPipe, PseudoCheckboxStatePipe, IsIconObjectPipe];
