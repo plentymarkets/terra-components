e29a7bb74009dd05d5586cec213b63084b4869c3
Alexandru Dumitrescu <alexandru.dumitrescu@plentymarkets.com>
2021-11-03 09:55:48 +0200

    Revert

--- src/lib/components/directive-collection.ts
+++ src/lib/components/directive-collection.ts
@@ -3,7 +3,6 @@ import { TerraDataTableContextMenuDirective } from './tables/data-table/context-
 import { TerraTwoColumnsContainerDirective } from './layouts/column-container/two-columns/terra-two-columns-container.directive';
 import { FormEntryContainerDirective } from './forms/form/form-entry/form-entry-container.directive';
 import { CKEditorDirective } from './editors/ck-editor/ck-editor.directive';
-import { TooltipDirective } from './tooltip/tooltip.directive';
 import { SelectSortDirective } from './forms/select-box/directive/select-sort.directive';
 import { IbanValidatorDirective } from '../validators/iban-validator';
 
